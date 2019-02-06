e394b27b361816c0008a6ca2d065f058d50149dd
Annie Enchakattu <annie.enchakattu@gramener.com>
2019-02-05 17:43:58 -0800

    added demo site

---
+++ demo/aadConfig.py
@@ -0,0 +1,9 @@
+RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
+TENANT = "microsoft.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
+CLIENT_ID = '<add client ID here>'
+CLIENT_SECRET = '<add client secret here>'
+
+
+# These settings are for the Microsoft Graph API Call
+API_VERSION = 'v1.0'
