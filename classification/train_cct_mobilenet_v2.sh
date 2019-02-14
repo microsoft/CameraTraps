7aa6a9cadc5badfe51787ced0b145403740dd62b
Marcel Simon <a-msimon@microsoft.com>
2019-02-14 11:26:22 +0000

    Finished script for training mobilenet on cct
    Former-commit-id: e2656069aa785f357dbba22ca203ca1b24816744

--- classification/train_cct_mobilenet_v2.sh
+++ classification/train_cct_mobilenet_v2.sh
@@ -1,12 +1,49 @@
 DATASET_DIR=/data/lila/caltech/cct_cropped_tfrecords
 TRAIN_DIR=./log/$(date +"%Y-%m-%d_%H.%M.%S")-logits
 CHECKPOINT_PATH=/home/loris/git/tf-classification/pre-trained/mobilenet_v2_1.4_224/mobilenet_v2_1.4_224.ckpt
+MODEL_NAME=mobilenet_v2_140
+CHECKPOINT_EXCLUDE=MobilenetV2/Logits
+NUM_GPUS=1
+
 python train_image_classifier.py \
     --train_dir=${TRAIN_DIR} \
     --dataset_dir=${DATASET_DIR} \
-    --dataset_name=cameratrap \
+    --dataset_name=cct \
     --dataset_split_name=train \
-    --model_name=inception_v3 \
+    --model_name=${MODEL_NAME} \
     --checkpoint_path=${CHECKPOINT_PATH} \
-    --checkpoint_exclude_scopes=Mobilenet/Logits \
-    --trainable_scopes=Mobilenet/Logits
+    --checkpoint_exclude_scopes=${CHECKPOINT_EXCLUDE} \
+    --trainable_scopes=${CHECKPOINT_EXCLUDE} \
+    --max_number_of_steps=20000 \
+    --preprocessing_name="inception_v2" \
+    --learning_rate=0.045 \
+    --label_smoothing=0.1 \
+    --batch_size=96 \
+    --learning_rate_decay_factor=0.9 \
+    --num_epochs_per_decay=1 \
+    --moving_average_decay=0.9999
+
+# Run evaluation.
+python eval_image_classifier.py \
+    --eval_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=cct \
+    --dataset_split_name=test \
+    --model_name=${MODEL_NAME} \
+    --checkpoint_path=${TRAIN_DIR}
+
+python train_image_classifier.py \
+    --train_dir=${TRAIN_DIR}/all \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=cct \
+    --dataset_split_name=train \
+    --model_name=${MODEL_NAME} \
+    --checkpoint_path=${TRAIN_DIR} \
+    --preprocessing_name="inception_v2" \
+    --learning_rate=0.0045 \
+    --label_smoothing=0.1 \
+    --batch_size=96 \
+    --learning_rate_decay_factor=0.98 \
+    --num_epochs_per_decay=50 \
+    --moving_average_decay=0.9999
+
