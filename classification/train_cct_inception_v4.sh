d8028ee61519559227b48a6a340558a455d046bb
Marcel Simon <a-msimon@microsoft.com>
2019-02-19 08:07:02 +0000

    Some fixes for the Inception script
    Former-commit-id: 87c8d1cf93a8d959885672af235124aaa8213ca5

--- classification/train_cct_inception_v4.sh
+++ classification/train_cct_inception_v4.sh
@@ -1,12 +1,10 @@
 DATASET_DIR=/data/lila/caltech/cct_cropped_tfrecords
-#TRAIN_DIR=./log/$(date +"%Y-%m-%d_%H.%M.%S")-incv4
-TRAIN_DIR=log/2019-02-15_08.49.56-incv4/
-CHECKPOINT_PATH=/home/loris/git/tf-classification/pre-trained/inception_v4/inception_v4.ckpt
+TRAIN_DIR=./log/$(date +"%Y-%m-%d_%H.%M.%S")-incv4
+CHECKPOINT_PATH=/path/to/pre-trained/inception_v4/inception_v4.ckpt
 MODEL_NAME=inception_v4
 CHECKPOINT_EXCLUDE=InceptionV4/AuxLogits,InceptionV4/Logits
 NUM_GPUS=1
 
-: '
 python train_image_classifier.py \
     --train_dir=${TRAIN_DIR}/init \
     --dataset_dir=${DATASET_DIR} \
@@ -34,7 +32,7 @@ python eval_image_classifier.py \
     --dataset_split_name=test \
     --model_name=${MODEL_NAME} \
     --checkpoint_path=${TRAIN_DIR}
-'
+
 python train_image_classifier.py \
     --train_dir=${TRAIN_DIR}/all \
     --dataset_dir=${DATASET_DIR} \
