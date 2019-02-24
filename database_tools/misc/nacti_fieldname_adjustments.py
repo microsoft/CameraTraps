a4e85767d3026349c6e1529d143cc72b31d41569
Ubuntu <findingcows@nactidownloadvm.5svuschbwnqu5b5t3fbzyv3y5b.xx.internal.cloudapp.net>
2019-02-24 23:51:38 +0000

    Sanity-checking NACTI data
    Former-commit-id: 3bd652ae5edaf06bbd9528a75278d68229cfa565

--- database_tools/misc/nacti_fieldname_adjustments.py
+++ database_tools/misc/nacti_fieldname_adjustments.py
@@ -12,8 +12,8 @@
 import json
 import os
 
-inputJsonFile = r'd:\temp\nacti_metadata_orig.json'
-outputJsonFile = r'd:\temp\nacti_metadata.json'
+inputJsonFile = r'/datadrive1/nacti_metadata_orig.json'
+outputJsonFile = r'/datadrive1/nacti_metadata.json'
 
 assert os.path.isfile(inputJsonFile)
 
@@ -28,23 +28,43 @@ annotations = data['annotations']
 categories = data['categories']
 info = data['info']
 
+print('Finished reading input .json')
+
 
 #%% Rev version number, update field names and types
 
-assert(info['version']==1.0)
-info['version'] = 1.01
+assert(info['version'] == 1.0)
+info['version'] = 1.1
+nFilenameConversions = 0
+nCatConversions = 0
 
 for image in images:
     assert 'filename' in image
     image['file_name'] = image['filename']
     del image['filename']
-
+    nFilenameConversions += 1
+    assert 'SEQ_NO' in image
+    del image['SEQ_NO']
+    assert 'IMG_WIDTH' in image and isinstance(image['IMG_WIDTH'],str)
+    assert 'IMG_HEIGHT' in image and isinstance(image['IMG_HEIGHT'],str)
+    image['width'] = int(image['IMG_WIDTH']) 
+    image['height'] = int(image['IMG_HEIGHT']) 
+    del image['IMG_WIDTH']
+    del image['IMG_HEIGHT']
+    
 for cat in categories:
     assert 'id' in cat
     assert isinstance(cat['id'],str)
     cat['id'] = int(cat['id'])
+    nCatConversions += 1
     
-    
+print('Finished checking data, converted {} filename fields and {} category IDs'.format(
+        nFilenameConversions,nCatConversions))
+
+
 #%% Write json file
     
 json.dump(data, open(outputJsonFile, 'w'))
+
+print('Finished writing output .json to {}'.format(outputJsonFile))
+
