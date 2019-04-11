47bd259b195addd51fa8adb3d4b4ea8feb8bcde3
aenchakattu <annie.enchakattu@gramener.com>
2019-04-11 10:24:35 -0700

    updated demo with latest API

--- demo/app.py
+++ demo/app.py
@@ -5,7 +5,10 @@ from flask_restful import Resource, Api
 from urllib.parse import unquote
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
+from requests_toolbelt.multipart import decoder
 from . import app, photos, api
+import CameraTrapAssets.apiconfig as apiconfig
+
 import os
 # from . import model 
 import json
@@ -21,6 +24,10 @@ from PIL import Image
 from . import aadConfig as aad
 from . import login_helper 
 import time
+import io
+
+api_url_format = '{0}/camera-trap/detect?confidence={1}&render={1}'
+results_folder = '/CameraTrapAssets/results/'
 
 
 # routes for cameratrapassets as these are being loaded
@@ -37,196 +44,112 @@ def gallery_images(path):
 def gallery_resut_images(path):
     return send_from_directory('CameraTrapAssets/gallery_results/', path)
 
+@app.route('/CameraTrapAssets/results/<path:path>')
+def result_images(path):
+    return send_from_directory('CameraTrapAssets/results/', path)
 
-@app.route('/sessionclear')
-def ClearLoginSession():
-    if login_helper.is_logged_in():
-        session.pop('logged_in')
-    return 'session cleared'
+def get_api_headers():
+    return {'Ocp-Apim-Subscription-Key': apiconfig.api['subscription_key']}
 
+def call_api():
+    params = {
+      'confidence': 0.8,
+      'render': True
+    }
 
-@app.route("/authorized")
-def authorized():
+    posted_files = request.files
+    files = {}
+    images = []
+    for f in posted_files:
+        file = posted_files.get(f)
+        image_name = secure_filename(file.filename)
+        images.append(image_name)
 
-    REDIRECT_URI = os.path.dirname(request.url) + "/authorized"
+        if not image_name.lower().endswith('.jpg'):
+            continue
 
-    code = request.args['code']
+        files[image_name] = (image_name, file, 'image/jpeg')
     
-    auth_context = adal.AuthenticationContext(app.config["AUTHORITY_URL"])
-    token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, aad.RESOURCE,
-                                                                        aad.CLIENT_ID, aad.CLIENT_SECRET)
+    r = requests.post(apiconfig.api['base_url'] + 'detect', params=params, files=files, headers=get_api_headers())
     
-    # It is recommended to save this to a database when using a production app.
-    session['access_token'] = token_response['accessToken']
-    session['logged_in'] = True
+    start = time.time()
+
+    for f in posted_files:
+        file = posted_files.get(f)
+        image_name = secure_filename(file.filename)
+        try:
+            save_file = file
+            save_file.save(os.getcwd() + '/CameraTrapAssets/imgs/' + image_name)
+        except Exception as e:
+            print(str(e))
     
-    url = session['path']
+    end = time.time()
+    print(end - start)
+
+    return r, images
+
+#def track_images(file, name):
+#   print(str(e))
+
+@app.route('/processImages', methods=['POST'])
+def process_images():
+    error = False
+    print("calling api...")
     
-    resp = Response(status=307)
-    resp.headers['location'] =  url
-    return resp
-    #return render_template(template_path)
+    try:
+        
+        r, images = call_api()
+        results = decoder.MultipartDecoder.from_response(r)
+
+        image_output = []
+        for part in results.parts:
+            #print(part)
+            # part is a BodyPart object with b'Content-Type', and b'Content-Disposition', 
+            # the later includes 'name' and 'filename' info
+            headers = {}
+            for k, v in part.headers.items():
+                headers[k.decode(part.encoding)] = v.decode(part.encoding)
+            if headers.get('Content-Type', None) == 'image/jpeg':
+                c = headers.get('Content-Disposition')
+                image_name = c.split('name="')[1].split('"')[0]  
+                image = Image.open(io.BytesIO(part.content))
+                image.save(os.getcwd() + results_folder + image_name)
+    
+            elif headers.get('Content-Type', None) == 'application/json':
+                #bbox points, confidence
+                json_results = json.loads(part.content.decode())
+                print(json_results)
+                for img_name in images:
+                    img_result = json_results[img_name]
+                    #print(img_result)
+                    image_output.append({
+                        "path": results_folder + img_name,
+                        "num_objects": len(img_result),
+                        "org_path": results_folder + img_name,
+                        "image_name": img_name,
+                        "result": {},
+                        "bboxes": {}
+                    })
+        #print(image_output)
+        session['image_output'] = image_output
+        
+    except Exception as e:
+        print(str(e))
+        error = True
+    if error:
+        return "Error occurred while calling API"
+    return "Success"
 
 @app.route('/')
 def index():
-    #if not login_helper.is_logged_in():
-    #    return login_helper.redirect_to_login()
     return render_template('index.html')
             
 @app.route('/upload')
 def upload():
-    #if not login_helper.is_logged_in():
-    #    return login_helper.redirect_to_login()
     return render_template('upload.html')
-
-@app.route("/login")
-def login():
-    
-    REDIRECT_URI = os.path.dirname(request.url) + "/authorized"
-    print(REDIRECT_URI)
-
-    auth_state = str(uuid.uuid4())
-    session['state'] = auth_state
-    authorization_url = app.config["TEMPLATE_AUTHZ_URL"].format(
-        aad.TENANT,
-        aad.CLIENT_ID,
-        REDIRECT_URI,
-        auth_state,
-        aad.RESOURCE)
-    
-    resp = Response(status=307)
-    resp.headers['location'] = authorization_url
-    return resp
-
-@app.route('/processurlimage', methods=['GET'])
-def processurlimage():
-    print('here')
-    image_output = []
-    download_json = {}
-    image_url = request.args.get('image_url')
-    org_url = image_url
-    filename = image_url.split('/')[-1].split('?')[0]
-    replacefilename = filename
-    r = requests.get(image_url,
-                 stream=True, headers={'User-agent': 'Mozilla/5.0'})
-    if r.status_code == 200:
-        r.raw.decode_content = True
-        image_url = BytesIO(r.raw.read())
-
-
-    params = {filename: image_url}
-    detection_output = requests.post(app.config['API_URL'], files=params).json()
-    print(detection_output)
-    
-    name, ext = os.path.splitext(filename.split('/')[-1])
-    replace_characters = [' ', '_', ',', '-']
-    for rc in replace_characters:
-        name = name.replace(rc, '')
-        replacefilename = replacefilename.replace(rc, '')
-    
-    detection_key = name + '.jpg'
-    img_file = detection_output[detection_key].get('img_file')
-    #outputFileName = "{}{}".format('static/results/' + name, '.png')
-    outputfile = detection_output[detection_key].get('img_file')
-    image_output.append({
-        "path": outputfile,
-        "num_objects": detection_output[detection_key].get('number_of_animals'),
-        "org_path": org_url,
-        "image_name": filename,
-        "result": detection_output[detection_key].get('status'),
-        "bboxes": detection_output[detection_key].get('boxes')
-    })
-    download_json[img_file.split('/')[-1]] = {
-        "bboxes": '"' + str(detection_output[detection_key].get('boxes')) + '"',
-            "message": detection_output[detection_key].get('status'),
-            "num_objects": detection_output[detection_key].get('number_of_animals')
-
-    }
-
-    return render_template('results.html', result_det=image_output, output_json=download_json)
-
-
-@app.route('/processimages', methods=['POST'])
-def processimages():
-    image_output = []
-    download_json = {}
-    if request.method == 'POST':
-        file_obj = request.files
-        for f in file_obj:
-            file = request.files.get(f)
-            img_name = secure_filename(file.filename)
-            #print(img_name)
-
-              
-            if os.path.exists(os.path.join(os.getcwd(), 'static/uploads/', img_name)):
-                os.remove(os.path.join(os.getcwd(), 'static/uploads/', img_name))
-
-            filename = photos.save(
-                file,
-                name=img_name
-            )
-            img_file = photos.url(filename)
-            filename = img_file.split('/')[-1]
-            
-            print("-----")
-            print("url:" + filename)
-            print("-----")
-
-            org_path = img_file
-            base_url = request.base_url.replace('processimages', 'static/uploads/')
-            img_url = base_url  + filename
-
-
-            start = time.time()
-            img = Image.open(BytesIO(urlopen.urlopen(img_file).read()))
-            img.thumbnail((img.size[0] / 2, img.size[1] / 2), Image.ANTIALIAS)
-            os.remove(os.path.join(os.getcwd(), 'static/uploads/', img_name))
-            img.save(os.path.join(os.getcwd(), 'static/uploads/', img_name))
-            end = time.time()
-            print('Time taken', end - start)
-            files = {img_file.split('/')[-1]: BytesIO(urlopen.urlopen(img_url).read())}
-            form = {'image_name': img_file.split('/')[-1]}
-            detection_output = requests.post(app.config["API_URL"], files=files).json()
-            print(detection_output)
-            name, ext = os.path.splitext(img_file.split('/')[-1])
-
-            #remove this when api is fixed to not remove spaces and other characters
-            replace_characters = [' ', '_', ',', '-']
-            for rc in replace_characters:
-                name = name.replace(rc, '')
-           
-            detection_key = name + '.jpg'
-            
-            print(detection_key)
-            
-            outputfile = detection_output[detection_key].get('img_file')
-
-            print(outputfile)
-
-            image_output.append({
-                "path": outputfile,
-                "num_objects": detection_output[detection_key].get('number_of_animals'),
-                "org_path": org_path,
-                "image_name": filename,
-                "result": detection_output[detection_key].get('status'),
-                "bboxes": detection_output[detection_key].get('boxes')
-            })
-            download_json[img_file.split('/')[-1]] = {
-                "bboxes": '"' + str(detection_output[detection_key].get('boxes')) + '"',
-                 "message": detection_output[detection_key].get('status'),
-                 "num_objects": detection_output[detection_key].get('number_of_animals')
-
-            }
-
-        session['image_output'] = image_output
-        session['download_json'] = download_json
-        return 'Uploading ....'
-
 
 @app.route('/results')
 def results():
-    #if not login_helper.is_logged_in():
-    #    return login_helper.redirect_to_login()
     print('results...')
     # redirect to home if no images to display
     if "image_output" not in session or session['image_output'] == []:
@@ -234,25 +157,19 @@ def results():
 
     image_output = session['image_output']
     session.pop('image_output', None)
-
-    return render_template('results.html', result_det=image_output, output_json=session['download_json'])
+    
+    return render_template('results.html', result_det=image_output, output_json=[{}])
 
 
 @app.route('/gallery')
 def gallery():
-    #if not login_helper.is_logged_in():
-    #    return login_helper.redirect_to_login()
     gallery_images = os.listdir('CameraTrapAssets/gallery/')
     gallery_images = ['CameraTrapAssets/gallery/' + img for img in gallery_images]
-    #gallery_images = random.sample(gallery_images, 12)
+    
     return render_template('gallery.html', gallery_images=gallery_images)
-
-
 
 @app.route('/gallery_results/<img_index>', methods=['GET'])
 def gallery_results(img_index):
-    #if not login_helper.is_logged_in():
-    #    return login_helper.redirect_to_login()
     gallery_images = os.listdir('CameraTrapAssets/gallery/')
     gallery_images.remove(img_index)
     gallery_images.insert(0, img_index)
