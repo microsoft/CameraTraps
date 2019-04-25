a0bca615f8d09c215b54fe6bb544285bd062cccb
Dan Morris <dmorris@cs.stanford.edu>
2019-04-25 16:37:37 -0700

    batch results postprocessing, json wrapper classes

--- api/detector_batch_processing/postprocess_batch_results.py
+++ api/detector_batch_processing/postprocess_batch_results.py
@@ -23,6 +23,7 @@ import os
 import sys
 import argparse
 import pandas as pd
+from enum import Enum
 from tqdm import tqdm
 from collections import defaultdict
 
@@ -34,35 +35,145 @@ image_base_dir = r'd:\wildlife_data\mcgill_test'
 ground_truth_json_file = r'd:\wildlife_data\mcgill_test\mcgill_test.json'
 output_dir = r'd:\temp\postprocessing_tmp'
 
+negative_classes = ['empty']
+
+
+#%% Helper classes and functions
+
+# Flags used to mark images as positive or negative for P/R analysis (according
+# to ground truth and/or detector output)
+class DetectionStatus(Enum):
+    
+    # This image is a negative
+    DS_NEGATIVE = 0
+    
+    # This image is a positive
+    DS_POSITIVE = 1
+    
+    # This image has annotations suggesting both negative and positive
+    DS_AMBIGUOUS = 2
+    
+    # This image is not annotated
+    DS_UNKNOWN = 3
+
+
+class IndexedJsonDb:
+    """
+    Wrapper for a COCO Camera Traps database.
+    
+    Handles boilerplate dictionary creation that we do almost every time we load 
+    a .json database.
+    """
+    
+    # The underlying .json db
+    db = None
+    
+    # Useful dictionaries
+    cat_id_to_name = None
+    cat_name_to_id = None
+    filename_to_id = None
+    image_id_to_annotations = None
+
+    def __init__(self,jsonFilename,b_normalize_paths=False):
+       
+        self.db = json.load(open(jsonFilename))
+    
+        if b_normalize_paths:
+            # Normalize paths to simplify comparisons later
+            for im in self.db['images']:
+                im['file_name'] = os.path.normpath(im['file_name'])
+        
+        ### Build useful mappings to facilitate working with the DB
+        
+        # Category ID <--> name
+        self.cat_id_to_name = {cat['id']: cat['name'] for cat in self.db['categories']}
+        self.cat_name_to_id = {cat['name']: cat['id'] for cat in self.db['categories']}
+        
+        # Image filename --> ID
+        self.filename_to_id = {im['file_name']: im['id'] for im in self.db['images']}
+        
+        # Each image can potentially multiple annotations, hence using lists
+        self.image_id_to_annotations = defaultdict(list)
+        
+        # Image ID --> image object
+        self.image_id_to_image = {im['id'] : im for im in self.db['images']}
+        
+        # Image ID --> annotations
+        for ann in self.db['annotations']:
+            self.image_id_to_annotations[ann['image_id']].append(ann)
+            
+            
+def mark_detection_status(indexed_db,negative_classes=['empty']):
+    """
+    For each image in indexed_db.db['images'], add a '_detection_status' field
+    to indicate whether to treat this image as positive, negative, ambiguous,
+    or unknown.
+    
+    Makes modifications in-place.
+    
+    returns (nNegative,nPositive,nUnknown,nAmbiguous)
+    """
+          
+    nUnknown = 0
+    nAmbiguous = 0
+    nPositive = 0
+    nNegative = 0
+ 
+    db = indexed_db.db
+    for im in db['images']:
+        
+        image_id = im['id']
+        annotations = indexed_db.image_id_to_annotations[image_id]
+        image_categories = [ann['category_id'] for ann in annotations]
+        
+        image_status = DetectionStatus.DS_UNKNOWN
+        
+        if len(image_categories) == 0:
+            
+            image_status = DetectionStatus.DS_UNKNOWN
+            
+        else:            
+            
+            for cat_id in image_categories:
+                
+                cat_name = indexed_db.cat_id_to_name[cat_id]            
+                
+                if cat_name in negative_classes:                    
+                    if image_status == DetectionStatus.DS_UNKNOWN:                        
+                        image_status = DetectionStatus.DS_NEGATIVE
+                    elif image_status == DetectionStatus.DS_POSITIVE:
+                        image_status = DetectionStatus.DS_AMBIGUOUS                    
+                else:                    
+                    if image_status == DetectionStatus.DS_UNKNOWN:                        
+                        image_status = DetectionStatus.DS_POSITIVE
+                    elif image_status == DetectionStatus.DS_NEGATIVE:
+                        image_status = DetectionStatus.DS_AMBIGUOUS
+        
+        if image_status == DetectionStatus.DS_NEGATIVE:
+            nNegative += 1
+        elif image_status == DetectionStatus.DS_POSITIVE:
+            nPositive += 1
+        elif image_status == DetectionStatus.DS_UNKNOWN:
+            nUnknown += 1
+        elif image_status == DetectionStatus.DS_AMBIGUOUS:
+            nAmbiguous += 1
+
+    return (nNegative,nPositive,nUnknown,nAmbiguous)
+
 
 #%% Load ground truth if available
 
-ground_truth_db = None
+ground_truth_indexed_db = None
 
 if len(ground_truth_json_file) > 0:
         
-    ground_truth_db = json.load(open(ground_truth_json_file))
+    ground_truth_indexed_db = IndexedJsonDb(ground_truth_json_file,True)
     
-    # Normalize paths to simplify comparisons later
-    for im in ground_truth_db['images']:
-        im['file_name'] = os.path.normpath(im['file_name'])
-    
-    ### Build useful mappings to facilitate working with the DB
-    
-    # Category ID <--> name
-    ground_truth_cat_id_to_name = {cat['id']: cat['name'] for cat in ground_truth_db['categories']}
-    ground_truth_name_to_cat_id = {cat['name']: cat['id'] for cat in ground_truth_db['categories']}
-    
-    # Image filename --> ID
-    ground_trith_filename_to_id = {im['file_name']: im['id'] for im in ground_truth_db['images']}
-    
-    ground_truth_image_id_to_category_ids = defaultdict(list)  # each image could potentially have multiple species, hence using lists
-
-    # Image ID --> categories
-    for ann in ground_truth_db['annotations']:
-        ground_truth_image_id_to_category_ids[ann['image_id']].append(ann['category_id'])
-
-    print('Finished loading and indexing ground truth')
+    # Mark images in the ground truth as positive or negative
+    (nNegative,nPositive,nUnknown,nAmbiguous) = mark_detection_status(ground_truth_indexed_db,
+        negative_classes)
+    print('Finished loading and indexing ground truth: {} negative, {} positive, {} unknown, {} ambiguous'.format(
+            nNegative,nPositive,nUnknown,nAmbiguous))
 
 
 #%% Load detection results
@@ -72,7 +183,10 @@ detection_results = pd.read_csv(detector_output_file)
 # Sanity-check that this is really a detector output file
 for s in ['image_path','max_confidence','detections']:
     assert s in detection_results.columns
-    
+
+# Normalize paths to simplify comparisons later
+detection_results['image_path'] = detection_results['image_path'].apply(os.path.normpath)
+
     
 #%% If ground truth is available, merge it into the detection results
 
