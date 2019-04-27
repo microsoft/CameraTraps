a0ff22d3622b09a9c7ecc0e5a3e669ec9aef0bb6
Dan Morris <dmorris@cs.stanford.edu>
2019-04-27 12:13:41 -0700

    Update postprocess_batch_results.py
    Former-commit-id: b05e3a7a5499a32a226bbcf736ecf8d965ca8cac

--- api/detector_batch_processing/postprocess_batch_results.py
+++ api/detector_batch_processing/postprocess_batch_results.py
@@ -5,14 +5,20 @@
 # Given a .csv file representing the output from the batch API, do one or more of 
 # the following:
 #
-# * Eliminate "suspicious detections", i.e. detections repeated numerous times with
-#   unrealistically limited movement
-#
 # * Evaluate detector precision/recall, optionally rendering results (requires ground truth)
 #
 # * Sample true/false positives/negatives and render to html (requires ground truth)
 #
 # * Sample detections/non-detections and render to html (when ground truth isn't available)
+#
+# Upcoming improvements:
+#
+# * Elimination of "suspicious detections", i.e. detections repeated numerous times with
+#   unrealistically limited movement
+# 
+# * Support for accessing blob storage directly (currently images are accessed by
+#   file paths, so images in Azure blobs should be accessed by mounting the 
+#   containers)
 #
 ########
 
