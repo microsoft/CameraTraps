9d5847650f322bda5f3de51f6373ae936789912e
Marcel Simon <a-msimon@microsoft.com>
2019-05-08 07:24:22 +0000

    Renamed output node of exported/frozen classification model
    Former-commit-id: d7f66c672d96cb755ecd2d14ccd0f0dfdf620aa1

--- classification/export_inference_graph_serengeti.sh
+++ classification/export_inference_graph_serengeti.sh
@@ -3,7 +3,6 @@ DATASET_DIR=/data/lila/serengeti/serengeti_cropped_tfrecords
 #CHECKPOINT_DIR=./$(echo log/20* | sort | tail -n 1)/all
 CHECKPOINT_DIR=/home/loris/git/tf-detection/CameraTraps/classification/log/serengeti_cropped_resnext_86.3/all
 MODEL_NAME=inception_v4
-OUTPUT_NODE_NAMES=InceptionV4/Logits/Predictions
 NUM_GPUS=1
 
 python ../export_inference_graph_with_preprocessing.py \
@@ -13,10 +12,10 @@ python ../export_inference_graph_with_preprocessing.py \
     --write_text_graphdef=True
 
 
-python ../freeze_and_optimize_graph.py \
+python ../freeze_graph.py \
     --input_graph=${CHECKPOINT_DIR}/${MODEL_NAME}_inf_graph_def.pbtxt \
     --input_checkpoint=`ls ${CHECKPOINT_DIR}/model.ckpt*meta | tail -n 1 | rev | cut -c 6- | rev` \
     --output_graph=${CHECKPOINT_DIR}/frozen_inference_graph.pb \
     --input_node_names=input \
-    --output_node_names=${OUTPUT_NODE_NAMES} \
+    --output_node_names=output \
     --clear_devices=True 
