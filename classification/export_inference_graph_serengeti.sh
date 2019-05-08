c9bc351708201907b36e846848f9d74a4a3d39d7
Marcel Simon <a-msimon@microsoft.com>
2019-05-08 03:20:10 +0000

    Added frozen graph export with preprocessing
    Former-commit-id: 23b1484cbc2b7c465726b3523e2c27ea46098be7

--- classification/export_inference_graph_serengeti.sh
+++ classification/export_inference_graph_serengeti.sh
@@ -6,14 +6,14 @@ MODEL_NAME=inception_v4
 OUTPUT_NODE_NAMES=InceptionV4/Logits/Predictions
 NUM_GPUS=1
 
-python export_inference_graph.py \
+python ../export_inference_graph_with_preprocessing.py \
     --model_name=${MODEL_NAME} \
     --output_file=${CHECKPOINT_DIR}/${MODEL_NAME}_inf_graph_def.pbtxt \
     --dataset_name=serengeti \
     --write_text_graphdef=True
 
 
-python freeze_and_optimize_graph.py \
+python ../freeze_and_optimize_graph.py \
     --input_graph=${CHECKPOINT_DIR}/${MODEL_NAME}_inf_graph_def.pbtxt \
     --input_checkpoint=`ls ${CHECKPOINT_DIR}/model.ckpt*meta | tail -n 1 | rev | cut -c 6- | rev` \
     --output_graph=${CHECKPOINT_DIR}/frozen_inference_graph.pb \
