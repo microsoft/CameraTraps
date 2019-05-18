e71f5036b0335949c06c437950e8d052bb8f26fb
Dan Morris <dmorris@cs.stanford.edu>
2019-05-18 13:29:43 -0700

    Allowed reading a subset of rows in load_api_results
    Former-commit-id: 937f1ba1d48b81ff41963adb96e1217fa25a3085

--- detection/detection_eval/load_api_results.py
+++ detection/detection_eval/load_api_results.py
@@ -18,11 +18,11 @@ headers = ['image_path','max_confidence','detections']
 
 #%% Functions
 
-def load_api_results(filename,normalize_paths=True,filename_replacements={}):
+def load_api_results(filename,normalize_paths=True,filename_replacements={},nrows=None):
     
     print('Loading API results from {}'.format(filename))
     
-    detection_results = pd.read_csv(filename)
+    detection_results = pd.read_csv(filename,nrows=nrows)
     
     print('De-serializing API results from {}'.format(filename))
     
