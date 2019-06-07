1b7fc671934de82c5767d9faee29e451b3bbc992
amritagupta <gupta.amrita20@gmail.com>
2019-06-07 17:45:59 +0000

    Added Image class for filling out image table in database
    Former-commit-id: dabf846244b0450ca153cf112e79dedd020c5fe9

--- research/active_learning/Database/DB_models.py
+++ research/active_learning/Database/DB_models.py
@@ -13,20 +13,39 @@ db_proxy = Proxy() # create a proxy backend for the database
 
 class Info(Model):
     '''
-    Class defining table containing information about the target dataset.
+    Table containing information about the target dataset.
     '''
     name = CharField() # name of dataset
-    description= CharField()
-    contributor= CharField()
-    version= IntegerField()
-    year= IntegerField()
+    description = CharField()
+    contributor = CharField()
+    version = IntegerField()
+    year = IntegerField()
     date_created = DateField()
-    RM= FloatField(null = True, default= -1) # mean in RGB channels(?)
-    GM= FloatField(null = True, default= -1)
-    BM= FloatField(null = True, default= -1)
-    RS= FloatField(null = True, default= -1) # standard deviation in RGB channels (?)
-    GS= FloatField(null = True, default= -1)
-    BS= FloatField(null = True, default= -1)
+    RM = FloatField(null = True, default= -1) # mean in RGB channels(?)
+    GM = FloatField(null = True, default= -1)
+    BM = FloatField(null = True, default= -1)
+    RS = FloatField(null = True, default= -1) # standard deviation in RGB channels (?)
+    GS = FloatField(null = True, default= -1)
+    BS = FloatField(null = True, default= -1)
 
+    class Meta:
+        database = db_proxy
+
+
+class Image(Model):
+    '''
+    Table containing information about each image in the target dataset.
+    '''
+    id = CharField(primary_key=True)
+    file_name = CharField()
+    #label= CharField()
+    width = IntegerField(null = True) # image dimensions in pixels
+    height = IntegerField(null= True)
+    location = CharField(null = True) # location of camera trap
+    datetime = DateTimeField(null = True)
+    seq_id = CharField(null= True) # sequence identifier
+    seq_num_frames = IntegerField(null = True) # number of frames in sequence
+    frame_num = IntegerField(null = True) # which frame number in sequence
+    
     class Meta:
         database = db_proxy
