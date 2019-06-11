e9571b01478c9f80689fe224331bd593faf0f609
Marcel Simon <a-msimon@microsoft.com>
2019-06-11 03:04:45 +0000

    Updated frozen graph name for classifier export
    Former-commit-id: eb8313093f402f9f59d6aac8b2092b4c9d9c268e

--- classification/export_inference_graph_serengeti.sh
+++ classification/export_inference_graph_serengeti.sh
@@ -14,7 +14,7 @@ python ../export_inference_graph_definition.py \
 python ../freeze_graph.py \
     --input_graph=${CHECKPOINT_DIR}/${MODEL_NAME}_inf_graph_def.pbtxt \
     --input_checkpoint=`ls ${CHECKPOINT_DIR}/model.ckpt*meta | tail -n 1 | rev | cut -c 6- | rev` \
-    --output_graph=${CHECKPOINT_DIR}/frozen_inference_graph_w_preprocessing.pb \
+    --output_graph=${CHECKPOINT_DIR}/${DATASET_NAME}_${MODEL_NAME}_$(date +"%Y_%m_%d")_w_preprocessing.pb \
     --input_node_names=input \
     --output_node_names=output \
     --clear_devices=True 
