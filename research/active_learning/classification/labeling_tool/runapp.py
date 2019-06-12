c31f5107bd42f9c48e3521cef21335104f2273ad
amritagupta <gupta.amrita20@gmail.com>
2019-06-12 16:26:59 +0000

    Starting to implement dynamic routing for loading images
    Former-commit-id: c694551fdf53ec6c61181433ddcfb92ba6514b4e

--- research/active_learning/classification/labeling_tool/runapp.py
+++ research/active_learning/classification/labeling_tool/runapp.py
@@ -3,35 +3,40 @@ runapp.py
 
 Starts running a web application for labeling samples.
 '''
+import json
+import bottle
 
-from bottle import Bottle, static_file, request
+webUIapp = bottle.Bottle()
 
-webUIapp = Bottle()
-
-class DBMiddleware():
-    def __init__(self, config):
-        self.config = config
-        self.dbConnector = Database(config)
+# class DBMiddleware():
+#     def __init__(self, config):
+#         self.config = config
+#         self.dbConnector = Database(config)
     
-    def getImageBatch(self, limit=None):
-        response = {}
+#     def getImageBatch(self, limit=None):
+#         response = {}
 
-        return response
+#         return response
 
 
-
+#--------static routings for the webUIapp bottle server--------#
 @webUIapp.route('/')
 def hello():
-    return(static_file("index.html", root='static/html'))
+    return(bottle.static_file("index.html", root='static/html'))
 
 @webUIapp.route('/<filename:re:.*>')
 def send_static(filename):
-    return static_file(filename, root='static')
+    return bottle.static_file(filename, root='static')
 
-@webUIapp.get('/getImageBatch')
-def get_image_batch():
-    postData = request.body.read()
-    dataIDs = postData['imageIDs'] #????
-    json = webUImiddleware.getImageBatch(dataIDs)
+#--------dynamic routings for the webUIapp bottle server--------#
+@webUIapp.post('/loadImages')
+def load_images():
+    bottle.response.content_type('application/json')
+    returned_images = bottle.request.json
+    
+    returned_images = [{"id": "testid", "name": "test name"}]
+    return json.dumps(returned_images)
+    # postData = request.body.read()
+    # dataIDs = postData['imageIDs'] #????
 
 webUIapp.run(host='localhost', port=8080)
