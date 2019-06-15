555b260649f6e88ffa98cec50021c88af9f6efc4
Siyu Yang <yasiyu@microsoft.com>
2019-06-15 01:17:39 -0700

    Update aggregate_results_manually.py to accommodate changes from Zooniverse feature additions.

--- api/batch_processing/api_support/aggregate_results_manually.py
+++ api/batch_processing/api_support/aggregate_results_manually.py
@@ -3,6 +3,8 @@
 # interrupted (uncaught exception or having to re-start the API container), we could manually
 # aggregate results from each shard using this script, assuming all jobs submitted to AML have finished.
 #
+# Call this script from the root of the repo.
+#
 # Need to have set environment variables STORAGE_ACCOUNT_NAME and STORAGE_ACCOUNT_KEY to those of the
 # storage account backing the API.
 #
@@ -13,20 +15,25 @@ import argparse
 import json
 import sys
 
+# for orchestrator.py imports to work properly... you may still need to manually change the imports in
+# orchestrator.py to be absolute imports, i.e. change `import api_config` to
+# `from api/batch_processing/api/orchestrator_api/api_config as api_config`
 sys.path.append('./api/detector_batch_processing/api/orchestrator_api')
 
-from api.detector_batch_processing.api.orchestrator_api.orchestrator import AMLMonitor
+from api.batch_processing.api.orchestrator_api.orchestrator import AMLMonitor
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('request_id', action='store', type=str,
-                        help='the request ID to restart monitoring')
+                        help='the request ID to retrieve results for')
+    parser.add_argument('--request_name', action='store', type=str, default='',
+                        help='an identifier for the output files; fewer than 32 characters with - and _ allowed')
     args = parser.parse_args()
 
     # list_jobs_submitted cannot be serialized ("can't pickle _thread.RLock objects "), but
     # do not need it for aggregating results
-    aml_monitor = AMLMonitor(args.request_id, None)
+    aml_monitor = AMLMonitor(args.request_id, None, args.request_name, '', '')  # won't provide request_submission_timestamp, model_version
     output_file_urls = aml_monitor.aggregate_results()
     output_file_urls_str = json.dumps(output_file_urls)
     print(output_file_urls_str)
