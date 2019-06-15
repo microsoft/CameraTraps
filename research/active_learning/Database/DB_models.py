f576616011b344d0d5879e1c6f365612057e605c
amritagupta <gupta.amrita20@gmail.com>
2019-06-14 17:58:01 -0700

    More accurate descriptions/understanding of what goes in each table
    Former-commit-id: b0a96444859405f59d4fca577f4903ec5f2b86fb

--- research/active_learning/Database/DB_models.py
+++ research/active_learning/Database/DB_models.py
@@ -6,9 +6,14 @@ Defines model classes representing database tables used in active learning for c
 '''
 
 from peewee import *
-
+from enum import Enum
 
 db_proxy = Proxy() # create a proxy backend for the database
+
+class DetectionKind(Enum):
+    ModelDetection = 0  # "output of the detection model"--i.e. a crop with no label? or a crop with a label guessed by the classifier?
+    ActiveDetection = 1 # "selected for labeling by the oracle"--i.e. a crop currently flagged as needing a label?
+    UserDetection = 2   # label has been assigned by the user
 
 
 class Info(Model):
@@ -34,18 +39,21 @@ class Info(Model):
 
 class Image(Model):
     '''
-    Table containing information about each image in the dataset.
+    Table containing information about each cropped image in the dataset.
     '''
-    id = CharField(primary_key=True)
-    file_name = CharField()
-    #label= CharField()
-    width = IntegerField(null = True) # image dimensions in pixels
+    id = CharField(primary_key=True)    # cropped image unique identifier
+    file_name = CharField()             # cropped image file name
+    width = IntegerField(null = True)   # cropped image dimensions in pixels
     height = IntegerField(null= True)
-    location = CharField(null = True) # location of camera trap
+    grayscale = BooleanField(null = True)   # whether the cropped image is grayscale
+    relative_size = FloatField(null = True) # cropped image size relative to original image size   
+
+    source_file_name = CharField()      # full image file name from which cropped image was generated
+    seq_id = CharField(null= True)      # sequence identifier for the full image
+    seq_num_frames = IntegerField(null = True)  # number of frames in sequence
+    frame_num = IntegerField(null = True)       # which frame number in sequence
+    location = CharField(null = True)   # location of camera trap
     datetime = DateTimeField(null = True)
-    seq_id = CharField(null= True) # sequence identifier
-    seq_num_frames = IntegerField(null = True) # number of frames in sequence
-    frame_num = IntegerField(null = True) # which frame number in sequence
     
     class Meta:
         database = db_proxy
@@ -64,15 +72,17 @@ class Category(Model):
 
 class Detection(Model):
     '''
-    Table containing information about each crop in the pretrain dataset.
+    Table containing information about detections (annotations) in the dataset.
     '''
-    id = CharField(primary_key=True)
-    image = ForeignKeyField(Image) # path specifying which image the crop was generated from
-    kind = IntegerField() # numeric code representing ???
-    category = ForeignKeyField(Category) # image dimensions in pixels
-    category_confidence = FloatField(null= True)
-    grayscale = BooleanField(null = True)
-    relative_size = FloatField(null = True) # crop area relative to original image area    
+    id = CharField(primary_key=True)    # detection unique identifier
+    image = ForeignKeyField(Image)      # pointer to cropped image the detection corresponds to
+    kind = IntegerField()               # numeric code representing what kind of detection this is
+    category = ForeignKeyField(Category)    # label assigned to the detection
+    category_confidence = FloatField(null= True)    # confidence associated with the detection
+
+    class Meta:
+        database = db_proxy
+     
     
 
 class Oracle(Model):
