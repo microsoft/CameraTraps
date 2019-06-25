b827040e5b3f1c3943fb6730f225df9d5fe6be0b
amritagupta <gupta.amrita20@gmail.com>
2019-06-25 07:42:19 +0000

    Added tables for Oracle, Detection
    Former-commit-id: 8601e136301ac0c32c6d7e02b5c0aa66d012e442

--- research/active_learning/Database/initialize_pretrain_db.py
+++ research/active_learning/Database/initialize_pretrain_db.py
@@ -1,8 +1,12 @@
 '''
 initialize_pretrain_db.py
 
+(Largely draws from CameraTraps/research/active_learning/import_folder.py)
+
 Creates a PostgreSQL database of camera trap images for use in active learning for classification. The script assumes that the images have been
-used to prepare a classification dataset (i.e. the images have been cropped and organized into subfolders named by class)
+used to prepare a classification dataset (i.e. the images have been cropped and organized into subfolders named by class).
+
+
 a COCO JSON file is available for the dataset.
 #  TODO update: Assumes that crops have already
 # been generated for the images using make_active_learning_classification_dataset.py. The created DB contains tables:
@@ -12,7 +16,7 @@ a COCO JSON file is available for the dataset.
 
 '''
 
-import argparse, glob, json, os, PIL.Image, psycopg2, sys, uuid
+import argparse, glob, json, os, PIL.Image, psycopg2, sys, time, uuid
 import numpy as np
 from datetime import datetime
 from peewee import *
@@ -25,8 +29,12 @@ parser.add_argument('--db_user', default='new_user', type=str,
                     help='Name of the user accessing the Postgres DB.')
 parser.add_argument('--db_password', default='new_user_password', type=str,
                     help='Password of the user accessing the Postgres DB.')
+parser.add_argument('--crop_dir', metavar='DIR', required=True,
+                    help='Path to dataset directory containing all cropped images')
 parser.add_argument('--image_dir', metavar='DIR',
-                    help='Path to dataset directory containing all images')
+                    help='Path to dataset directory containing all original images')
+parser.add_argument('--verbose', default=False, type=bool,
+                    help='Output print messages while running')
 parser.add_argument('--coco_json', metavar='DIR',
                     help='Path to COCO Camera Traps json file if available', default=None)
 args = parser.parse_args()
@@ -53,52 +61,101 @@ pretrain_db.create_tables([Info, Category, Image, Detection, Oracle])
 
 
 # Populate Tables
-## get class names
+
+## create Info table
+info_name = args.crop_dir
+info_desc = 'Active learning for classification database of cropped images used to train embedding model (pretraining dataset).'
+info_contrib = 'Amrita'
+info_version = 0
+info_year = 2019
+info_date = datetime.today().date()
+existing_info_entries = Info.select().where(Info.name == info_name)
+try:
+    existing_info_entry = existing_info_entries.get()
+except:
+    info_entry = Info.create(name = info_name, description = info_desc, contributor = info_contrib, version = info_version, year = info_year, date_created = info_date)
+    info_entry.save()
+
+## get class names for Category table
 if sys.version_info >= (3, 5):
     # Faster anD available in Python 3.5 and above
-    classes = [d.name for d in os.scandir(args.image_dir) if d.is_dir()]
+    classes = [d.name for d in os.scandir(args.crop_dir) if d.is_dir()]
 else:
-    classes = [d for d in os.listdir(args.image_dir) if os.path.isdir(os.path.join(args.image_dir, d))]
+    classes = [d for d in os.listdir(args.crop_dir) if os.path.isdir(os.path.join(args.crop_dir, d))]
 classes.sort()
 class_to_idx = {classes[i]: i for i in range(len(classes))}
 
 ## iterate through images in each class folder
 for cat in sorted(class_to_idx.keys()):
+    if args.verbose:
+        print("Start processing "+cat)
+        timer = time.time()
+
     existing_cat_entries = Category.select().where((Category.name == cat))
     try:
         existing_cat_entry = existing_cat_entries.get()
     except:
-        category = Category.create(id = class_to_idx[cat], name = cat)
-        category.save()
+        category_entry = Category.create(id = class_to_idx[cat], name = cat)
+        category_entry.save()
     
-    image_list = []
-    for root, _, fnames in sorted(os.walk(os.path.join(args.image_dir, cat))):
+    for root, _, fnames in sorted(os.walk(os.path.join(args.crop_dir, cat))):
         for i, fname in enumerate(sorted(fnames)):
             if fname.endswith(".JPG"):
-                image_uid = str(uuid.uuid4())
-                image_fname = os.path.join(root, fname)
-                image = np.array(PIL.Image.open(os.path.join(root, fname)))
-                if image.dtype != np.uint8:
+                ## get cropped image data for Image table
+                img_uid = str(uuid.uuid4())
+                img_fname = os.path.join(root, fname)
+                img = np.array(PIL.Image.open(os.path.join(root, fname)))
+                if img.dtype != np.uint8:
                     print('Failed to load image ' + fname)
                     continue
-                img_width = image.shape[1]
-                img_height = image.shape[0]
+                img_width = img.shape[1]
+                img_height = img.shape[0]
                 # if mean of each channel is about the same, image is likely grayscale
-                if (abs(np.mean(image[:,:,0]) - np.mean(image[:,:,1])) < 1e-1) & (abs(np.mean(image[:,:,1]) - np.mean(image[:,:,2])) < 1e-1):
+                if (abs(np.mean(img[:,:,0]) - np.mean(img[:,:,1])) < 1e-1) & (abs(np.mean(img[:,:,1]) - np.mean(img[:,:,2])) < 1e-1):
                     img_grayscale = True
                 else:
                     img_grayscale = False
-                image_list.append((image_uid, image_fname, img_width, img_height, img_grayscale))
-                
-                # still have no info on these:
-                # relative_size = FloatField(null = True) # cropped image size relative to original image size
-                # source_file_name = CharField()      # full image file name from which cropped image was generated
-                # seq_id = CharField(null= True)      # sequence identifier for the full image
-                # seq_num_frames = IntegerField(null = True)  # number of frames in sequence
-                # frame_num = IntegerField(null = True)       # which frame number in sequence
-                # location = CharField(null = True)   # location of camera trap
+                if len(fname.split('.JPG')[-2].split('_')) > 1:
+                    img_source_fname = os.path.join(args.image_dir, os.path.basename(root), fname.split('.JPG')[-2].split('_')[0]+'.JPG')
+                else:
+                    img_source_fname = os.path.join(args.image_dir, os.path.basename(root), fname)
+
+                orig_img = np.array(PIL.Image.open(img_source_fname))
+                orig_img_width = orig_img.shape[1]
+                orig_img_height = orig_img.shape[0]
+                img_relative_size = (img_height*img_width)/float(orig_img_height*orig_img_width)
+                ## still have no info on these:
+                # seq_id = CharField(null= True)                # sequence identifier for the original image
+                # seq_num_frames = IntegerField(null = True)    # number of frames in sequence
+                # frame_num = IntegerField(null = True)         # which frame number in sequence
+                # location = CharField(null = True)             # location of camera trap
                 # datetime = DateTimeField(null = True)
+                
+                existing_image_entries = Image.select().where((Image.file_name == img_fname) & (Image.width == img_width) & (Image.height == img_height))
+                try:
+                    existing_image_entry = existing_image_entries.get()
+                except:
+                    image_entry = Image.create(id = img_uid, file_name = img_fname, width = img_width, height = img_height, grayscale = img_grayscale,
+                                                source_file_name = img_source_fname, relative_size = img_relative_size)
+                    image_entry.save()
+
+                    ## store info about the detection corresponding to this image
+                    detection_uid = str(uuid.uuid4())
+                    detection_img = img_uid
+                    detection_kind = DetectionKind.UserDetection.value  # pretrain dataset has user-provided annotations; ALTHOUGH crops were actually produced using detector...
+                    detection_cat = class_to_idx[cat]
+                    detection_cat_conf = 1
+                    detection_entry = Detection.create(id = detection_uid, image = detection_img, kind = detection_kind, category = detection_cat, category_confidence = detection_cat_conf)
+                    detection_entry.save()
+
+                    ## store info about the true labels for the detection
+                    ##  - for pretrain dataset this is the same as the detection_category if the detection categories
+                    oracle_entry = Oracle.create(detection = detection_uid, label = detection_cat)
+                
+            if i%10==0 and i>0 and args.verbose:
+                print('Processed %d files in %0.2f seconds'%(i, time.time() - timer))
+                
                     
                 
 
-print(classes)
+# print(classes)
