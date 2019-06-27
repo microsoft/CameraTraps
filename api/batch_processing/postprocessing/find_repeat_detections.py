d9ec252a63651bd889f80da176a57f260b837deb
Dan Morris <dmorris@cs.stanford.edu>
2019-06-27 08:58:02 -0700

    path fixes in find_repeat_detections
    Former-commit-id: 1dd656c88b9a5f93f4b6d350778cda23b9497cea

--- api/batch_processing/postprocessing/find_repeat_detections.py
+++ api/batch_processing/postprocessing/find_repeat_detections.py
@@ -33,7 +33,7 @@ from tqdm import tqdm
 # from ai4eutils; this is assumed to be on the path, as per repo convention
 import write_html_image_list
 
-from api.batch_processing.load_api_results import load_api_results, write_api_results
+from api.batch_processing.postprocessing.load_api_results import load_api_results, write_api_results
 
 # Imports I'm not using but use when I tinker with parallelization
 #
