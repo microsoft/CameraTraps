3f02249c6f445f59c70dd01cfffa88c3be3b891c
amritagupta <gupta.amrita20@gmail.com>
2019-07-03 03:10:54 +0000

    Modifying sql_data_loader to get it working for training classifier: IN PROGRESS
    Former-commit-id: f292e71cfa8d1350d0620de5bf2b1e785da16b0f

--- research/active_learning/DL/sqlite_data_loader.py
+++ research/active_learning/DL/sqlite_data_loader.py
@@ -3,160 +3,157 @@ from torchvision.transforms import *
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import BatchSampler
 
+import os, random, sys
 import numpy as np
-import os
-import sys
-import random
 from PIL import Image as PILImage
 from PIL import ImageStat
 from peewee import *
-from UIComponents.DBObjects import *
+from Database.DB_models import *
+# from UIComponents.DBObjects import *
 from .Engine import Engine
 
 class SQLDataLoader(Dataset):
-
-  def __init__(self, img_base, query = None, is_training= False, embedding_mode = False, kind = DetectionKind.ModelDetection.value, model = None, num_workers=8, raw_size=[256,256], processed_size=[224,224], limit = 5000000):
-      self.is_training= is_training
-      self.embedding= embedding_mode
-      self.num_workers = num_workers
-      self.img_base = img_base
-      if query is None:
-          self.query = Detection.select(Detection.id,Oracle.label,Detection.kind).join(Oracle).order_by(fn.random()).limit(limit)
-              #([0, 1, 4, 7,90, 1, 3, 5, 6, 8, 14, 20, 23, 27, 30, 33, 35, 36, 37, 39, 41, 43, 44, 47])).order_by(fn.random()).limit(30000)
-      else:
-          self.query = query
-      self.set_indices = [[],[],[]]
-      self.refresh(self.query)
-      self.kind = kind
-      for i, s in enumerate(self.samples):
-          self.set_indices[s[2]].append(i)
-      print([len(x) for x in self.set_indices])
-      self.current_set = self.set_indices[kind]
-      mean, std= self.get_mean_std()
-      self.train_transform = transforms.Compose([Resize(raw_size), RandomCrop(processed_size), RandomHorizontalFlip(), ColorJitter(), RandomRotation(20), ToTensor(), Normalize(mean, std)])
-      #self.train_transform = transforms.Compose([Resize(raw_size), CenterCrop((processed_size)), ToTensor(), Normalize(mean, std)])
-      self.eval_transform = transforms.Compose([Resize(raw_size), CenterCrop((processed_size)), ToTensor(), Normalize(mean, std)])
-      if model is not None:
-          self.updateEmbedding(model)
-
-
-  def train(self):
-      self.is_training = True
-  
-  def eval(self):
-      self.is_training = False
-
-  def embedding_mode(self):
-      self.embedding = True
-  
-  def image_mode(self):
-      self.embedding = False
-
-  def setKind(self, kind):
-      self.current_set = self.set_indices[kind]
-
-  def refresh(self,query):
-      print('Reading database')
-      self.samples= list(query.tuples())
-
-  def updateEmbedding(self, model):
-      print('Extracting embedding from the provided model ...')
-      self.model = model
-      e = Engine(model, None, None, verbose = True, print_freq = 10)
-      temp = self.is_training
-      self.current_set= list(range(len(self.samples)))#[item for sublist in self.set_indices for item in sublist]
-      print( len(self.current_set))
-      self.eval()
-      self.em = e.embedding(self.getSingleLoader(batch_size = 1024))
-      self.is_training = temp
-      self.setKind(self.kind)
-      print('Embedding extraction is done.')
-      print(len(self.current_set))
-
-  def get_mean_std(self):
-      info= Info.get()
-      print(info)
-      if info.RM == -1 and info.RS == -1: 
-          print("Calculating Mean and Std")
-          means = np.zeros((3))
-          stds = np.zeros((3))
-          sample_size= min(len(self.samples), 10000)
-          for i in range(sample_size):
-              img = self.loader(random.choice(self.samples)[0])
-              stat = ImageStat.Stat(img)
-              means+= np.array(stat.mean)/255.0
-              stds+= np.array(stat.stddev)/255.0
-          means= means/sample_size
-          stds= stds/sample_size
-          info.RM, info.GM, info.BM= means
-          info.RS, info.GS, info.BS= stds
-          info.save()
-      else:
-          print("Load Mean and Std from database")
-          means= [info.RM, info.GM, info.BM]
-          stds=  [info.RS, info.GS, info.BS]
-          print(means,stds)
-
-      return means, stds
-
-  def __len__(self):
-      return len(self.current_set)
-
-  def loader(self,path):
-      return PILImage.open(os.path.join(self.img_base,path+".JPG")).convert('RGB')
-      #return PILImage.open(os.path.join(self.img_base,path)).convert('RGB')
-
-  def __getitem__(self, idx):
-    """
-      Args:
-        index (int): Index
-      Returns:
-        tuple: (sample, target) where target is class_index of the target class.
-    """
-    index = self.current_set[idx]
-    path = self.samples[index][0]
-    target = self.samples[index][1]
-    if not self.embedding:
-        sample = self.loader(path)
-        if self.is_training:
-            sample = self.train_transform(sample)
+    def __init__(self, img_base, query=None, is_training=False, embedding_mode=False, kind=DetectionKind.ModelDetection.value, model=None, num_workers=8, raw_size=[256,256], processed_size=[224,224], limit=5000000):
+        self.is_training = is_training
+        self.embedding = embedding_mode
+        self.num_workers = num_workers
+        self.img_base = img_base
+        if query is None:
+            self.query = Detection.select(Detection.id,Oracle.label,Detection.kind).join(Oracle).order_by(fn.random()).limit(limit)
         else:
-            sample = self.eval_transform(sample)
-        return sample, target, path
-    else:
-        return self.em[index], target, path
+            self.query = query
+        self.set_indices = [[],[],[],[]]
+        self.refresh(self.query)
+        self.kind = kind
+        for i, s in enumerate(self.samples):
+            self.set_indices[s[2]].append(i)
+        print([len(x) for x in self.set_indices])
+        self.current_set = self.set_indices[kind]
+        mean, std = self.get_mean_std()
+        self.train_transform = transforms.Compose([Resize(raw_size), RandomCrop(processed_size), RandomHorizontalFlip(), ColorJitter(), RandomRotation(20), ToTensor(), Normalize(mean, std)])
+        #self.train_transform = transforms.Compose([Resize(raw_size), CenterCrop((processed_size)), ToTensor(), Normalize(mean, std)])
+        self.eval_transform = transforms.Compose([Resize(raw_size), CenterCrop((processed_size)), ToTensor(), Normalize(mean, std)])
+        if model is not None:
+            self.updateEmbedding(model)
+
+
+    def train(self):
+        self.is_training = True
   
-  def getpaths(self):
-      return [os.path.join(self.img_base,self.samples[i][0]+".JPG") for i in self.current_set]
+    def eval(self):
+        self.is_training = False
 
-  def getIDs(self):
-      return [self.samples[i][0] for i in self.current_set]
+    def embedding_mode(self):
+        self.embedding = True
+
+    def image_mode(self):
+        self.embedding = False
+
+    def setKind(self, kind):
+        self.current_set = self.set_indices[kind]
+
+    def refresh(self,query):
+        print('Reading database')
+        self.samples= list(query.tuples())
+
+    def updateEmbedding(self, model):
+        print('Extracting embedding from the provided model ...')
+        self.model = model
+        e = Engine(model, None, None, verbose = True, print_freq = 10)
+        temp = self.is_training
+        self.current_set= list(range(len(self.samples)))#[item for sublist in self.set_indices for item in sublist]
+        print( len(self.current_set))
+        self.eval()
+        self.em = e.embedding(self.getSingleLoader(batch_size = 1024))
+        self.is_training = temp
+        self.setKind(self.kind)
+        print('Embedding extraction is done.')
+        print(len(self.current_set))
+
+    def get_mean_std(self):
+        info= Info.get()
+        print(info)
+        if info.RM == -1 and info.RS == -1: 
+            print("Calculating Mean and Std")
+            means = np.zeros((3))
+            stds = np.zeros((3))
+            sample_size= min(len(self.samples), 10000)
+            for i in range(sample_size):
+                img = self.loader(random.choice(self.samples)[0])
+                stat = ImageStat.Stat(img)
+                means+= np.array(stat.mean)/255.0
+                stds+= np.array(stat.stddev)/255.0
+            means= means/sample_size
+            stds= stds/sample_size
+            info.RM, info.GM, info.BM= means
+            info.RS, info.GS, info.BS= stds
+            info.save()
+        else:
+            print("Load Mean and Std from database")
+            means= [info.RM, info.GM, info.BM]
+            stds=  [info.RS, info.GS, info.BS]
+            print(means,stds)
+
+        return means, stds
+
+    def __len__(self):
+        return len(self.current_set)
+
+    def loader(self,path):
+        return PILImage.open(os.path.join(self.img_base,path+".JPG")).convert('RGB')
+        #return PILImage.open(os.path.join(self.img_base,path)).convert('RGB')
+
+    def __getitem__(self, idx):
+        """
+        Args:
+        index (int): Index
+        Returns:
+        tuple: (sample, target) where target is class_index of the target class.
+        """
+        index = self.current_set[idx]
+        path = self.samples[index][0]
+        target = self.samples[index][1]
+        if not self.embedding:
+            sample = self.loader(path)
+            if self.is_training:
+                sample = self.train_transform(sample)
+            else:
+                sample = self.eval_transform(sample)
+            return sample, target, path
+        else:
+            return self.em[index], target, path
   
-  def getallIDs(self):
-      return [self.samples[i][0] for i in range(len(self.samples))]
+    def getpaths(self):
+        return [os.path.join(self.img_base,self.samples[i][0]+".JPG") for i in self.current_set]
 
-  def getlabels(self):
-      return [self.samples[i][1] for i in self.current_set]
+    def getIDs(self):
+        return [self.samples[i][0] for i in self.current_set]
 
-  def getalllabels(self):
-      return [self.samples[i][1] for i in range(len(self.samples))]
+    def getallIDs(self):
+        return [self.samples[i][0] for i in range(len(self.samples))]
 
-  def writeback(self):
-      for i, l in enumerate(self.set_indices):
-          query= Detection.update(kind = i).where(Detection.id.in_(rList))  
-          query.execute()
+    def getlabels(self):
+        return [self.samples[i][1] for i in self.current_set]
 
-  def getClassesInfo(self):
-    return list(Category.select())
+    def getalllabels(self):
+        return [self.samples[i][1] for i in range(len(self.samples))]
 
-  def getBalancedLoader(self, P=14, K=10):
-    train_batch_sampler = BalancedBatchSampler(self, n_classes=P, n_samples=K)
-    return DataLoader(self, batch_sampler=train_batch_sampler, num_workers= self.num_workers)
+    def writeback(self):
+        for i, l in enumerate(self.set_indices):
+            query= Detection.update(kind = i).where(Detection.id.in_(rList))  
+            query.execute()
 
-  def getSingleLoader(self, batch_size = -1):
-      if batch_size == -1:
-          batch_size = 128 if self.is_training else 256
-      return DataLoader(self, batch_size= batch_size, shuffle= self.is_training, num_workers= self.num_workers)
+    def getClassesInfo(self):
+        return list(Category.select())
+
+    def getBalancedLoader(self, P=14, K=10):
+        train_batch_sampler = BalancedBatchSampler(self, n_classes=P, n_samples=K)
+        return DataLoader(self, batch_sampler=train_batch_sampler, num_workers= self.num_workers)
+
+    def getSingleLoader(self, batch_size = -1):
+        if batch_size == -1:
+            batch_size = 128 if self.is_training else 256
+        return DataLoader(self, batch_size= batch_size, shuffle= self.is_training, num_workers= self.num_workers)
 
 
 class BalancedBatchSampler(BatchSampler):
--- research/active_learning/run.py
+++ research/active_learning/run.py
@@ -1,19 +1,5 @@
-import argparse
-import os
-import random
-import time
-import sys
-import warnings
+import argparse, os, random, sys, time, warnings
 from shutil import copy
-
-import torch
-import torch.nn as nn
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.distributed as dist
-import torch.optim
-import torch.utils.data
-import torch.utils.data.distributed
 
 import numpy as np
 np.set_printoptions(threshold=np.inf)
@@ -26,16 +12,28 @@ from sklearn.cluster import DBSCAN, MiniBatchKMeans
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils.extmath import softmax
 
-from DL.sqlite_data_loader import SQLDataLoader
-from DL.losses import *
-from DL.utils import *
-from DL.networks import *
-from DL.Engine import Engine
-from UIComponents.DBObjects import *
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
 
 from sampling_methods.constants import get_AL_sampler
 from sampling_methods.constants import get_wrapper_AL_mapping
 get_wrapper_AL_mapping()
+
+from Database.DB_models import *
+
+from DL.networks import *
+from DL.sqlite_data_loader import SQLDataLoader
+from DL.losses import *
+from DL.utils import *
+from DL.Engine import Engine
+# from UIComponents.DBObjects import *
+
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -59,17 +57,17 @@ model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
 
-parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-parser.add_argument('--run_data', metavar='DIR',
-                    help='path to train dataset', default='../../NACTI_crops')
+parser = argparse.ArgumentParser(description='PyTorch Classifier Training with Active Learning')
+parser.add_argument('--db_name', default='missouricameratraps', type=str, help='Name of the training (target) data Postgres DB.')
+parser.add_argument('--db_user', default='new_user', type=str, help='Name of the user accessing the Postgres DB.')
+parser.add_argument('--db_password', default='new_user_password', type=str, help='Password of the user accessing the Postgres DB.')
+parser.add_argument('--base_model', default='', type=str, metavar='PATH', help='Path to latest checkpoint for embedding model (default: none).')
+parser.add_argument('--experiment_name', default='', type=str, metavar='PATH', help='Prefix name for output files.')
+parser.add_argument('--crop_dir', default='', type=str, metavar='PATH', help='Path to cropped image files.')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('-b', '--batch_size', default=256, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--base_model', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('--experiment_name', default='', type=str, metavar='PATH',
-                    help='prefix name for output files')
 parser.add_argument('-N', '--active_batch', default=100, type=int,
                     help='number of queries per batch')
 parser.add_argument('-A', '--active_budget', default= 25000, type=int,
@@ -78,16 +76,16 @@ parser.add_argument('--K', default=5, type=int,
                     help='number of clusters')
 parser.add_argument('--strategy', default='confidence', type=str,
                     help='Active learning strategy')
-parser.add_argument('--limit', default=500000, type=int,
-                    help='number of records to read')
+parser.add_argument('--limit', default=500, type=int,
+                    help='Number of records to read.')
 parser.add_argument('--num_classes', default=50, type=int,
                     help='number of species')
 
 
 def moveRecords(dataset, srcKind, destKind, rList):
-      for e in rList:
-          dataset.set_indices[srcKind].remove(e)
-          dataset.set_indices[destKind].append(e)
+    for e in rList:
+        dataset.set_indices[srcKind].remove(e)
+        dataset.set_indices[destKind].append(e)
 
 def finetune_embedding(model, loss_type, train_dataset, P, K, epochs):
     train_dataset.image_mode()
@@ -109,16 +107,29 @@ def finetune_embedding(model, loss_type, train_dataset, P, K, epochs):
 
 def main():
     args = parser.parse_args()
-    print(args)
-    print("DB Connect")
-    db_path = os.path.join(args.run_data, os.path.basename(args.run_data)) + ".db"
-    print(db_path)
-    db = SqliteDatabase(db_path)
-    proxy.initialize(db)
-    db.connect()
-    print("connected")
-    print("CompleteLoop")
     
+    # Initialize Database
+    ## database connection credentials
+    DB_NAME = args.db_name
+    USER = args.db_user
+    PASSWORD = args.db_password
+    print("DB Connect")
+    ## try to connect as USER to database DB_NAME through peewee
+    target_db = PostgresqlDatabase(DB_NAME, user=USER, password=PASSWORD, host='localhost')
+    target_db.connect(reuse_if_open=True)
+    db_proxy.initialize(target_db)
+    # print(os.path.join(args.run_data, os.path.basename(args.run_data)) + ".db")
+    # db_path = os.path.join(args.run_data, os.path.basename(args.run_data)) + ".db"
+    # print(db_path)
+    # db = SqliteDatabase(db_path)
+    # proxy.initialize(db)
+    # db.connect()
+    target_db.close()
+    print("connected")
+
+
+    print("CompleteLoop")
+    # Load the saved embedding model
     checkpoint = load_checkpoint(args.base_model)
     if args.experiment_name == '':
         args.experiment_name = "experiment_%s_%s"%(checkpoint['loss_type'], args.strategy)
@@ -132,8 +143,10 @@ def main():
 
     model = torch.nn.DataParallel(embedding_net).cuda()
     model.load_state_dict(checkpoint['state_dict'])
-    dataset_query =  Detection.select(Detection.image_id,Oracle.label,Detection.kind).join(Oracle).order_by(fn.random()).limit(args.limit)
-    dataset = SQLDataLoader('/lscratch/datasets/serengeti/resized', query=dataset_query, is_training= False, kind = DetectionKind.ModelDetection.value, num_workers= 8, limit = args.limit)
+
+    # dataset_query = Detection.select().limit(5)
+    dataset_query =  Detection.select(Detection.image_id, Oracle.label, Detection.kind).join(Oracle).order_by(fn.random()).limit(args.limit) ## TODO: should this really be order_by random?
+    dataset = SQLDataLoader(args.crop_dir, query=dataset_query, is_training= False, kind = DetectionKind.ModelDetection.value, num_workers= 8, limit = args.limit)
     #dataset = SQLDataLoader(os.path.join(args.run_data, "crops"), is_training= False, kind = DetectionKind.ModelDetection.value, num_workers= 8, limit = args.limit)
     dataset.updateEmbedding(model)
     #print('Embedding Done')
@@ -146,76 +159,76 @@ def main():
     # Move Records
     #moveRecords(dataset, DetectionKind.ModelDetection.value, DetectionKind.UserDetection.value, random_ids)
 
-    #print([len(x) for x in dataset.set_indices])
-    # Finetune the embedding model
-    #dataset.setKind(DetectionKind.UserDetection.value)
-    #dataset.train()
-    #train_dataset = SQLDataLoader(trainset_query, os.path.join(args.run_data, 'crops'), is_training= True)
-    #finetune_embedding(model, checkpoint['loss_type'], dataset, 32, 4, 100)
-    #save_checkpoint({
-    #        'arch': model.arch,
-    #        'state_dict': model.state_dict(),
-    #        'optimizer' : optimizer.state_dict(),
-    #        'loss_type' : loss_type,
-    #        }, False, "%s%s_%s_%04d.tar"%('finetuned', loss_type, model.arch, len(dataset.set_indices[DetectionKind.UserDetection.value])))
+    # #print([len(x) for x in dataset.set_indices])
+    # # Finetune the embedding model
+    # #dataset.setKind(DetectionKind.UserDetection.value)
+    # #dataset.train()
+    # #train_dataset = SQLDataLoader(trainset_query, os.path.join(args.run_data, 'crops'), is_training= True)
+    # #finetune_embedding(model, checkpoint['loss_type'], dataset, 32, 4, 100)
+    # #save_checkpoint({
+    # #        'arch': model.arch,
+    # #        'state_dict': model.state_dict(),
+    # #        'optimizer' : optimizer.state_dict(),
+    # #        'loss_type' : loss_type,
+    # #        }, False, "%s%s_%s_%04d.tar"%('finetuned', loss_type, model.arch, len(dataset.set_indices[DetectionKind.UserDetection.value])))
 
-    #unlabeled_dataset.updateEmbedding(model)
-    #dataset.updateEmbedding(model)
-    #dataset.setKind(DetectionKind.UserDetection.value)
-    #print(dataset.em[dataset.current_set].shape, np.asarray(dataset.getlabels()).shape, len(dataset.getpaths()))
-    #plot_embedding( dataset.em[dataset.current_set], np.asarray(dataset.getlabels()) , dataset.getpaths(), {})
-    #plot_embedding( unlabeled_dataset.em, np.asarray(unlabeled_dataset.getlabels()) , unlabeled_dataset.getIDs(), {})
-    dataset.embedding_mode()
-    dataset.train()
-    sampler = get_AL_sampler(args.strategy)(dataset.em, dataset.getalllabels(), 12)
+    # #unlabeled_dataset.updateEmbedding(model)
+    # #dataset.updateEmbedding(model)
+    # #dataset.setKind(DetectionKind.UserDetection.value)
+    # #print(dataset.em[dataset.current_set].shape, np.asarray(dataset.getlabels()).shape, len(dataset.getpaths()))
+    # #plot_embedding( dataset.em[dataset.current_set], np.asarray(dataset.getlabels()) , dataset.getpaths(), {})
+    # #plot_embedding( unlabeled_dataset.em, np.asarray(unlabeled_dataset.getlabels()) , unlabeled_dataset.getIDs(), {})
+    # dataset.embedding_mode()
+    # dataset.train()
+    # sampler = get_AL_sampler(args.strategy)(dataset.em, dataset.getalllabels(), 12)
 
-    kwargs = {}
-    kwargs["N"] = args.active_batch
-    kwargs["already_selected"] = dataset.set_indices[DetectionKind.UserDetection.value]
-    kwargs["model"] = MLPClassifier(alpha=0.0001)
+    # kwargs = {}
+    # kwargs["N"] = args.active_batch
+    # kwargs["already_selected"] = dataset.set_indices[DetectionKind.UserDetection.value]
+    # kwargs["model"] = MLPClassifier(alpha=0.0001)
 
-    print("start the loop")
-    sys.stdout.flush()
-    numLabeled = len(dataset.set_indices[DetectionKind.UserDetection.value])
-    while numLabeled <= args.active_budget:
-        print([len(x) for x in dataset.set_indices])
-        sys.stdout.flush()
-        # Active Learning
-        if numLabeled == 0:
-            indices = np.random.choice(dataset.current_set, 1000, replace=False).tolist()
-        else:
-            indices = sampler.select_batch(**kwargs)
-        numLabeled = len(dataset.set_indices[DetectionKind.UserDetection.value])
-        #kwargs["already_selected"].extend(indices)
-        moveRecords(dataset, DetectionKind.ModelDetection.value, DetectionKind.UserDetection.value, indices)
+    # print("start the loop")
+    # sys.stdout.flush()
+    # numLabeled = len(dataset.set_indices[DetectionKind.UserDetection.value])
+    # while numLabeled <= args.active_budget:
+    #     print([len(x) for x in dataset.set_indices])
+    #     sys.stdout.flush()
+    #     # Active Learning
+    #     if numLabeled == 0:
+    #         indices = np.random.choice(dataset.current_set, 1000, replace=False).tolist()
+    #     else:
+    #         indices = sampler.select_batch(**kwargs)
+    #     numLabeled = len(dataset.set_indices[DetectionKind.UserDetection.value])
+    #     #kwargs["already_selected"].extend(indices)
+    #     moveRecords(dataset, DetectionKind.ModelDetection.value, DetectionKind.UserDetection.value, indices)
 
-        dataset.setKind(DetectionKind.UserDetection.value)
-        X_train= dataset.em[dataset.current_set]
-        y_train= np.asarray(dataset.getlabels())
+    #     dataset.setKind(DetectionKind.UserDetection.value)
+    #     X_train= dataset.em[dataset.current_set]
+    #     y_train= np.asarray(dataset.getlabels())
 
-        kwargs["model"].fit(X_train, y_train)
-        joblib.dump(kwargs["model"], "%s/%s_%04d.skmodel"%(args.experiment_name, 'embedding', numLabeled))
-        dataset.setKind(DetectionKind.ModelDetection.value)
-        dataset.embedding_mode()
-        X_test= dataset.em[dataset.current_set]
-        y_test= np.asarray(dataset.getlabels())
-        print("Accuracy",kwargs["model"].score(X_test, y_test))
-        sys.stdout.flush()
-        if numLabeled % 2000 == 1000:
-            dataset.setKind(DetectionKind.UserDetection.value)
-            finetune_embedding(model, checkpoint['loss_type'], dataset, 32, 4, 100 if numLabeled == 1000 else 50)
-            save_checkpoint({
-            'arch': checkpoint['arch'],
-            'state_dict': model.state_dict(),
-            #'optimizer' : optimizer.state_dict(),
-            'loss_type' : checkpoint['loss_type'],
-            'feat_dim' : checkpoint['feat_dim'],
-            'num_classes' : args.num_classes
-            }, False, "%s/%s%s_%s_%04d.tar"%(args.experiment_name, 'finetuned', checkpoint['loss_type'], checkpoint['arch'], numLabeled))
+    #     kwargs["model"].fit(X_train, y_train)
+    #     joblib.dump(kwargs["model"], "%s/%s_%04d.skmodel"%(args.experiment_name, 'embedding', numLabeled))
+    #     dataset.setKind(DetectionKind.ModelDetection.value)
+    #     dataset.embedding_mode()
+    #     X_test= dataset.em[dataset.current_set]
+    #     y_test= np.asarray(dataset.getlabels())
+    #     print("Accuracy",kwargs["model"].score(X_test, y_test))
+    #     sys.stdout.flush()
+    #     if numLabeled % 2000 == 1000:
+    #         dataset.setKind(DetectionKind.UserDetection.value)
+    #         finetune_embedding(model, checkpoint['loss_type'], dataset, 32, 4, 100 if numLabeled == 1000 else 50)
+    #         save_checkpoint({
+    #         'arch': checkpoint['arch'],
+    #         'state_dict': model.state_dict(),
+    #         #'optimizer' : optimizer.state_dict(),
+    #         'loss_type' : checkpoint['loss_type'],
+    #         'feat_dim' : checkpoint['feat_dim'],
+    #         'num_classes' : args.num_classes
+    #         }, False, "%s/%s%s_%s_%04d.tar"%(args.experiment_name, 'finetuned', checkpoint['loss_type'], checkpoint['arch'], numLabeled))
 
-            dataset.setKind(DetectionKind.ModelDetection.value)
-            dataset.updateEmbedding(model)
-            dataset.embedding_mode()
+    #         dataset.setKind(DetectionKind.ModelDetection.value)
+    #         dataset.updateEmbedding(model)
+    #         dataset.embedding_mode()
 
 if __name__ == '__main__':
     print("start")
