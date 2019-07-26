e071829e233c751530ae214873f24d64018843e5
amritagupta <gupta.amrita20@gmail.com>
2019-07-26 03:14:19 +0000

    Experiments code ongoing
    Former-commit-id: 5135394ddd93e219156780772ff02e93dd2ae141

--- research/active_learning/experiments/visualize_feature_maps.py
+++ research/active_learning/experiments/visualize_feature_maps.py
@@ -1,6 +1,8 @@
 import argparse, random, sys, time
 import PIL
 import torch
+import torchvision.models as models
+import matplotlib.pyplot as plt
 from torchvision.transforms import *
 
 sys.path.append("..")
@@ -9,13 +11,17 @@ from DL.networks import *
 from Database.DB_models import *
 from DL.sqlite_data_loader import SQLDataLoader
 
-class SaveFeatures():
-    def __init__(self, module):
-        self.hook = module.register_forward_hook(self.hook_fn)
-    def hook_fn(self, module, input, output):
-        self.features = torch.tensor(output, requires_grad=True).cuda()
-    def close(self):
-        self.hook.remove()
+# class SaveFeatures():
+#     def __init__(self, module):
+#         self.hook = module.register_forward_hook(self.hook_fn)
+#     def hook_fn(self, module, input, output):
+#         self.features = torch.tensor(output, requires_grad=True).cuda()
+#     def close(self):
+#         self.hook.remove()
+
+outputs = []
+def hook(module, input, output):
+    outputs.append(output)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -43,8 +49,8 @@ def main():
     model.load_state_dict(checkpoint['state_dict'])
     model.eval()
 
-    # model_inner_resnet = list(model.children())[0].inner_model
 
+    # Get a sample from the database, with eval transforms applied, etc.
     DB_NAME = args.db_name
     USER = args.db_user
     PASSWORD = args.db_password
@@ -57,13 +63,37 @@ def main():
     dataset = SQLDataLoader(args.crop_dir, query=dataset_query, is_training=False, kind=DetectionKind.ModelDetection.value, num_workers=8, limit=args.num)
     imagepaths = dataset.getallpaths()
 
-    sample_image_path = '0ef0f79f-7b58-473d-abbf-75bba59e834d'
+    sample_image_path = '0ca68a6f-6348-4456-8fb5-c067e2cbfe14'#'0a170ee9-166d-45df-8f45-b14550fc124e'#'43e3d2a6-38ea-4d17-a712-1b0feab92d58'#'0ef0f79f-7b58-473d-abbf-75bba59e834d'
     dataset.image_mode()
     sample_image = dataset.loader(sample_image_path)
+    sample_image.save('sample_image_for_activations.png')
     print(sample_image.size)
     sample_image = dataset.eval_transform(sample_image)
     print(sample_image.shape)
+
+    # output = model.forward(sample_image.unsqueeze(0))
+    # print(output)
+
+    model_inner_resnet = list(model.children())[0].inner_model
+    model_inner_resnet.eval()
+    model_inner_resnet.layer1[0].conv2.register_forward_hook(hook)
+
+    output = model.forward(sample_image.unsqueeze(0))
+    intermediate_output = outputs[0].cpu().detach().numpy()
+    print(intermediate_output.shape)
     
+    for i in range(intermediate_output.shape[1]):
+        plt.subplot(8,8,i+1)
+        plt.imshow(intermediate_output[0,i,:,:], cmap='viridis')
+        plt.axis('off')
+    plt.suptitle('ResNet Layer1 Conv2 Activations')
+    plt.savefig('temp.png')
+
+
+    # with torch.no_grad():
+    #     sample_image_input = sample_image.cuda(non_blocking=True)
+    #     _, output = model(sample_image_input) # compute output
+    # print(output)
 
     # sample_image = PILImage.open(sample_image_path).convert('RGB')
     # sample_image = transforms.Compose([Resize([256, 256]), CenterCrop(([[224,224]])), ToTensor(), Normalize([0.369875, 0.388726, 0.347536], [0.136821, 0.143952, 0.145229])])(sample_image)
