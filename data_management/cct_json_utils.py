32b573f50e7d4890365bc9a9709834a7b052acd1
Siyu Yang <yasiyu@microsoft.com>
2019-10-03 17:06:56 -0700

    Benchmark (#99)
    * Initial commit for the benchmark dataset section.
    
    * Rename BNF importer notebook.
    
    * Add stacked bar chart plotting and turning DB into OrderedDict functions.
    
    * Add notebooks for making train/val splits for CCT-20 and SS season 1 and for plotting species and bbox distributions.
    
    * Make caltech_bboxes_20190904.json and a snapshot of make_tfrecords.py for calt-20 tfrecords.
    
    * Snapshot after making the SS Season 1 tfrecords.
    
    * Add pipeline config for CCT-20 benchmark baseline training run.
    
    * Use manual learning rate schedule with 2k warmup.
    
    * Add pipeline config for SS season 1 benchmark baseline training run.
    
    * Refactor detector evaluation for benchmark detectors' results.
    
    * Add empty/non-empty classification accuracy evaluation.
    
    * Check in round 2 of CCT species distribution plotting and DB splitting.
    
    * Finalize DB splitting code.
    
    * Set up empty non-empty classification evaluation.
    
    * Finish empty non-empty classification eval.
    
    * Add wrongly classified sequences visualization to empty eval.
    
    
    Former-commit-id: 14cde2e6706bf9493d3e4a37bff76964fe6420fd

--- data_management/cct_json_utils.py
+++ data_management/cct_json_utils.py
@@ -15,7 +15,7 @@
 
 import os
 import json
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 
 #%% Classes
@@ -45,6 +45,74 @@ class CameraTrapJsonUtils:
         class_names = sorted(list(set(class_names)))
         return class_names
 
+    @staticmethod
+    def order_db_keys(db):
+        """
+        Given a dict representing a JSON database in the COCO Camera Trap format, return an
+        OrderedDict with keys in the order of 'info', 'categories', 'annotations' and 'images'.
+        When this OrderedDict is serialized with json.dump(), the order of the keys are preserved.
+        Args:
+            db: a dict representing a JSON database in the COCO Camera Trap format
+
+        Returns:
+            the same db but as an OrderedDict with keys ordered for readability.
+        """
+        ordered = OrderedDict([
+            ('info', db['info']),
+            ('categories', db['categories']),
+            ('annotations', db['annotations']),
+            ('images', db['images'])])
+        return ordered
+
+    @staticmethod
+    def annotations_groupby_image_field(db_indexed, image_field='seq_id'):
+        """
+        Given an instance of IndexedJsonDb, group annotation entries by a field in the
+        image entry.
+        """
+        image_id_to_image_field = {}
+        for image_id, image_entry in db_indexed.image_id_to_image.items():
+            image_id_to_image_field[image_id] = image_entry[image_field]
+
+        res = defaultdict(list)
+        for annotations in db_indexed.image_id_to_annotations.values():
+            for annotation_entry in annotations:
+                field_value = image_id_to_image_field[annotation_entry['image_id']]
+                res[field_value].append(annotation_entry)
+        return res
+
+    @staticmethod
+    def get_entries_from_locations(db, locations):
+        """
+        Given a dict representing a JSON database in the COCO Camera Trap format, return a dict
+        with the 'images' and 'annotations' fields in the CCT format, each is an array that only
+        includes entries in the original `db` that are in the `locations` set.
+        Args:
+            db: a dict representing a JSON database in the COCO Camera Trap format
+            locations: a set or list of locations to include; each item is a string
+
+        Returns:
+            a dict with the 'images' and 'annotations' fields in the CCT format
+        """
+        locations = set(locations)
+        print('Original DB has {} image and {} annotation entries.'.format(len(db['images']), len(db['annotations'])))
+        new_db = {
+            'images': [],
+            'annotations': []
+        }
+        new_images = set()
+        for i in db['images']:
+            # cast location to string as the entries in locations are strings
+            if str(i['location']) in locations:
+                new_db['images'].append(i)
+                new_images.add(i['id'])
+        for a in db['annotations']:
+            if a['image_id'] in new_images:
+                new_db['annotations'].append(a)
+        print(
+            'New DB has {} image and {} annotation entries.'.format(len(new_db['images']), len(new_db['annotations'])))
+        return new_db
+
 
 class IndexedJsonDb:
     """
@@ -54,15 +122,49 @@ class IndexedJsonDb:
     a .json database.
     """
 
-    # The underlying .json db
-    db = None
+    def __init__(self, json_filename, b_normalize_paths=False, filename_replacements={}):
+        '''
+        json_filename can also be an existing json db
+        '''
 
-    # Useful dictionaries
-    cat_id_to_name = None
-    cat_name_to_id = None
-    filename_to_id = None
-    image_id_to_annotations = None
+        if isinstance(json_filename, str):
+            self.db = json.load(open(json_filename))
+        else:
+            self.db = json_filename
 
+        assert 'images' in self.db, 'Could not find image list in file {}, are you sure this is a COCO camera traps file?'.format(
+            json_filename)
+
+        if b_normalize_paths:
+            # Normalize paths to simplify comparisons later
+            for im in self.db['images']:
+                im['file_name'] = os.path.normpath(im['file_name'])
+
+        for s in filename_replacements:
+            r = filename_replacements[s]
+            for im in self.db['images']:
+                im['file_name'] = im['file_name'].replace(s, r)
+
+        ### Build useful mappings to facilitate working with the DB
+
+        # Category ID <--> name
+        self.cat_id_to_name = {cat['id']: cat['name'] for cat in self.db['categories']}
+        self.cat_name_to_id = {cat['name']: cat['id'] for cat in self.db['categories']}
+
+        # Image filename --> ID
+        self.filename_to_id = {im['file_name']: im['id'] for im in self.db['images']}
+
+        # Each image can potentially multiple annotations, hence using lists
+        self.image_id_to_annotations = defaultdict(list)
+
+        # Image ID --> image object
+        self.image_id_to_image = {im['id']: im for im in self.db['images']}
+
+        # Image ID --> annotations
+        for ann in self.db['annotations']:
+            self.image_id_to_annotations[ann['image_id']].append(ann)
+
+    # ...__init__
 
     def get_annotations_for_image(self, image):
         """
@@ -103,49 +205,5 @@ class IndexedJsonDb:
         class_names = [self.cat_id_to_name[x] for x in class_ids]
         
         return class_names
-        
-        
-    def __init__(self, json_filename, b_normalize_paths=False, filename_replacements={}):
-        '''
-        json_filename can also be an existing json db
-        '''
-        
-        if isinstance(json_filename,str):
-            self.db = json.load(open(json_filename))
-        else:
-            self.db = json_filename
-    
-        assert 'images' in self.db, 'Could not find image list in file {}, are you sure this is a COCO camera traps file?'.format(json_filename)
-        
-        if b_normalize_paths:
-            # Normalize paths to simplify comparisons later
-            for im in self.db['images']:
-                im['file_name'] = os.path.normpath(im['file_name'])
-
-        for s in filename_replacements:
-            r = filename_replacements[s]
-            for im in self.db['images']:
-                im['file_name'] = im['file_name'].replace(s, r)
-        
-        ### Build useful mappings to facilitate working with the DB
-
-        # Category ID <--> name
-        self.cat_id_to_name = {cat['id']: cat['name'] for cat in self.db['categories']}
-        self.cat_name_to_id = {cat['name']: cat['id'] for cat in self.db['categories']}
-
-        # Image filename --> ID
-        self.filename_to_id = {im['file_name']: im['id'] for im in self.db['images']}
-
-        # Each image can potentially multiple annotations, hence using lists
-        self.image_id_to_annotations = defaultdict(list)
-
-        # Image ID --> image object
-        self.image_id_to_image = {im['id']: im for im in self.db['images']}
-        
-        # Image ID --> annotations
-        for ann in self.db['annotations']:
-            self.image_id_to_annotations[ann['image_id']].append(ann)
-
-    # ...__init__
 
 # ...class IndexedJsonDb
