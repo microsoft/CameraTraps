e6672dc1f6083b8486fec78596f43e371d3fca3b
Siyu Yang <yasiyu@microsoft.com>
2019-10-03 17:06:56 -0700

    Benchmark (#99)
    * Initial commit for the benchmark dataset section.
    
    * Rename BNF importer notebook.
    
    * Add stacked bar chart plotting and turning DB into OrderedDict functions.
    
    * Add notebooks for making train/val splits for CCT-20 and SS season 1 and for plotting species and bbox distributions.
    
    * Make caltech_bboxes_20190904.json and a snapshot of make_tfrecords.py for calt-20 tfrecords.
    
    * Snapshot after making the SS Season 1 tfrecords.
    
    * Add pipeline config for CCT-20 benchmark baseline training run.
    
    * Use manual learning rate schedule with 2k warmup.
    
    * Add pipeline config for SS season 1 benchmark baseline training run.
    
    * Refactor detector evaluation for benchmark detectors' results.
    
    * Add empty/non-empty classification accuracy evaluation.
    
    * Check in round 2 of CCT species distribution plotting and DB splitting.
    
    * Finalize DB splitting code.
    
    * Set up empty non-empty classification evaluation.
    
    * Finish empty non-empty classification eval.
    
    * Add wrongly classified sequences visualization to empty eval.

--- visualization/visualization_utils.py
+++ visualization/visualization_utils.py
@@ -8,10 +8,13 @@
 
 #%% Constants and imports
 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 from PIL import Image, ImageFile, ImageFont, ImageDraw
-import matplotlib.pyplot as plt
+
 from data_management.annotations import annotation_constants
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
@@ -69,6 +72,21 @@ def resize_image(image, target_width, target_height=-1):
 
     resized_image = image.resize((target_width, target_height), Image.ANTIALIAS)
     return resized_image
+
+
+def show_images_in_a_row(images):
+    num = len(images)
+    assert num > 0
+
+    if isinstance(images[0], str):
+        images = [Image.open(img) for img in images]
+
+    fig, axarr = plt.subplots(1, num, squeeze=False)  # number of rows, number of columns
+    fig.set_size_inches((num * 5, 25))  # each image is 2 inches wide
+    for i, img in enumerate(images):
+        axarr[0, i].set_axis_off()
+        axarr[0, i].imshow(img)
+    return fig
 
 
 def render_iMerit_boxes(boxes, classes, image,
@@ -135,10 +153,9 @@ def render_db_bounding_boxes(boxes, classes, image, original_size=None,
 
         if label_map:
             clss = label_map[int(clss)]
-        display_strs.append([clss])
+        display_strs.append([str(clss)])  # need to be a string here because PIL needs to iterate through chars
 
     display_boxes = np.array(display_boxes)
-
     draw_bounding_boxes_on_image(image, display_boxes, classes, display_strs=display_strs,
                                  thickness=thickness)
 
@@ -238,7 +255,6 @@ def render_detection_bounding_boxes(detections, image,
                     displayed_label += ['{}: {:5.1%}'.format(class_name.lower(), classification[1])]
                     
             # ...if we have detection results
-            
             display_strs.append(displayed_label)
             classes.append(clss)
 
@@ -294,7 +310,7 @@ def draw_bounding_boxes_on_image(image,
       image: a PIL.Image object.
       boxes: a 2 dimensional numpy array of [N, 4]: (ymin, xmin, ymax, xmax).
              The coordinates are in normalized format between [0, 1].
-      classes: a list of ints or strings (which can be cast to ints) corresponding to the class labels of the boxes.
+      classes: a list of ints or strings (that can be cast to ints) corresponding to the class labels of the boxes.
              This is only used for selecting the color to render the bounding box in.
       thickness: line thickness. Default value is 4.
       display_strs: list of list of strings.
@@ -346,7 +362,7 @@ def draw_bounding_box_on_image(image,
       xmin: xmin of bounding box.
       ymax: ymax of bounding box.
       xmax: xmax of bounding box.
-      clss: the class of the object in this bounding box - will be cast to an int.
+      clss: str, the class of the object in this bounding box - will be cast to an int.
       thickness: line thickness. Default value is 4.
       display_str_list: list of strings to display in box
                         (each to be shown on its own line).
@@ -378,7 +394,7 @@ def draw_bounding_box_on_image(image,
     # box exceeds the top of the image, stack the strings below the bounding box
     # instead of above.
     display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
-    
+
     # Each display_str has a top and bottom margin of 0.05x.
     total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
 
@@ -389,6 +405,7 @@ def draw_bounding_box_on_image(image,
 
     # Reverse list and print from bottom to top.
     for display_str in display_str_list[::-1]:
+
         text_width, text_height = font.getsize(display_str)
         margin = np.ceil(0.05 * text_height)
 
@@ -396,11 +413,13 @@ def draw_bounding_box_on_image(image,
             [(left, text_bottom - text_height - 2 * margin), (left + text_width,
                                                               text_bottom)],
             fill=color)
+
         draw.text(
             (left + margin, text_bottom - text_height - margin),
             display_str,
             fill='black',
             font=font)
+
         text_bottom -= (text_height + 2 * margin)
 
 
@@ -454,9 +473,9 @@ def plot_confusion_matrix(matrix, classes,
     for i, j in np.ndindex(matrix.shape):
 
         plt.text(j, i, '{:.0f}%'.format(matrix[i, j]*100),
-                 horizontalalignment="center",
-                 verticalalignment="center",
-                 color="white" if matrix[i, j] > 0.5 else "black",
+                 horizontalalignment='center',
+                 verticalalignment='center',
+                 color='white' if matrix[i, j] > 0.5 else 'black',
                 fontsize='x-small')
 
     if y_label:
@@ -495,5 +514,58 @@ def plot_precision_recall_curve(precisions, recalls, title='Precision/Recall cur
     plt.ylabel('Precision')
     plt.ylim([0.0, 1.05])
     plt.xlim([0.0, 1.05])
+
+    return fig
+
+
+def plot_stacked_bar_chart(data, series_labels, col_labels=None, x_label=None, y_label=None, log_scale=False):
+    """
+    For plotting e.g. species distribution across locations.
+    Reference: https://stackoverflow.com/questions/44309507/stacked-bar-plot-using-matplotlib
+    Args:
+        data: a 2-dimensional numpy array or nested list containing data for each series (species)
+              in rows (1st dimension) across locations (columns, 2nd dimension)
+
+    Returns:
+        the plot that can then be saved as a png.
+    """
+
+    fig = plt.figure()
+    ax = plt.subplot(111)
+
+    data = np.array(data)
+    num_series, num_columns = data.shape
+    ind = list(range(num_columns))
+
+    colors = cm.rainbow(np.linspace(0, 1, num_series))
+
+    cumulative_size = np.zeros(num_columns)  # stacked bar charts are made with each segment starting from a y position
+
+    for i, row_data in enumerate(data):
+        ax.bar(ind, row_data, bottom=cumulative_size, label=series_labels[i], color=colors[i])
+        cumulative_size += row_data
+
+    if col_labels and len(col_labels) < 25:
+        ax.set_xticks(ind)
+        ax.set_xticklabels(col_labels, rotation=90)
+    elif col_labels:
+        ax.set_xticks(list(range(0, len(col_labels), 20)))
+        ax.set_xticklabels(col_labels, rotation=90)
+
+    if x_label:
+        ax.set_xlabel(x_label)
+
+    if y_label:
+        ax.set_ylabel(y_label)
+
+    if log_scale:
+        ax.set_yscale('log')
+
+    # To fit the legend in, shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(0.99, 0.5), frameon=False)
 
     return fig
