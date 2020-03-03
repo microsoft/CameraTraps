e5abc5daa25acb1bde7c7aa4b7e60e5ec2ab3fed
Dan Morris <dmorris@cs.stanford.edu>
2020-03-02 19:08:57 -0800

    starting to review carrizo data sets

--- data_management/importers/carrizo_trail_cam_2017.py
+++ data_management/importers/carrizo_trail_cam_2017.py
@@ -1,111 +1,114 @@
 #
 # carrizo_trail_cam_2017.py
 #
-# Convert the .csv files provided for the Carrizo Trail Cam 2017 data set to a 
-# COCO-camera-traps .json file
+# Convert the .csv files provided for the "Trail Cam Carrizo" 2017 data set to 
+# a COCO-camera-traps .json file.
 #
 
-# %% Constants and environment
+#%% Constants and environment
 
 import pandas as pd
 import os
-import glob
 import json
-import re
 import uuid
 import time
-import ntpath
 import humanfriendly
-import PIL
 from PIL import Image
 import numpy as np
-import logging
 
+from tqdm import tqdm
 from path_utils import find_images
 
-input_base = r'/mnt/blobfuse/wildlifeblobssc/Trail Cam Carrizo 2017/'
+input_base = r'Z:\Trail Cam Carrizo 2017'
 open_metadata_file = os.path.join(input_base, 'Carrizo open 2017.csv')
 shrub_metadata_file = os.path.join(input_base, 'Carrizo Shrub 2017.csv')
 
-output_base = r'/home/gramener'
+output_base = r'G:\carrizo-mojave'
 output_json_file = os.path.join(output_base, 'Trail Cam Carrizo 2017.json')
 image_directory = input_base
-
-
 input_metadata_files = [open_metadata_file, shrub_metadata_file]
-# output_file = r'C:\Users\Gramener\Desktop\Projects\Microsoft\Camera Traps\carrizo_trail_cam_2017.json'
-# image_directory = r'C:\Users\Gramener\Desktop\Projects\Microsoft\Camera Traps\Trail Cam Carrizo 2017'
-final_data = pd.DataFrame()
-assert(os.path.isdir(image_directory))
-logging.basicConfig(filename='carrizo_camera_trap_2017.log', level=logging.INFO)
 
-# %% Read source data
+load_width_and_height = False
+
+assert(os.path.isdir(image_directory))
+
+category_replacements = {'unidnetifiable':'unidentifiable','unidentifiable animal':'unidentifiable'}
+
+annotation_fields_to_copy = ['rep','photo rep','timeblock','night.day','observations']
+
+
+#%% Read source data
+
+final_data = pd.DataFrame()
 
 for inp_file in input_metadata_files:
-    print("Reading : {0}".format(inp_file))
+    
+    print("Reading: {0}".format(inp_file))
     input_metadata = pd.read_csv(inp_file)
-    print('Read {} columns and {} rows from metadata file'.format(len(input_metadata.columns), len(input_metadata)))
+    
+    print('Read {} columns and {} rows from metadata file'.format(len(input_metadata.columns), 
+                                                                  len(input_metadata)))
+    
     # Removing the empty records
     input_metadata = input_metadata[~np.isnan(input_metadata['rep'])]
+    
     # Filenames were provided as numbers, but images were *.JPG, converting here
     input_metadata['file'] = input_metadata.groupby(["rep", "week"]).cumcount()
     week_folder_format = {1: 'week 1- 2017', 2: 'week2- 2017', 3: 'week3-2017'}
+    
     input_metadata['file'] = input_metadata[['file', 'rep', 'week', 'microsite']].apply(
-        lambda x: "{3}/{4}{1}-week{0}-carrizo-2017/IMG_{2}.JPG".format(int(x[2]), int(x[1]), str(int(x[0]+1)).zfill(4), week_folder_format[int(x[2])], x[3].lower()), axis=1)
-    print('Converted filenames in the dataframe')
+        lambda x: "{3}/{4}{1}-week{0}-carrizo-2017/IMG_{2}.JPG".format(int(x[2]), int(x[1]), 
+                                                                       str(int(x[0]+1)).zfill(4), 
+                                                                       week_folder_format[int(x[2])], 
+                                                                       x[3].lower()), axis=1)
+    
     final_data = final_data.append(input_metadata)
-# import pdb;pdb.set_trace()
-# %% Map filenames to rows, verify image existence
-# import pdb;pdb.set_trace()
-startTime = time.time()
-filenamesToRows = {}
-imageFilenames = input_metadata.file
 
-duplicateRows = []
+print('Read {} metadata rows'.format(len(final_data)))
 
-logging.info("File names which are present in CSV but not in the directory")
+    
+#%% Map filenames to rows, verify image existence
+    
+start_time = time.time()
+filenames_to_rows = {}
+image_filenames = input_metadata.file
+
+missing_files = []
+duplicate_rows = []
+
 # Build up a map from filenames to a list of rows, checking image existence as we go
-for iFile, fn in enumerate(imageFilenames):
-    if (fn in filenamesToRows):
-        duplicateRows.append(iFile)
-        filenamesToRows[fn].append(iFile)
+for iFile, fn in tqdm(enumerate(image_filenames),total=len(image_filenames)):
+    if (fn in filenames_to_rows):
+        duplicate_rows.append(iFile)
+        filenames_to_rows[fn].append(iFile)
     else:
-        filenamesToRows[fn] = [iFile]
-        imagePath = os.path.join(image_directory, fn)
-        try:
-            assert(os.path.isfile(imagePath))
-        except Exception:
-            logging.info(imagePath)
+        filenames_to_rows[fn] = [iFile]
+        image_path = os.path.join(image_directory, fn)
+        if not os.path.isfile(image_path):
+            missing_files.append(fn)
 
-elapsed = time.time() - startTime
-print('Finished verifying image existence in {}, found {} filenames with multiple labels'.format(
-    humanfriendly.format_timespan(elapsed), len(duplicateRows)))
+elapsed = time.time() - start_time
 
-# I didn't expect this to be true a priori, but it appears to be true, and
-# it saves us the trouble of checking consistency across multiple occurrences
-# of an image.
-assert(len(duplicateRows) == 0)    
+print('Finished verifying image existence in {}, found {} missing files'.format(
+    humanfriendly.format_timespan(elapsed), 
+    len(missing_files)))
 
-# %% Check for images that aren't included in the metadata file
 
-# Enumerate all images
-# imageFullPaths = glob.glob(os.path.join(image_directory, '*\\*\\*.JPG'))
-imageFullPaths = find_images(image_directory, bRecursive=True)
-for iImage, imagePath in enumerate(imageFullPaths):
-    # fn = ntpath.basename(imagePath)
-    # parent_dir = os.path.basename(os.path.dirname(imagePath))
-    # import pdb;pdb.set_trace()
-    try:
-        assert(imagePath.replace(input_base, '') in filenamesToRows)
-    except Exception as e:
-        print(e)
-        continue
-        # import pdb;pdb.set_trace()
+#%% Check for images that aren't included in the metadata file
 
-print('Finished checking {} images to make sure they\'re in the metadata'.format(
-        len(imageFullPaths)))
+image_full_paths = find_images(image_directory, bRecursive=True)
+images_missing_from_metadata = []
 
-# %% Create CCT dictionaries
+for iImage, image_path in tqdm(enumerate(image_full_paths),total=len(image_full_paths)):
+
+    relative_path = os.path.relpath(image_path,input_base).replace('\\','/')
+    if relative_path not in filenames_to_rows:
+        images_missing_from_metadata.append(relative_path)
+    
+print('{} of {} files are not in metadata'.format(len(images_missing_from_metadata),len(image_full_paths)))
+
+
+#%% Create CCT dictionaries
 
 # Also gets image sizes, so this takes ~6 minutes
 #
@@ -115,43 +118,52 @@ images = []
 annotations = []
 
 # Map categories to integer IDs (that's what COCO likes)
-nextCategoryID = 0
-categoriesToCategoryId = {}
-categoriesToCounts = {}
+next_category_id = 0
+categories_to_category_id = {}
+categories_to_counts = {}
 
 # For each image
 #
 # Because in practice images are 1:1 with annotations in this data set,
 # this is also a loop over annotations.
 
-startTime = time.time()
-#print(imageFilenames)
-imageName = imageFilenames[0]
-for imageName in imageFilenames:
-    rows = filenamesToRows[imageName]
-    # As per above, this is convenient and appears to be true; asserting to be safe
-    assert(len(rows) == 1)    
+start_time = time.time()
+
+for image_name in tqdm(image_filenames):
+    
+    rows = filenames_to_rows[image_name]
+    
+    # Each filename should just match one row
+    assert(len(rows) == 1)   
+    
     iRow = rows[0]
     row = input_metadata.iloc[iRow]
     im = {}
-    im['id'] = imageName.replace('\\','/').replace('/','_').replace(' ','_')
-    im['file_name'] = imageName
+    im['id'] = image_name.replace('\\','/').replace('/','_').replace(' ','_')
+    im['file_name'] = image_name
     im['region'] = row['region']
     im['site']= row['site']
     im['mircosite'] = row['microsite']
     im['datetime'] = row['calendar date']
     im['location'] = "{0}_{1}_{2}".format(row['region'], row['site'], row['microsite'])
-    # Check image height and width
-    imagePath = os.path.join(image_directory, fn)
-    assert(os.path.isfile(imagePath))
-    pilImage = Image.open(imagePath)
-    width, height = pilImage.size
-    im['width'] = width
-    im['height'] = height
-
+    
+    image_path = os.path.join(image_directory, image_name)
+    
+    # Don't include images that don't exist on disk
+    if not os.path.isfile(image_path):
+        continue
+    
+    if load_width_and_height:
+        pilImage = Image.open(image_path)
+        width, height = pilImage.size
+        im['width'] = width
+        im['height'] = height
+    else:
+        im['width'] = -1
+        im['height'] = -1
+        
     images.append(im)
     
-#     category = row['label'].lower()
     is_image = row['animal.capture']
     
     # Use 'empty', to be consistent with other data on lila    
@@ -161,17 +173,20 @@ for imageName in imageFilenames:
         if row['animal'] is np.nan:
             category = 'unidentifiable'
         else:
-            category = row['animal']
+            category = row['animal'].strip()
+    
+    if category in category_replacements:
+        category = category_replacements[category]
         
     # Have we seen this category before?
-    if category in categoriesToCategoryId:
-        categoryID = categoriesToCategoryId[category]
-        categoriesToCounts[category] += 1
+    if category in categories_to_category_id:
+        categoryID = categories_to_category_id[category]
+        categories_to_counts[category] += 1
     else:
-        categoryID = nextCategoryID
-        categoriesToCategoryId[category] = categoryID
-        categoriesToCounts[category] = 0
-        nextCategoryID += 1
+        categoryID = next_category_id
+        categories_to_category_id[category] = categoryID
+        categories_to_counts[category] = 0
+        next_category_id += 1
     
     # Create an annotation
     ann = {}
@@ -182,6 +197,12 @@ for imageName in imageFilenames:
     ann['image_id'] = im['id']    
     ann['category_id'] = categoryID
     
+    for fieldname in annotation_fields_to_copy:
+        ann[fieldname] = row[fieldname]
+        if ann[fieldname] is np.nan:
+            ann[fieldname] = ''
+        ann[fieldname] = str(ann[fieldname])
+        
     annotations.append(ann)
     
 # ...for each image
@@ -190,37 +211,36 @@ for imageName in imageFilenames:
 
 categories = []
 
-for category in categoriesToCounts:
-    print('Category {}, count {}'.format(category,categoriesToCounts[category]))
-    categoryID = categoriesToCategoryId[category]
+for category in categories_to_counts:
+    print('Category {}, count {}'.format(category,categories_to_counts[category]))
+    categoryID = categories_to_category_id[category]
     cat = {}
     cat['name'] = category
     cat['id'] = categoryID
     categories.append(cat)    
     
-elapsed = time.time() - startTime
+elapsed = time.time() - start_time
 print('Finished creating CCT dictionaries in {}'.format(
       humanfriendly.format_timespan(elapsed)))
     
 
-# %% Create info struct
+#%% Create info struct
 
 info = {}
-info['year'] = 2018
+info['year'] = 2017
 info['version'] = 1
-info['description'] = 'COCO style database'
-info['secondary_contributor'] = 'Converted to COCO .json by Dan Morris'
-info['contributor'] = 'Vardhan Duvvuri'
+info['description'] = 'Carrizo Trail Cam 2017'
+info['contributor'] = 'York University'
 
 
-# %% Write output
+#%% Write output
 
 json_data = {}
 json_data['images'] = images
 json_data['annotations'] = annotations
 json_data['categories'] = categories
 json_data['info'] = info
-json.dump(json_data, open(output_json_file, 'w'), indent=4)
+json.dump(json_data, open(output_json_file, 'w'), indent=1)
 
 print('Finished writing .json file with {} images, {} annotations, and {} categories'.format(
         len(images),len(annotations),len(categories)))
@@ -235,7 +255,6 @@ options.baseDir = image_directory
 options.bCheckImageSizes = False
 options.bCheckImageExistence = False
 options.bFindUnusedImages = False
-# import pdb;pdb.set_trace()    
 data = sanity_check_json_db.sanity_check_json_db(output_json_file,options)
 
 
@@ -245,14 +264,15 @@ from visualization import visualize_db
 from data_management.databases import sanity_check_json_db
 
 viz_options = visualize_db.DbVizOptions()
-viz_options.num_to_visualize = 1000
+viz_options.num_to_visualize = 80
 viz_options.trim_to_images_with_bboxes = False
-viz_options.add_search_links = True
+viz_options.add_search_links = False
 viz_options.sort_by_filename = False
 viz_options.parallelize_rendering = True
+viz_options.classes_to_exclude = ['empty']
 html_output_file,image_db = visualize_db.process_images(db_path=output_json_file,
                                                         output_dir=os.path.join(
-                                                            output_base, 'Trail Cam Carrizo 2017/preview'),
+                                                        output_base, 'Trail Cam Carrizo 2017/preview'),
                                                         image_base_dir=image_directory,
                                                         options=viz_options)
 os.startfile(html_output_file)
