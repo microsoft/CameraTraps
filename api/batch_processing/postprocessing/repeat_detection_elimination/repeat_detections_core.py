c0fa725bfdf32423cf78913cd78974444a000ecb
Dan Morris <dmorris@cs.stanford.edu>
2020-04-05 11:18:11 -0700

    Updating imports to reflect new repeat_detection_elimination folder
    Former-commit-id: deeb2e3bd42be7998ce7448c8c77a3487ceddcaf

--- api/batch_processing/postprocessing/repeat_detection_elimination/repeat_detections_core.py
+++ api/batch_processing/postprocessing/repeat_detection_elimination/repeat_detections_core.py
@@ -23,8 +23,8 @@ import write_html_image_list
 import path_utils
 
 from api.batch_processing.postprocessing.load_api_results import load_api_results, write_api_results
-import ct_utils
 from visualization.visualization_utils import open_image, render_detection_bounding_boxes
+import ct_utils
 
 # Imports I'm not using but use when I tinker with parallelization
 #
