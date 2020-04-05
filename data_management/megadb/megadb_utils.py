a94bc2bb701037db1e49d74cc853f8fcd7796f50
Siyu Yang <yasiyu@microsoft.com>
2020-04-05 15:17:23 -0700

    Modify the MegaDB seq entries to CCT images and annotations workflow to get dimensions of images. (#148)
    Former-commit-id: a3742f740bd6ede104e88a26abe778af562cc2e2

--- data_management/megadb/megadb_utils.py
+++ data_management/megadb/megadb_utils.py
@@ -8,6 +8,7 @@ or passed in to the initializer.
 import os
 
 from azure.cosmos.cosmos_client import CosmosClient
+from azure.storage.blob import BlockBlobService
 
 
 class MegadbUtils:
@@ -19,6 +20,7 @@ class MegadbUtils:
             key = os.environ['COSMOS_KEY']
         client = CosmosClient(url, credential=key)
         self.database = client.get_database_client('camera-trap')
+
 
     def get_datasets_table(self):
         """
@@ -36,6 +38,7 @@ class MegadbUtils:
                     iter(result_iterable)}
         return datasets
 
+
     def get_splits_table(self):
         """
 
@@ -51,3 +54,39 @@ class MegadbUtils:
         splits = {i['dataset']: {k: set(v) for k, v in i.items() if not k.startswith('_')} for i in
                     iter(result_iterable)}
         return splits
+
+
+    @staticmethod
+    def get_blob_service(datasets_table, dataset_name):
+        """
+        Return the azure.storage.blob BlockBlobService object corresponding to the dataset
+
+        datasets_table is updated (no new copy) if a new BlockBlobService is created for a dataset
+        """
+        if dataset_name not in datasets_table:
+            raise KeyError('Dataset {} is not in the datasets table.'.format(dataset_name))
+
+        entry = datasets_table[dataset_name]
+
+        if 'blob_service' in entry:
+            return entry['blob_service']
+
+        # need to create a new blob service for this dataset
+        if 'container_sas_key' not in entry:
+            raise KeyError('Dataset {} does not have the container_sas_key field in the datasets table.'.format(dataset_name))
+
+        # the SAS token can be just for the container, not the storage account
+        # - will be fine for accessing files in that container later
+        blob_service = BlockBlobService(account_name=entry['storage_account'],
+                                        sas_token=entry['container_sas_key'])
+        datasets_table[dataset_name]['blob_service'] = blob_service  # in-place update
+        return blob_service
+
+
+    @staticmethod
+    def get_full_path(datasets_table, dataset_name, img_path):
+        entry = datasets_table[dataset_name]
+        if 'path_prefix' not in entry or entry['path_prefix'] == '':
+            return img_path
+        else:
+            return os.path.join(entry['path_prefix'], img_path)
--- data_management/megadb/query_script.py
+++ data_management/megadb/query_script.py
@@ -14,10 +14,22 @@ from azure.cosmos.cosmos_client import CosmosClient
 
 #%% Common queries
 
+# This query is used when preparing tfrecords for object detector training
+# We do not want to get the whole seq obj where at least one image has bbox because some images in that sequence
+# will not be bbox labeled so will be confusing
 query_bbox = '''
 SELECT im.bbox, im.file, seq.dataset, seq.location
 FROM sequences seq JOIN im IN seq.images 
 WHERE ARRAY_LENGTH(im.bbox) >= 0
+'''
+
+# For public datasets to be converted to the CCT format, we get the whole seq object because
+# sequence level attributes need to be included too. megadb/converters/megadb_to_cct.py handles
+# the case of bbox-only JSONs with the flag exclude_species_class
+query_lila = '''
+SELECT seq
+FROM sequences seq
+WHERE (SELECT VALUE COUNT(im) FROM im IN seq.images WHERE ARRAY_LENGTH(im.bbox) >= 0) > 0
 '''
 
 query_empty = '''
@@ -29,14 +41,14 @@ OR ARRAY_LENGTH(seq.class) = 1 AND ARRAY_CONTAINS(seq.class, "empty")
 
 #%% Parameters
 
-query = query_bbox
+query = query_lila
 
-output_dir = '/home/marmot/camtrap/data/mdv4_empty'
+output_dir = '/home/marmot/camtrap/data/wcs_boxes'
 assert os.path.isdir(output_dir), 'Please create the output directory first'
 
 output_indent = None  # None if no indentation needed in the output, or int
 
-partition_key=None  # use None if querying across all partitions
+partition_key='wcs'  # use None if querying across all partitions
 
 save_every = 20000
 assert save_every > 0
@@ -74,7 +86,7 @@ part_count = 0
 part_paths = []
 
 for item in result_iterable:
-    results.append(item)
+    results.append(item['seq'])  # MODIFY HERE depending on the query
     item_count += 1
 
     if item_count % save_every == 0:
