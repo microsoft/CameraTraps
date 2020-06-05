43655554f67277c835ff2864576cc37c3e0aea99
Siyu Yang <yasiyu@microsoft.com>
2020-06-05 16:59:53 -0700

    Tidy up sync API. Fix wrong int label for vehicle class in detection inference scripts. (#165)

--- data_management/annotations/annotation_constants.py
+++ data_management/annotations/annotation_constants.py
@@ -15,13 +15,20 @@ NUM_DETECTOR_CATEGORIES = 3  # this is for choosing colors, so ignoring the "emp
 
 # This is the label mapping used for our incoming iMerit annotations
 # Only used to parse the incoming annotations. In our database, the string name is used to avoid confusion
-label_annotations_bbox_categories = [
+annotation_bbox_categories = [
     {'id': 0, 'name': 'empty'},
     {'id': 1, 'name': 'animal'},
     {'id': 2, 'name': 'person'},
     {'id': 3, 'name': 'group'},  # group of animals
     {'id': 4, 'name': 'vehicle'}
 ]
+
+annotation_bbox_category_id_to_name = {}
+annotation_bbox_category_name_to_id = {}
+
+for cat in annotation_bbox_categories:
+    annotation_bbox_category_id_to_name[cat['id']] = cat['name']
+    annotation_bbox_category_name_to_id[cat['name']] = cat['id']
 
 # MegaDetector outputs
 detector_bbox_categories = [
