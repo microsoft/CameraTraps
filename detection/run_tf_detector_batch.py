5b0151f8cf42decc29142dc2076eeafd583a0751
Ubuntu <mallard@mallardvm.vby1cnsztnbubpoopriasnvbxa.jx.internal.cloudapp.net>
2020-06-17 01:21:45 +0000

    Add documentation to run_tf_detector_batch.py
    Lots of code cleanup too
    
    
    Former-commit-id: 7f4884789cdd2eb3f580ba6e2a1db8b027daa3e4

--- detection/run_tf_detector_batch.py
+++ detection/run_tf_detector_batch.py
@@ -24,13 +24,10 @@ is not supported when using multiprocessing.
 
 Sample invocation:
 
-```
-python run_tf_detector_batch.py "d:\temp\models\megadetector_v3.pb" "d:\temp\test_images" "d:\temp\test\out.json" --recursive
-```
-
+python run_tf_detector_batch.py "d:\temp\models\md_v4.1.0.pb" "d:\temp\test_images" "d:\temp\out.json" --recursive
 """
 
-#%% Constants, imports, environment
+# %% Constants, imports, environment
 
 import argparse
 import json
@@ -40,7 +37,7 @@ import time
 import copy
 import warnings
 import itertools
-        
+
 from datetime import datetime
 from functools import partial
 
@@ -61,32 +58,45 @@ print('TensorFlow version:', tf.__version__)
 print('tf.test.is_gpu_available:', tf.test.is_gpu_available())
 
 
-#%% Support functions for multiprocessing
+# %% Support functions for multiprocessing
 
 def process_images(im_files, tf_detector, confidence_threshold):
-    
-    if isinstance(tf_detector,str):
+    '''Runs the MegaDetector over a list of image files.
+
+    Args
+    - im_files: list of str, paths to image files
+    - tf_detector: TFDetector (loaded model) or str (path to .pb model file)
+    - confidence_threshold: float, only detections above this threshold are returned
+
+    Returns
+    - results: list of dict, each dict represents detections on one image
+        see the 'images' key in https://github.com/microsoft/CameraTraps/tree/master/api/batch_processing#batch-processing-api-output-format
+    '''
+    if isinstance(tf_detector, str):
         start_time = time.time()
         tf_detector = TFDetector(tf_detector)
         elapsed = time.time() - start_time
-        print('Loaded model (batch level) in {}'.format(humanfriendly.format_timespan(elapsed)))       
-    
+        print('Loaded model (batch level) in {}'.format(humanfriendly.format_timespan(elapsed)))
+
     results = []
     for im_file in im_files:
         results.append(process_image(im_file, tf_detector, confidence_threshold))
     return results
-        
+
 
 def process_image(im_file, tf_detector, confidence_threshold):
-    
-    if isinstance(tf_detector,str):
-        start_time = time.time()
-        tf_detector = TFDetector(tf_detector)
-        elapsed = time.time() - start_time
-        print('Loaded model (worker level) in {}'.format(humanfriendly.format_timespan(elapsed)))       
-    
+    '''Runs the MegaDetector over a single image file.
+
+    Args
+    - im_file: str, path to image file
+    - tf_detector: TFDetector, loaded model
+    - confidence_threshold: float, only detections above this threshold are returned
+
+    Returns:
+    - result: dict representing detections on one image
+        see the 'images' key in https://github.com/microsoft/CameraTraps/tree/master/api/batch_processing#batch-processing-api-output-format
+    '''
     print('Processing image {}'.format(im_file))
-    image = None
     try:
         image = viz_utils.load_image(im_file)
     except Exception as e:
@@ -94,125 +104,120 @@ def process_image(im_file, tf_detector, confidence_threshold):
         result = {
             'file': im_file,
             'failure': TFDetector.FAILURE_IMAGE_OPEN
-        }            
+        }
         return result
-    
+
     try:
-        result = tf_detector.generate_detections_one_image(image, im_file, 
-                                                           detection_threshold=confidence_threshold)
+        result = tf_detector.generate_detections_one_image(
+            image, im_file, detection_threshold=confidence_threshold)
     except Exception as e:
         print('Image {} cannot be processed. Exception: {}'.format(im_file, e))
         result = {
             'file': im_file,
             'failure': TFDetector.FAILURE_TF_INFER
-        }            
+        }
         return result
-    
+
     return result
 
-# Split a list into chunks of size n
-def chunks_by_size(l, n):
-    return [l[i:i+n] for i in range(0, len(l), n)]
 
-# Split a list into n even chunks 
-def chunks_by_number_of_chunks(l, n):
+def chunks_by_number_of_chunks(ls, n):
+    '''Splits a list into n even chunks.
+
+    Args
+    - ls: list
+    - n: int, # of chunks
+    '''
     for i in range(0, n):
-        yield l[i::n]
-        
-        
-#%% Main function
-        
+        yield ls[i::n]
+
+
+# %% Main function
+
 def load_and_run_detector_batch(model_file, image_file_names, checkpoint_path=None,
-                                confidence_threshold=0, checkpoint_frequency=-1, 
+                                confidence_threshold=0, checkpoint_frequency=-1,
                                 results=None, n_cores=0):
-    
+    '''
+    Args
+    - model_file: str, path to .pb model file
+    - image_file_names: list of str, paths to image files
+    - checkpoint_path: str, path to JSON checkpoint file
+    - confidence_threshold: float, only detections above this threshold are returned
+    - checkpoint_frequency: int, write results to JSON checkpoint file every N images
+    - results: list of dict, existing results loaded from checkpoint
+    - n_cores: int, # of CPU cores to use
+
+    Returns
+    - results: list of dict, each dict represents detections on one image
+    '''
     if results is None:
         results = []
-        
+
     already_processed = set([i['file'] for i in results])
-        
+
     if n_cores > 1 and tf.test.is_gpu_available():
         print('Warning: multiple cores requested, but a GPU is available; parallelization across GPUs is not currently supported, defaulting to one GPU')
-    
+
     # If we're not using multiprocessing...
     if n_cores <= 1 or tf.test.is_gpu_available():
+
         # Load the detector
         start_time = time.time()
         tf_detector = TFDetector(model_file)
         elapsed = time.time() - start_time
-        print('Loaded model in {}'.format(humanfriendly.format_timespan(elapsed)))    
-    else:
-        # If we're using multiprocessing, let the workers load the model, just store
-        # the model filename.
-        tf_detector = model_file
+        print('Loaded model in {}'.format(humanfriendly.format_timespan(elapsed)))
 
-    if n_cores <= 1 or tf.test.is_gpu_available():
-        
         # Does not count those already processed
-        count = 0  
-        
+        count = 0
+
         for im_file in tqdm(image_file_names):
-            
+
             # Will not add additional entries not in the starter checkpoint
-            if im_file in already_processed:  
+            if im_file in already_processed:
                 print('Bypassing image {}'.format(im_file))
                 continue
-    
+
             count += 1
-    
-            try:
-                image = viz_utils.load_image(im_file)
-            except Exception as e:
-                print('Image {} cannot be loaded. Exception: {}'.format(im_file, e))
-                result = {
-                    'file': im_file,
-                    'failure': TFDetector.FAILURE_IMAGE_OPEN
-                }
-                results.append(result)
-                continue
-    
-            try:
-                result = tf_detector.generate_detections_one_image(image, im_file, detection_threshold=confidence_threshold)
-                results.append(result)
-    
-            except Exception as e:
-                print('An error occurred while running the detector on image {}. Exception: {}'.format(im_file, e))
-                result = {
-                    'file': im_file,
-                    'failure': TFDetector.FAILURE_IMAGE_INFER
-                }
-                results.append(result)
-                continue
-    
+
+            result = process_image(im_file, tf_detector, confidence_threshold)
+            results.append(result)
+
             # checkpoint
             if checkpoint_frequency != -1 and count % checkpoint_frequency == 0:
                 print('Writing a new checkpoint after having processed {} images since last restart'.format(count))
                 with open(checkpoint_path, 'w') as f:
                     json.dump({'images': results}, f)
+
     else:
+        # when using multiprocessing, let the workers load the model
+        tf_detector = model_file
+
         print('Creating pool with {} cores'.format(n_cores))
-        
+
         if len(already_processed) > 0:
             print('Warning: when using multiprocessing, all images are reprocessed')
-            
+
         pool = workerpool(n_cores)
-        
-        image_batches = list(chunks_by_number_of_chunks(image_file_names,n_cores))
-        results = pool.map(partial(process_images, tf_detector=tf_detector, 
-                                    confidence_threshold=confidence_threshold), image_batches)
+
+        image_batches = list(chunks_by_number_of_chunks(image_file_names, n_cores))
+        results = pool.map(partial(process_images, tf_detector=tf_detector,
+                                   confidence_threshold=confidence_threshold), image_batches)
 
         results = list(itertools.chain.from_iterable(results))
-        
-    # This was modified in place, but we also return it for backwards-compatibility.
-    return results 
+
+    # results may have been modified in place, but we also return it for backwards-compatibility.
+    return results
 
 
 def write_results_to_file(results, output_file, relative_path_base=None):
+    '''Writes list of detection results to JSON output file. Format matches
+    https://github.com/microsoft/CameraTraps/tree/master/api/batch_processing#batch-processing-api-output-format
+
+    Args
+    - results: list of dict, each dict represents detections on one image
+    - output_file: str, path to JSON output file, should end in '.json'
+    - relative_path_base: str, path to a directory as the base for relative paths
     '''
-    Write the list of detection results in *results* to the .json file 
-    *output_file*, optionally making filenames relative to *relative_path_base*.
-    '''
-    
     if relative_path_base is not None:
         results_relative = []
         for r in results:
@@ -233,12 +238,10 @@ def write_results_to_file(results, output_file, relative_path_base=None):
         json.dump(final_output, f, indent=1)
     print('Output file saved at {}'.format(output_file))
 
-    
 
-#%% Command-line driver
+# %% Command-line driver
 
 def main():
-
     parser = argparse.ArgumentParser(
         description='Module to run a TF animal detection model on lots of images')
     parser.add_argument(
@@ -246,10 +249,10 @@ def main():
         help='Path to .pb TensorFlow detector model file')
     parser.add_argument(
         'image_file',
-        help='Can be a single image file, a json file containing a list of paths to images, or a directory')
+        help='Path to a single image file, a JSON file containing a list of paths to images, or a directory')
     parser.add_argument(
         'output_file',
-        help='Output results file, should end with a .json extension')
+        help='Path to output JSON results file, should end with a .json extension')
     parser.add_argument(
         '--recursive',
         action='store_true',
@@ -270,13 +273,13 @@ def main():
         help='Write results to a temporary file every N images; default is -1, which disables this feature')
     parser.add_argument(
         '--resume_from_checkpoint',
-        help='Initiate from the specified checkpoint, which is in the same directory as the output_file specified')
+        help='Path to a JSON checkpoint file to resume from, must be in same directory as output_file')
     parser.add_argument(
         '--ncores',
         type=int,
         default=0,
         help='Number of cores to use; only applies to CPU-based inference, does not support checkpointing when ncores > 1')
-    
+
     if len(sys.argv[1:]) == 0:
         parser.print_help()
         parser.exit()
@@ -289,14 +292,14 @@ def main():
     if args.checkpoint_frequency != -1:
         assert args.checkpoint_frequency > 0, 'Checkpoint_frequency needs to be > 0 or == -1'
     if args.output_relative_filenames:
-        assert os.path.isdir(args.image_file), 'Since output_relative_filenames is set, image_file needs to be a directory'
+        assert os.path.isdir(args.image_file), 'image_file must be a directory when --output_relative_filenames is set'
 
     if os.path.exists(args.output_file):
         print('Warning: output_file {} already exists and will be overwritten'.format(args.output_file))
 
     # Load the checkpoint if available
     #
-    # Relative file names are only output at the end; all file paths in the checkpoint are 
+    # Relative file names are only output at the end; all file paths in the checkpoint are
     # still full paths.
     if args.resume_from_checkpoint:
         assert os.path.exists(args.resume_from_checkpoint), 'File at resume_from_checkpoint specified does not exist'
@@ -323,17 +326,17 @@ def main():
         image_file_names = [args.image_file]
         print('A single image at {} is the input file'.format(args.image_file))
     else:
-        print('image_file specified is not a directory, a json list or an image file (or does not have recognizable extensions), exiting.')
-        sys.exit(1)
+        raise ValueError('image_file specified is not a directory, a json list, or an image file, '
+                         '(or does not have recognizable extensions).')
 
     assert len(image_file_names) > 0, 'Specified image_file does not point to valid image files'
     assert os.path.exists(image_file_names[0]), 'The first image to be scored does not exist at {}'.format(image_file_names[0])
 
     output_dir = os.path.dirname(args.output_file)
-        
+
     assert os.path.exists(output_dir), 'Invalid output filename (folder does not exist)'
     assert not os.path.isdir(args.output_file), 'Specified output file is a directory'
-    
+
     # Test that we can write to the output_file's dir if checkpointing requested
     if args.checkpoint_frequency != -1:
         checkpoint_path = os.path.join(output_dir, 'checkpoint_{}.json'.format(datetime.utcnow().strftime("%Y%m%d%H%M%S")))
@@ -360,11 +363,11 @@ def main():
     if args.output_relative_filenames:
         relative_path_base = args.image_file
     write_results_to_file(results, args.output_file, relative_path_base=relative_path_base)
-    
+
     if checkpoint_path:
         os.remove(checkpoint_path)
         print('Deleted checkpoint file')
-        
+
     print('Done!')
 
 
