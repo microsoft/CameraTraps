1756d58404a1493501d661a41f88e7d4a2f89b3a
Siyu Yang <yasiyu@microsoft.com>
2020-07-06 15:38:45 -0700

    Sync API - separate the check for no payload sent. (#182)
    Former-commit-id: 094feb3abe847c005d2e5a85aadc4a226ee5e271

--- api/synchronous/api_core/build_docker.sh
+++ api/synchronous/api_core/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# these dependency files are outside of the Docker context, so cannot COPY in the Dockerfile
+# these dependency files are outside of the Docker context, so cannot use the COPY action in the Dockerfile to copy them into the Docker image.
 
 # this is the main dependency
 cp ../../../detection/run_tf_detector.py animal_detection_classification_api/
@@ -17,7 +17,7 @@ mkdir animal_detection_classification_api/data_management/annotations/
 cp ../../../data_management/annotations/annotation_constants.py animal_detection_classification_api/data_management/annotations/
 
 
-# MODIFY the version and build numbers as needed
+# Modify the version and build numbers as needed in the API_DOCKER_IMAGE argument passed to this script
 echo $1
 sudo docker build . -t $1
 
