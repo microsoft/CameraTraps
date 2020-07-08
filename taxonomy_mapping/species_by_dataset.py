afa1f283ff8941d096543bf83e09a654cc17ec6b
Chris Yeh <chrisyeh96@users.noreply.github.com>
2020-07-07 23:10:51 -0600

    taxonomy_mapping linting

--- taxonomy_mapping/species_by_dataset.py
+++ taxonomy_mapping/species_by_dataset.py
@@ -1,48 +1,56 @@
 """
-This program compiles an Excel spreadsheet for manually mapping dataset-specific species names to
-a common taxonomy.
+This program compiles an Excel spreadsheet for manually mapping dataset-specific
+species names to a common taxonomy.
 
-We are currently doing the counting of species here instead of as a part of the Cosmos DB query
+We are currently doing the counting of species here instead of as a part of the
+Cosmos DB query
 - see SDK issue in notes.
 
-It first goes through the list of datasets in the `datasets` table to find out which "species" are in each
-dataset and the count of its "occurrences" (each sequence is counted as 1 if the class label is on the sequence;
-each image is counted as 1 as well if the class label is on the image level; so a sequence/image count mixture).
-This information is saved in a JSON file in the `output_dir` for each dataset.
+It first goes through the list of datasets in the `datasets` table to find out
+which "species" are in each dataset and the count of its "occurrences" (each
+sequence is counted as 1 if the class label is on the sequence; each image is
+counted as 1 as well if the class label is on the image level; so a
+sequence/image count mixture). This information is saved in a JSON file in the
+`output_dir` for each dataset.
 
-Once this information is collected, for each "species" in a dataset, it queries the TOP 100 sequences
-where the "species" is in the list of class names at either the sequence or the image level. It samples
-7 of these TOP 500 sequences (sequences returned by TOP may have little variety) and from each sequence
-samples an image to surface as an example. The spreadsheet is then prepared, adding a Bing search URL
-with the species class name as the query string and fields to filter and fill in Excel.
+Once this information is collected, for each "species" in a dataset, it queries
+the TOP 100 sequences where the "species" is in the list of class names at
+either the sequence or the image level. It samples 7 of these TOP 500 sequences
+(sequences returned by TOP may have little variety) and from each sequence
+samples an image to surface as an example. The spreadsheet is then prepared,
+adding a Bing search URL with the species class name as the query string and
+fields to filter and fill in Excel.
 
-Because querying for all species present in a dataset may take a long time, a dataset is only queried
-if it does not yet have a JSON file in the `output_dir`.
+Because querying for all species present in a dataset may take a long time, a
+dataset is only queried if it does not yet have a JSON file in the `output_dir`.
 
-Also, the spreadsheet creation step is only done for datasets in DATASETS_TO_INCLUDE_IN_SPREADSHEET specified below.
-This is usually the datasets just ingested that need their species names mapped next.
+Also, the spreadsheet creation step is only done for datasets in
+DATASETS_TO_INCLUDE_IN_SPREADSHEET specified below. This is usually the datasets
+just ingested that need their species names mapped next.
 
-Leave out the flag `--query_species` if you only want to prepare the spreadsheet using previously queried
-species presence result.
+Leave out the flag `--query_species` if you only want to prepare the spreadsheet
+using previously queried species presence result.
 
 
 Example invocation:
 ```
-python data_management/megadb/query_and_upsert_examples/species_by_dataset.py --output_dir /Users/siyuyang/Source/temp_data/CameraTrap/megadb_query_results/species_by_dataset_trial --query_species
+python taxonomy_mapping/species_by_dataset.py \
+    --output_dir /Users/siyuyang/Source/temp_data/CameraTrap/megadb_query_results/species_by_dataset_trial \
+    --query_species
 ```
 """
 
 import argparse
-import json
-import os
-import urllib
 from collections import Counter
 from datetime import datetime
+import json
+import os
 from random import sample
+import urllib
 
-import pandas as pd
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
+import pandas as pd
 from tqdm import tqdm
 
 from data_management.megadb.megadb_utils import MegadbUtils
@@ -51,19 +59,28 @@ from data_management.megadb.megadb_utils import MegadbUtils
 NUMBER_EXAMPLES_PER_SPECIES = 7
 NUMBER_SEQUENCES_TO_QUERY = 500
 
-DATASETS_TO_INCLUDE_IN_SPREADSHEET = ['ena24', 'sulross_2018', 'sulross_2019_spring', 'sulross_kitfox', 'idfg_swwlf_2019']
+DATASETS_TO_INCLUDE_IN_SPREADSHEET = [
+    'ena24',
+    'sulross_2018',
+    'sulross_2019_spring',
+    'sulross_kitfox',
+    'idfg_swwlf_2019'
+]
 
 
 def query_species_by_dataset(megadb_utils, output_dir):
     # which datasets are already processed?
     queried_datasets = os.listdir(output_dir)
-    queried_datasets = set([i.split('.json')[0] for i in queried_datasets if i.endswith('.json')])
+    queried_datasets = set(
+        i.split('.json')[0] for i in queried_datasets if i.endswith('.json')
+    )
 
     datasets_table = megadb_utils.get_datasets_table()
     dataset_names = list(datasets_table.keys())
     dataset_names = [i for i in dataset_names if i not in queried_datasets]
 
-    print(f'{len(queried_datasets)} datasets already queried. Querying species in {len(dataset_names)} datasets...')
+    print(f'{len(queried_datasets)} datasets already queried. Querying species '
+          f'in {len(dataset_names)} datasets...')
 
     for dataset_name in dataset_names:
         print(f'Querying dataset {dataset_name}...')
@@ -71,24 +88,33 @@ def query_species_by_dataset(megadb_utils, output_dir):
         query_seq_level = '''
         SELECT VALUE seq.class
         FROM seq
-        WHERE ARRAY_LENGTH(seq.class) > 0 AND NOT ARRAY_CONTAINS(seq.class, "empty") AND NOT ARRAY_CONTAINS(seq.class, "__label_unavailable")
+        WHERE ARRAY_LENGTH(seq.class) > 0
+            AND NOT ARRAY_CONTAINS(seq.class, "empty")
+            AND NOT ARRAY_CONTAINS(seq.class, "__label_unavailable")
         '''
 
-        results = megadb_utils.query_sequences_table(query_seq_level, partition_key=dataset_name)
+        results = megadb_utils.query_sequences_table(
+            query_seq_level, partition_key=dataset_name)
 
         counter = Counter()
         for i in results:
             counter.update(i)
 
-        # cases when the class field is on the image level (images in a sequence had different class labels)
+        # cases when the class field is on the image level (images in a sequence
+        # that had different class labels)
         # 'caltech' dataset is like this
         query_image_level = '''
         SELECT VALUE seq.images
         FROM sequences seq
-        WHERE (SELECT VALUE COUNT(im) FROM im IN seq.images WHERE ARRAY_LENGTH(im.class) > 0) > 0
+        WHERE (
+            SELECT VALUE COUNT(im)
+            FROM im IN seq.images
+            WHERE ARRAY_LENGTH(im.class) > 0
+        ) > 0
         '''
 
-        results_im = megadb_utils.query_sequences_table(query_image_level, partition_key=dataset_name)
+        results_im = megadb_utils.query_sequences_table(
+            query_image_level, partition_key=dataset_name)
         for seq_images in results_im:
             for im in seq_images:
                 if 'class' in im:
@@ -101,17 +127,21 @@ def query_species_by_dataset(megadb_utils, output_dir):
 def get_example_images(megadb_utils, dataset_name, class_name):
     datasets_table = megadb_utils.get_datasets_table()
 
-    query_both_levels = '''
-    SELECT TOP {} VALUE seq
+    query_both_levels = f'''
+    SELECT TOP {NUMBER_SEQUENCES_TO_QUERY} VALUE seq
     FROM seq
-    WHERE ARRAY_CONTAINS(seq.class, "{}") OR (SELECT VALUE COUNT(im) FROM im IN seq.images WHERE ARRAY_CONTAINS(im.class, "{}")) > 0
-    '''.format(NUMBER_SEQUENCES_TO_QUERY, class_name, class_name)
+    WHERE ARRAY_CONTAINS(seq.class, "{class_name}")
+        OR (SELECT VALUE COUNT(im)
+            FROM im IN seq.images
+            WHERE ARRAY_CONTAINS(im.class, "{class_name}")) > 0
+    '''
 
-    sequences = megadb_utils.query_sequences_table(query_both_levels, partition_key=dataset_name)
+    sequences = megadb_utils.query_sequences_table(
+        query_both_levels, partition_key=dataset_name)
     sample_seqs = sample(sequences, min(len(sequences), NUMBER_EXAMPLES_PER_SPECIES))  # sample 7 sequences if possible
 
     image_urls = []
-    for i, seq in enumerate(sample_seqs):
+    for seq in sample_seqs:
         sample_image = sample(seq['images'], 1)[0]  # sample one image from each sequence
         img_path = sample_image['file']
 
@@ -139,7 +169,8 @@ def make_spreadsheet(megadb_utils, output_dir):
 
     species_by_dataset = {}
 
-    classes_excluded = ['empty', 'car', 'vehicle', 'unidentified', 'unknown', '__label_unavailable', 'error']
+    classes_excluded = ['empty', 'car', 'vehicle', 'unidentified', 'unknown',
+                        '__label_unavailable', 'error']
 
     # read species presence info from the JSON files for each dataset
     for file_name in os.listdir(output_dir):
@@ -236,14 +267,13 @@ def make_spreadsheet(megadb_utils, output_dir):
 
     for i_example, ws_col in enumerate(sas_cols):
         for i_row, cell in enumerate(ws_col):
-            if i_row > 0:
-                if cell.value is not None:
-                    if not isinstance(cell.value, str):
-                        print(f'WARNING cell.value is {cell.value}, type is {type(cell.value)}')
-                        continue
-                    cell.hyperlink = cell.value
-                    cell.value = f'example{i_example + 1}'
-                    cell.style = 'Hyperlink'
+            if i_row > 0 and cell.value is not None:
+                if not isinstance(cell.value, str):
+                    print(f'WARNING cell.value is {cell.value}, type is {type(cell.value)}')
+                    continue
+                cell.hyperlink = cell.value
+                cell.value = f'example{i_example + 1}'
+                cell.style = 'Hyperlink'
 
     date = datetime.now().strftime('%Y_%m_%d')
     wb.save(os.path.join(output_dir, f'species_by_dataset_{date}.xlsx'))
@@ -251,13 +281,14 @@ def make_spreadsheet(megadb_utils, output_dir):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_dir',
-                        required=True,
-                        help='Path to directory where the JSONs containing species count for each dataset live')
-    parser.add_argument('--query_species',
-                        action='store_true',
-                        help=('If flagged, query what species are present in a dataset. '
-                              'Otherwise, create a spreadsheet for labeling the taxonomy'))
+    parser.add_argument(
+        '--output_dir', required=True,
+        help='Path to directory where the JSONs containing species count for '
+             'each dataset live')
+    parser.add_argument(
+        '--query_species', action='store_true',
+        help='If flagged, query what species are present in a dataset. '
+             'Otherwise, create a spreadsheet for labeling the taxonomy')
     args = parser.parse_args()
 
     assert 'COSMOS_ENDPOINT' in os.environ and 'COSMOS_KEY' in os.environ
@@ -268,7 +299,6 @@ def main():
 
     if args.query_species:
         query_species_by_dataset(megadb_utils, args.output_dir)
-
     make_spreadsheet(megadb_utils, args.output_dir)
 
 
