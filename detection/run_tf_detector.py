eb91f835de5bb314a31eb96bba966d77237ba297
Siyu Yang <yasiyu@microsoft.com>
2020-07-22 14:23:47 -0700

    Fix/sync api coords (#198)
    * Fix bug in sync API coordinates.
    
    Former-commit-id: b5ea75efa0c1dfd94bd30595a56eaf3ab727b513

--- detection/run_tf_detector.py
+++ detection/run_tf_detector.py
@@ -180,10 +180,18 @@ class TFDetector:
 
     @staticmethod
     def convert_to_tf_coords(array):
-        """From [x1, y1, width, height] to [y1, x1, y2, x2]"""
-        x2 = array[0] + array[2]
-        y2 = array[1] + array[3]
-        return [array[0], array[1], x2, y2]
+        """From [x1, y1, width, height] to [y1, x1, y2, x2], where x1 is x_min, x2 is x_max
+
+        This is an extraneous step as the model outputs [y1, x1, y2, x2] but were converted to the API
+        output format - only to keep the interface of the sync API.
+        """
+        x1 = array[0]
+        y1 = array[1]
+        width = array[2]
+        height = array[3]
+        x2 = x1 + width
+        y2 = y1 + height
+        return [y1, x1, y2, x2]
 
     @staticmethod
     def __load_model(model_path):
