17ba04e05e18cd6b89ba681a6178c7a3e6a6d09b
Dan Morris <dmorris@cs.stanford.edu>
2020-08-02 14:19:36 -0700

    created initial auckland test data importer (unfinished)

--- data_management/importers/auckland_doc_test_to_json.py
+++ data_management/importers/auckland_doc_test_to_json.py
@@ -1,8 +1,9 @@
 #
-# auckland_doc_to_json.py
+# auckland_doc_test_to_json.py
 #
 # Convert Auckland DOC data set to COCO camera traps format.  This was
-# for a training data set where class names were encoded in path names.
+# for a testing data set where a .csv file was provided with class
+# information.
 #
 
 #%% Constants and imports
@@ -10,17 +11,23 @@
 import json
 import os
 import uuid
+import pandas as pd
 import datetime
+import ntpath
+import numpy as np
 from tqdm import tqdm
 
 from visualization import visualize_db
 from data_management.databases import sanity_check_json_db
 from path_utils import find_images, split_path, insert_before_extension
 
+input_base_dir = r'e:\auckland-test\2_Testing'
+
+input_metadata_file = r'G:\auckland-doc\Maukahuka - Auckland Island - Cat camera data Master April 2019 - DOC-5924483.xlsx'
+
 # Filenames will be stored in the output .json relative to this base dir
-input_base_dir = 'y:\\'
-output_base_dir = r'f:\auckland-doc'
-output_json_filename = os.path.join(output_base_dir, 'auckland-doc-Maukahuka_Auckland_Island.json')
+output_base_dir = r'g:\auckland-doc'
+output_json_filename = os.path.join(output_base_dir, 'auckland-doc-test.json')
 
 assert os.path.isdir(input_base_dir)
 os.makedirs(output_base_dir,exist_ok=True)
@@ -29,9 +36,9 @@ output_encoding = 'utf-8'
 read_image_sizes = True
 
 info = {}
-info['year'] = 2019
+info['year'] = 2020
 info['version'] = '1.0'
-info['description'] = 'Auckaland DOC Camera Traps'
+info['description'] = 'Auckaland DOC Camera Traps (test)'
 info['contributor'] = 'Auckland DOC'
 info['date_created'] = str(datetime.date.today())
 
@@ -39,8 +46,28 @@ info['date_created'] = str(datetime.date.today())
 #%% Enumerate files
 
 print('Enumerating files from {}'.format(input_base_dir))
-image_files = find_images(input_base_dir, bRecursive=True)
-print('Enumerated {} images'.format(len(image_files)))
+absolute_image_paths = find_images(input_base_dir, recursive=True)
+print('Enumerated {} images'.format(len(absolute_image_paths)))
+
+relative_image_paths = []
+for fn in absolute_image_paths:
+    relative_image_paths.append(os.path.relpath(fn,input_base_dir).replace('\\','/'))
+
+relative_image_paths_set = set(relative_image_paths)
+
+# The excel file uses only filenames, not full paths; store just the filename
+filename_to_relative_path = {}
+for relative_path in relative_image_paths:
+    fn = ntpath.basename(relative_path)
+    filename_to_relative_path[fn] = relative_path
+    
+
+#%% Load input data
+
+input_metadata = pd.read_excel(input_metadata_file)
+
+print('Read {} columns and {} rows from metadata file'.format(len(input_metadata.columns),
+      len(input_metadata)))
 
 
 #%% Assemble dictionaries
@@ -51,38 +78,92 @@ annotations = []
 categories = []
 
 category_name_to_category = {}
-category_id_to_category = {}
 
 # Force the empty category to be ID 0
 empty_category = {}
 empty_category['name'] = 'empty'
 empty_category['id'] = 0
-category_id_to_category[0] = empty_category
+category_name_to_category['empty'] = empty_category
 categories.append(empty_category)
+
 next_id = 1
 
-behaviors = set()
+category_names = ['cat','mouse','unknown','human','pig','sealion','penguin','dog']
 
-# fn = image_files[0]; print(fn)
-for fn in tqdm(image_files):
+# array([nan, 'Blackbird', 'Bellbird', 'Tomtit', 'Song thrush', 'Pippit',
+#       'Pippet', '?', 'Dunnock', 'Song thursh', 'Kakariki', 'Tui', ' ',
+#       'Silvereye', 'NZ Pipit', 'Blackbird and Dunnock', 'Unknown',
+#       'Pipit', 'Songthrush'], dtype=object)
 
-    # Typically y:\Maukahuka_Auckland_Island\1_Training\Winter_Trial_2019\cat\cat\eat\20190903_IDdY_34_E3_tmp_201908240051.JPG
-    relative_path = os.path.relpath(fn,input_base_dir)
-    tokens = split_path(fn)
-    assert tokens[1] == 'Maukahuka_Auckland_Island'
-    
-    trainval_split = tokens[2]
-    assert trainval_split in ['1_Training','2_Testing']
-    
-    # This data set has two top-level folders, "1_Training" (which has class names encoded
-    # in paths) and "2_Testing" (which has no class information).
-    if trainval_split == '2_Testing':
-        category_name = 'test'
+bird_names = input_metadata.Bird_ID.unique()
+for bird_name in bird_names:
+    if isinstance(bird_name,float):
+        continue
+    bird_name = bird_name.lower().strip().replace(' ','_').replace('song_thursh','song_thrush')
+    if bird_name == '?' or bird_name == '' or bird_name == 'unknown':
+        category_name = 'unknown_bird'
     else:
-        category_name = tokens[-3]
-        if category_name.startswith('2_'):
-            category_name = category_name.replace('2_', '')
-        category_name = category_name.lower().strip()
+        category_name = bird_name
+    if category_name not in category_names:
+        category_names.append(category_name)
+        
+for category_name in category_names:
+    cat = {}
+    cat['name'] = category_name
+    cat['id'] = next_id
+    next_id = next_id +1
+    category_name_to_category[category_name] = cat
+    
+filenames_not_in_folder = []
+
+def create_annotation(image_id,category_name,count):
+    ann = {}    
+    ann['id'] = str(uuid.uuid1())
+    ann['image_id'] = image_id
+    category = category_name_to_category[category_name]
+    category_id = category['id']
+    ann['category_id'] = category_id
+    ann['count'] = count
+    return ann
+    
+# i_row = 0; row = input_metadata.iloc[i_row]
+for i_row,row in input_metadata.iterrows():
+
+    # E.g.: AucklandIsland_A1_1_42_SD114_20190210_01300009.jpg
+    filename = row['File']
+    if not filename in filename_to_relative_path:
+        filenames_not_in_folder.append(filename)
+        continue        
+
+    assert filename.endswith('.jpg') or filename.endswith('.JPG')
+    image_id = filename.lower().replace('.jpg','')
+
+    if image_id in image_id_to_image:
+        print('Warning: ignoring duplicate entry for {}'.format(image_id))
+        continue
+    
+    relative_path = filename_to_relative_path[filename]
+    
+    im = {}
+    im['id'] = image_id 
+    im['file_name'] = relative_path
+    im['datetime'] = str(row['Date and time'])
+    # Not a typo; the spreadsheet has a space after "Camera"
+    im['camera'] = row['Camera ']
+    im['sd_card'] = row['SD_Card']
+    im['sd_change'] = row['SD_Change']
+    im['comments'] = row['Comments']
+    
+    image_id_to_image[im['id']] = im
+    
+    images.append(im)
+    
+    if (not np.isnan(row['Cat'])):
+       assert np.isnan(row['Collared_cat'] )
+       ann = create_annotation(im['id'],'cat',row['Cat'])
+    
+    
+#%%    
 
     if category_name not in category_name_to_category:        
 
@@ -108,13 +189,6 @@ for fn in tqdm(image_files):
         behavior = fn.split('\\')[-2]
         behaviors.add(behavior)            
 
-    im = {}
-    im['id'] = str(uuid.uuid1())
-    im['file_name'] = relative_path
-    image_id_to_image[im['id']] = im
-    
-    images.append(im)
-    
     ann = {}
     
     ann['id'] = str(uuid.uuid1())
