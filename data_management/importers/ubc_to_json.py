fb7192bed8c6f0bb0f3ee6299c4051f5576e064b
Dan Morris <dmorris@cs.stanford.edu>
2020-08-03 12:19:32 -0700

    ic/ubc data import

--- data_management/importers/ubc_to_json.py
+++ data_management/importers/ubc_to_json.py
@@ -4,6 +4,11 @@
 # Convert the .csv file provided for the UBC data set to a 
 # COCO-camera-traps .json file
 #
+# Images were provided in eight folders, each of which contained a .csv
+# file with annotations.  Those annotations came in two slightly different 
+# formats, the two formats corresponding to folders starting with "SC_" and 
+# otherwise.
+#
 
 #%% Constants and environment
 
@@ -20,12 +25,13 @@ from tqdm import tqdm
 
 from path_utils import find_images
 
-input_base = r'/mnt/blobfuse/wildlifeblobssc/'
+input_base = r'e:\ubc'
+assert(os.path.isdir(input_base))
 
-output_base = r'/home/gramener/ubc/'
+output_base = r'f:\data_staging\ubc'
 output_json_file = os.path.join(output_base,'ubc.json')
-                        
 os.makedirs(output_base,exist_ok=True)
+
 mapped_fields = {"Survey.Name" : "survey_name",
                  "project_id": "survey_name",
                  "Camera.Name": "camera_name",
@@ -36,20 +42,27 @@ mapped_fields = {"Survey.Name" : "survey_name",
                  "Date.Time": "datetime",
                  "Species": "species",
                  "latin_name": "species",
-                 "common.name": "common_names",
-                 "common_names": "common_names",
+                 "common.name": "common_name",
+                 "common_names": "common_name",
                  "Sighting.Quantity": "species_count"
                 }
-target_fields = ['common_names', 'species_count', 'group_count', 'behaviour']
+
+target_fields = ['common_name', 'species_count', 'group_count', 'behavior']
+
+
+#%% Enumerate images
+
+image_full_paths = find_images(input_base, recursive=True)
+image_full_paths_set = set(image_full_paths)
+print('Enumerated {} images from {}'.format(len(image_full_paths),input_base))
+
 
 #%% Create CCT dictionaries
 
 images = []
 annotations = []
 categories = []
-
 image_ids_to_images = {}
-
 category_name_to_category = {}
 
 # Force the empty category to be ID 0
@@ -60,43 +73,52 @@ category_name_to_category['empty'] = empty_category
 categories.append(empty_category)
 next_category_id = 1
 
-s_time = time.time()
-for _folder in os.listdir(input_base):
-    print(_folder)
-    start_time = time.time()
+folders = os.listdir(input_base)
 
+# i_folder = 0; folder = folders[i_folder]; print(folder)
+for i_folder,folder in enumerate(folders):
+    
+    print('Processing folder {} of {}: {}'.format(i_folder,len(folders),folder))
+    
     filenames_to_rows = {}
     filenames_with_multiple_annotations = []
     missing_images = []
-    image_directory = os.path.join(input_base, _folder)
+    image_directory = os.path.join(input_base, folder)
     files = os.listdir(image_directory)
     files = list(filter(lambda f: f.endswith('.csv'), files))
-    input_metadata_file = os.path.join(input_base, _folder, files[0])
+    input_metadata_file = os.path.join(input_base, folder, files[0])
     assert(os.path.isfile(input_metadata_file))
-    # Read Source Data
+    
+    # Read source data for this folder
     input_metadata = pd.read_csv(input_metadata_file)
-    input_metadata.rename(columns= mapped_fields, inplace= True)
+    
+    # Rename columns
+    input_metadata.rename(columns=mapped_fields, inplace= True)
     print('Read {} columns and {} rows from metadata file'.format(len(input_metadata.columns), len(input_metadata)))
-    if _folder.startswith("SC_"):
-        input_metadata['image_name'] = input_metadata[['camera_name', 'filename']].apply(lambda x: os.path.join(_folder, x[0].lower(), x[1].replace(".JPG", ".jpg")), axis = 1)
+    
+    # Create relative path names from camera name and filename
+    if folder.startswith("SC_"):
+        input_metadata['image_relative_path'] = input_metadata[['camera_name', 'filename']].apply(
+            lambda x: os.path.join(folder, x[0].lower(), x[1].replace(".JPG", ".jpg")), axis = 1)
     else:
-        input_metadata['image_name'] = input_metadata[['camera_name', 'filename']].apply(lambda x: os.path.join(_folder, x[0], x[1]), axis = 1)
-    for i_row, fn in tqdm(enumerate(input_metadata['image_name']), total=len(input_metadata)):
-        # print(fn)       
+        input_metadata['image_relative_path'] = input_metadata[['camera_name', 'filename']].apply(
+            lambda x: os.path.join(folder, x[0], x[1]), axis = 1)
+
+    # i_row = 0; fn = input_metadata['image_relative_path'][i_row]
+    for i_row, fn in tqdm(enumerate(input_metadata['image_relative_path']), total=len(input_metadata)):
+        
         if fn in filenames_to_rows:
             filenames_with_multiple_annotations.append(fn)
             filenames_to_rows[fn].append(i_row)
         else:
             filenames_to_rows[fn] = [i_row]
             image_path = os.path.join(input_base, fn)
-            # print(image_path)
+            
             if not os.path.isfile(image_path):
                 missing_images.append(image_path)
         
-    elapsed = time.time() - start_time
-    print('Finished verifying image existence for {} files in {}, found {} filenames with multiple labels, {} missing images'.format(
-        len(filenames_to_rows), humanfriendly.format_timespan(elapsed),
-        len(filenames_with_multiple_annotations), len(missing_images)))
+    print('Finished verifying image existence for {} files, found {} filenames with multiple labels, {} missing images'.format(
+        len(filenames_to_rows), len(filenames_with_multiple_annotations), len(missing_images)))
     
     #%% Check for images that aren't included in the metadata file
 
@@ -112,7 +134,7 @@ for _folder in os.listdir(input_base):
 
     print('Finished checking {} images to make sure they\'re in the metadata, found {} unannotated images'.format(
             len(image_full_paths),len(unannotated_images)))
-    print("Adding entries to CCT JSON file for Survey {0}".format(_folder))
+    print("Adding entries to CCT JSON file for Survey {0}".format(folder))
     for image_name in tqdm(list(filenames_to_rows.keys())):
         img_id = image_name.replace('\\','/').replace('/','_').replace(' ','_')
         row_indices = filenames_to_rows[image_name]
