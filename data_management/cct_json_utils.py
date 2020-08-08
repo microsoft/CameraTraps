e9ee68f8a51a3bd4a4f0cf30a45f6898ad9566cd
Christopher Yeh <chrisyeh96@users.noreply.github.com>
2020-08-08 01:32:01 +0000

    vis cleanup

--- data_management/cct_json_utils.py
+++ data_management/cct_json_utils.py
@@ -1,22 +1,18 @@
-#######
-#
-# cct_json_utils.py
-#
-# Utilities for working with COCO Camera Traps .json databases
-#
-# Format spec:
-#
-# https://github.com/Microsoft/CameraTraps/blob/master/data_management/README.md#coco-cameratraps-format
-#
-#######
+"""
+Utilities for working with COCO Camera Traps .json databases
 
+https://github.com/Microsoft/CameraTraps/blob/master/data_management/README.md#coco-cameratraps-format
+"""
 
 #%% Constants and imports
 
-import os
-import json
 from collections import defaultdict, OrderedDict
+import json
+import os
+from typing import Any, Dict, Iterable, List, Mapping, Optional, Union
 
+
+JSONObject = Mapping[str, Any]
 
 #%% Classes
 
@@ -42,20 +38,22 @@ class CameraTrapJsonUtils:
         # Collect all names
         class_names = [cat_id_to_name[ann['category_id']] for ann in annotations]
         # Make names unique and sort
-        class_names = sorted(list(set(class_names)))
+        class_names = sorted(set(class_names))
         return class_names
 
     @staticmethod
-    def order_db_keys(db):
+    def order_db_keys(db: JSONObject) -> OrderedDict:
         """
-        Given a dict representing a JSON database in the COCO Camera Trap format, return an
-        OrderedDict with keys in the order of 'info', 'categories', 'annotations' and 'images'.
-        When this OrderedDict is serialized with json.dump(), the order of the keys are preserved.
+        Given a dict representing a JSON database in the COCO Camera Trap
+        format, return an OrderedDict with keys in the order of 'info',
+        'categories', 'annotations' and 'images'. When this OrderedDict is
+        serialized with json.dump(), the order of the keys are preserved.
+
         Args:
-            db: a dict representing a JSON database in the COCO Camera Trap format
+            db: dict representing a JSON database in the COCO Camera Trap format
 
         Returns:
-            the same db but as an OrderedDict with keys ordered for readability.
+            the same db but as an OrderedDict with keys ordered for readability
         """
         ordered = OrderedDict([
             ('info', db['info']),
@@ -82,7 +80,8 @@ class CameraTrapJsonUtils:
         return res
 
     @staticmethod
-    def get_entries_from_locations(db, locations):
+    def get_entries_from_locations(db: JSONObject, locations: Iterable[str]
+                                   ) -> Dict[str, Any]:
         """
         Given a dict representing a JSON database in the COCO Camera Trap format, return a dict
         with the 'images' and 'annotations' fields in the CCT format, each is an array that only
@@ -96,7 +95,7 @@ class CameraTrapJsonUtils:
         """
         locations = set(locations)
         print('Original DB has {} image and {} annotation entries.'.format(len(db['images']), len(db['annotations'])))
-        new_db = {
+        new_db: Dict[str, Any] = {
             'images': [],
             'annotations': []
         }
@@ -122,96 +121,100 @@ class IndexedJsonDb:
     a .json database.
     """
 
-    def __init__(self, json_filename, b_normalize_paths=False, 
-                 filename_replacements={}, b_convert_classes_to_lower=True):
+    def __init__(self, json_filename: Union[str, JSONObject],
+                 b_normalize_paths: bool = False,
+                 filename_replacements: Optional[Mapping[str, str]] = None,
+                 b_convert_classes_to_lower: bool = True):
         '''
         json_filename can also be an existing json db
         '''
-
         if isinstance(json_filename, str):
             self.db = json.load(open(json_filename))
         else:
             self.db = json_filename
 
-        assert 'images' in self.db, 'Could not find image list in file {}, are you sure this is a COCO camera traps file?'.format(
-            json_filename)
+        assert 'images' in self.db, (
+            f'Could not find image list in file {json_filename}, are you sure '
+            'this is a COCO camera traps file?')
 
         if b_convert_classes_to_lower:
             # Convert classnames to lowercase to simplify comparisons later
             for c in self.db['categories']:
                 c['name'] = c['name'].lower()
-                
+
         if b_normalize_paths:
             # Normalize paths to simplify comparisons later
             for im in self.db['images']:
                 im['file_name'] = os.path.normpath(im['file_name'])
 
-        for s in filename_replacements:
-            # Make custom replacements in filenames, typically used to
-            # accommodate changes in root paths between DB construction and use
-            r = filename_replacements[s]
-            for im in self.db['images']:
-                im['file_name'] = im['file_name'].replace(s, r)
+        if filename_replacements is not None:
+            for s in filename_replacements:
+                # Make custom replacements in filenames, typically used to
+                # accommodate changes in root paths after DB construction
+                r = filename_replacements[s]
+                for im in self.db['images']:
+                    im['file_name'] = im['file_name'].replace(s, r)
 
         ### Build useful mappings to facilitate working with the DB
 
         # Category ID <--> name
-        self.cat_id_to_name = {cat['id']: cat['name'] for cat in self.db['categories']}
-        self.cat_name_to_id = {cat['name']: cat['id'] for cat in self.db['categories']}
+        self.cat_id_to_name = {
+            cat['id']: cat['name'] for cat in self.db['categories']}
+        self.cat_name_to_id = {
+            cat['name']: cat['id'] for cat in self.db['categories']}
 
         # Image filename --> ID
-        self.filename_to_id = {im['file_name']: im['id'] for im in self.db['images']}
-
-        # Each image can potentially multiple annotations, hence using lists
-        self.image_id_to_annotations = defaultdict(list)
+        self.filename_to_id = {
+            im['file_name']: im['id'] for im in self.db['images']}
 
         # Image ID --> image object
         self.image_id_to_image = {im['id']: im for im in self.db['images']}
 
         # Image ID --> annotations
+        # Each image can potentially multiple annotations, hence using lists
+        self.image_id_to_annotations: Dict[str, List[Dict[str, Any]]]
+        self.image_id_to_annotations = defaultdict(list)
         for ann in self.db['annotations']:
             self.image_id_to_annotations[ann['image_id']].append(ann)
 
     # ...__init__
 
-    def get_annotations_for_image(self, image):
+    def get_annotations_for_image(self, image: JSONObject
+                                  ) -> Optional[List[Dict[str, Any]]]:
         """
-        Returns a list of annotations associated with [image]
-        
-        Returns None is the db has not been loaded, [] if no annotations are available
+        Returns: list of annotations associated with an image,
+            None if the db has not been loaded,
+            [] if no annotations are available
         """
-        
         if self.db is None:
             return None
-    
+
         if image['id'] not in self.image_id_to_annotations:
             return []
-        
+
         image_annotations = self.image_id_to_annotations[image['id']]
         return image_annotations
-    
-    
-    def get_classes_for_image(self, image):
+
+
+    def get_classes_for_image(self, image: JSONObject) -> Optional[List[str]]:
         """
         Returns a list of class names associated with [image]
-        
+
         Returns None is the db has not been loaded, [] if no annotations are available
         """
-        
         if self.db is None:
             return None
-    
+
         if image['id'] not in self.image_id_to_annotations:
             return []
-        
+
         class_ids = []
         image_annotations = self.image_id_to_annotations[image['id']]
         for ann in image_annotations:
             class_ids.append(ann['category_id'])
-        class_ids = list(set(class_ids))
-        class_ids.sort()
+        class_ids = sorted(set(class_ids))
         class_names = [self.cat_id_to_name[x] for x in class_ids]
-        
+
         return class_names
 
 # ...class IndexedJsonDb
