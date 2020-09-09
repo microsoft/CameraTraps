604a968873476182db9c6f42d23ac2f44916387d
Chris Yeh <chrisyeh96@users.noreply.github.com>
2020-09-09 03:27:22 -0700

    json_validator.py: refactor to use new networkx-backed taxonomy graph

--- classification/json_validator.py
+++ classification/json_validator.py
@@ -49,9 +49,6 @@ Example usage:
         $HOME/camera-traps-private/camera_trap_taxonomy_mapping.csv \
         --output-dir run --json-indent 2
 """
-# allow forward references in typing annotations
-from __future__ import annotations
-
 import argparse
 from concurrent import futures
 from datetime import datetime
@@ -60,8 +57,8 @@ import os
 import pprint
 import random
 import time
-from typing import (Any, ClassVar, Container, Dict, Iterable, List, Mapping,
-                    Optional, Set, Tuple, Union)
+from typing import (Any, Container, Dict, Iterable, List, Mapping, Optional,
+                    Set, Tuple, Union)
 
 import pandas as pd
 from tqdm import tqdm
@@ -69,132 +66,8 @@ from tqdm import tqdm
 from data_management.megadb import megadb_utils
 import path_utils  # from ai4eutils
 import sas_blob_utils  # from ai4eutils
-
-
-class TaxonNode:
-    r"""A node in a taxonomy tree, associated with a set of dataset labels.
-
-    By default we support multiple parents for each TaxonNode because different
-    taxonomies may have a different granularity of hierarchy. If the taxonomy
-    was created from a mixture of different taxonomies, then we may see the
-    following, for example:
-
-        "eastern gray squirrel" (inat)     "squirrel" (gbif)
-        ------------------------------     -----------------
-    family:                        sciuridae
-                                  /          \
-    subfamily:          sciurinae             |  # skips subfamily
-                                |             |
-    tribe:               sciurini             |  # skips tribe
-                                  \          /
-    genus:                          sciurus
-    """
-    # class variables
-    single_parent_only: ClassVar[bool] = False
-
-    # instance variables
-    level: str
-    name: str
-    ids: Set[Tuple[str, int]]
-    children: List[TaxonNode]
-    parents: List[TaxonNode]
-    dataset_labels: Set[Tuple[str, str]]
-
-    def __init__(self, level: str, name: str):
-        """Initializes a TaxonNode."""
-        self.level = level
-        self.name = name
-        self.ids = set()
-        self.children = []
-        self.parents = []
-        self.dataset_labels = set()
-
-    def __repr__(self):
-        id_str = ', '.join(f'{source}={id}' for source, id in self.ids)
-        return f'TaxonNode({id_str}, level={self.level}, name={self.name})'
-
-    def add_id(self, source: str, taxon_id: int) -> None:
-        assert source in ['gbif', 'inat', 'manual']
-        self.ids.add((source, taxon_id))
-
-    def add_parent(self, parent: TaxonNode) -> None:
-        """Adds a TaxonNode to the list of parents of the current TaxonNode.
-
-        Args:
-            parent: TaxonNode, must be higher in the taxonomical hierarchy
-        """
-        if TaxonNode.single_parent_only and len(self.parents) > 0:
-            assert len(self.parents) == 1
-            assert self.parents[0] is parent, (
-                f'self.parents: {self.parents}, new parent: {parent}')
-            return
-        if parent not in self.parents:
-            self.parents.append(parent)
-            parent.add_child(self)
-
-    def add_child(self, child: TaxonNode) -> None:
-        if child not in self.children:
-            self.children.append(child)
-            child.add_parent(self)
-
-    def add_dataset_label(self, ds: str, ds_label: str) -> None:
-        """
-        Args:
-            ds: str, name of dataset
-            ds_label: str, name of label used by that dataset
-        """
-        self.dataset_labels.add((ds, ds_label))
-
-    def get_dataset_labels(self,
-                           include_datasets: Optional[Container[str]] = None
-                           ) -> Set[Tuple[str, str]]:
-        """Returns a set of all (ds, ds_label) tuples that belong to this taxon
-        node or its descendants.
-
-        Args:
-            include_datasets: list of str, names of datasets to include
-                if None, then all datasets are included
-
-        Returns: set of (ds, ds_label) tuples
-        """
-        result = self.dataset_labels
-        if include_datasets is not None:
-            result = set(tup for tup in result if tup[0] in include_datasets)
-
-        for child in self.children:
-            result |= child.get_dataset_labels(include_datasets)
-        return result
-
-    @classmethod
-    def lowest_common_ancestor(cls, nodes: Iterable[TaxonNode]
-                               ) -> Optional[TaxonNode]:
-        """Returns the lowest common ancestor (LCA) of a list or set of nodes.
-
-        For each node in <nodes>, get the set of nodes on the path to the root.
-        The LCA of <nodes> is certainly in the intersection of these sets.
-        Iterate through the nodes in this set intersection, looking for a node
-        such that none of its children is in this intersection. Given n nodes
-        from a k-ary tree of height h, the algorithm runs in O((n + k)h).
-
-        Returns: TaxonNode, the LCA if it exists, or None if no LCA exists
-        """
-        paths = []
-        for node in nodes:
-            # get path to root
-            path = {node}
-            remaining = list(node.parents)  # make a shallow copy
-            while len(remaining) > 0:
-                x = remaining.pop()
-                if x not in path:
-                    path.add(x)
-                    remaining.extend(x.parents)
-            paths.append(path)
-        intersect = set.intersection(*paths)
-
-        for node in intersect:
-            if intersect.isdisjoint(node.children):
-                return node
-        return None
+from taxonomy_mapping.taxonomy_graph import (
+    build_taxonomy_graph, dag_to_tree, TaxonNode)
 
 
 def main(label_spec_json_path: str,
@@ -219,7 +92,8 @@ def main(label_spec_json_path: str,
     taxonomy_df = pd.read_csv(taxonomy_csv_path)
     if single_parent_taxonomy:
         TaxonNode.single_parent_only = True
-    taxonomy_dict, _ = build_taxonomy_dict(taxonomy_df)
+    graph, taxonomy_dict, _ = build_taxonomy_graph(taxonomy_df)
+    dag_to_tree(graph, taxonomy_dict)
 
     print('Validating input json')
     with open(label_spec_json_path, 'r') as f:
@@ -268,64 +142,6 @@ def main(label_spec_json_path: str,
             for label in sorted(input_js.keys())
         }
         json.dump(image_counts_by_label, f, indent=1)
-
-
-def build_taxonomy_dict(
-        taxonomy_df: pd.DataFrame
-        ) -> Tuple[
-            Dict[Tuple[str, str], TaxonNode],
-            Dict[Tuple[str, str], TaxonNode]
-        ]:
-    """Creates a mapping from (taxon_level, taxon_name) to TaxonNodes, used for
-    gathering all dataset labels associated with a given taxon.
-
-    Args:
-        taxonomy_df: pd.DataFrame, see taxonomy_mapping directory for more info
-
-    Returns:
-        taxon_to_node: dict, maps (taxon_level, taxon_name) to a TaxonNode
-        label_to_node: dict, maps (dataset_name, dataset_label) to the lowest
-            TaxonNode node in the tree that contains the label
-    """
-    taxon_to_node = {}  # maps (taxon_level, taxon_name) to a TaxonNode
-    label_to_node = {}  # maps (dataset_name, dataset_label) to a TaxonNode
-    for _, row in taxonomy_df.iterrows():
-        ds = row['dataset_name']
-        ds_label = row['query']
-        taxa_ancestry = row['taxonomy_string']
-        id_source = row['source']
-        if pd.isna(taxa_ancestry):
-            # taxonomy CSV rows without 'taxonomy_string' entries can only be
-            # added to the JSON via the 'dataset_labels' key
-            continue
-        else:
-            taxa_ancestry = eval(taxa_ancestry)  # pylint: disable=eval-used
-
-        taxon_child: Optional[TaxonNode] = None
-        for i, taxon in enumerate(taxa_ancestry):
-            taxon_id, taxon_level, taxon_name, _ = taxon
-
-            key = (taxon_level, taxon_name)
-            if key not in taxon_to_node:
-                node = TaxonNode(level=taxon_level, name=taxon_name)
-                taxon_to_node[key] = node
-            node = taxon_to_node[key]
-
-            if taxon_child is not None:
-                node.add_child(taxon_child)
-
-            node.add_id(id_source, int(taxon_id))  # np.int64 -> int
-            if i == 0:
-                assert row['taxonomy_level'] == taxon_level, (
-                    f'taxonomy CSV level: {row["taxonomy_level"]}, '
-                    f'level from taxonomy_string: {taxon_level}')
-                assert row['scientific_name'] == taxon_name
-                node.add_dataset_label(ds, ds_label)
-                label_to_node[(ds, ds_label)] = node
-
-            taxon_child = node
-
-    return taxon_to_node, label_to_node
 
 
 def parse_taxa(taxa_dicts: List[Dict[str, str]],
@@ -401,8 +217,9 @@ def validate_json(input_js: Dict[str, Dict[str, Any]],
             include_set -= parse_spec(spec_dict['exclude'], taxonomy_dict)
 
         for label_b, set_b in label_to_inclusions.items():
-            if not include_set.isdisjoint(set_b):
-                print(f'Labels {label} and {label_b} will share images')
+            shared = include_set.intersection(set_b)
+            if len(shared) > 0:
+                print(f'Labels {label} and {label_b} share images:', shared)
                 if not allow_multilabel:
                     raise ValueError('Intersection between sets!')
 
