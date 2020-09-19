78a0dfb0f1a1e6dadb603606540ffe6f27e17c7a
Christopher Yeh <chrisyeh96@users.noreply.github.com>
2020-09-19 08:44:32 +0000

    classifier: merge_classification_detection_output.py now supports merging classification results with single detection.json file

--- classification/merge_classification_detection_output.py
+++ classification/merge_classification_detection_output.py
@@ -2,7 +2,12 @@ r"""
 Merges classification results with Batch Detection API outputs.
 
 Takes as input a CSV containing columns:
-- 'path': str, path to cropped image, <dataset>/<crop-file-name>
+- 'path': str, path to cropped image
+    - if passing in a detections JSON, must match
+        <img_file>___cropXX_mdvY.Y.jpg
+    - if passing in a queried images JSON, must match
+        <dataset>/<img_file>___cropXX_mdvY.Y.jpg or
+        <dataset>/<img_file>___cropXX.jpg
 - 'label': str, label assigned to this crop
 - [label names]: float, confidence in each label
 
@@ -54,10 +59,12 @@ import argparse
 import datetime
 import json
 import os
-from typing import Any, List, Mapping, Optional, Sequence, Tuple
+from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple
 
 import pandas as pd
 from tqdm import tqdm
+
+from ct_utils import truncate_float
 
 
 def row_to_classification_list(row: Mapping[str, Any],
@@ -90,8 +97,11 @@ def row_to_classification_list(row: Mapping[str, Any],
             label_conf = row[row['label']]
             result = [(k, max(v - label_conf, 0)) for k, v in result]
 
-        # filter out confidences below the threshold
-        result = [(k, conf) for k, conf in result if conf >= threshold]
+        # filter out confidences below the threshold, and set precision to 4
+        result = [
+            (k, truncate_float(conf, precision=4))
+            for k, conf in result if conf >= threshold
+        ]
 
         # sort from highest to lowest probability
         result = sorted(result, key=lambda x: x[1], reverse=True)
@@ -109,41 +119,95 @@ def row_to_classification_list(row: Mapping[str, Any],
 
 def main(classification_csv_path: str,
          label_names_json_path: str,
-         queried_images_json_path: str,
-         classifier_name: str,
-         detector_output_cache_base_dir: str,
-         detector_version: str,
          output_json_path: str,
+         classifier_name: str,
          threshold: float,
-         datasets: Optional[Sequence[str]] = None,
-         samples_per_label: Optional[int] = None,
-         seed: int = 123,
-         label_pos: Optional[str] = None,
-         relative_conf: bool = False
-         ) -> None:
-    """Main function."""
+         datasets: Optional[Sequence[str]],
+         detection_json_path: Optional[str],
+         queried_images_json_path: Optional[str],
+         detector_output_cache_base_dir: Optional[str],
+         detector_version: Optional[str],
+         samples_per_label: Optional[int],
+         seed: int,
+         label_pos: Optional[str],
+         relative_conf: bool) -> None:
     # input validation
     assert os.path.exists(classification_csv_path)
     assert os.path.exists(label_names_json_path)
-    assert os.path.exists(queried_images_json_path)
-    detector_output_cache_dir = os.path.join(
-        detector_output_cache_base_dir, f'v{detector_version}')
-    assert os.path.isdir(detector_output_cache_dir)
     assert 0 <= threshold <= 1
+    for x in [detection_json_path, queried_images_json_path]:
+        if x is not None:
+            assert os.path.exists(x)
     assert label_pos in [None, 'first', 'last']
 
     # load classification CSV
-    # extract dataset name from img_file so we can process 1 dataset at a time
     print('Loading classification CSV...')
     df = pd.read_csv(classification_csv_path, float_precision='high',
-                     index_col=False)
-    df['dataset'] = df['path'].str.split('/', n=1, expand=True)[0]
-    df.set_index('path', inplace=True)
-
-    if label_pos is not None:
+                     index_col='path')
+    if relative_conf or label_pos is not None:
         assert 'label' in df.columns
 
+    # load label names
+    with open(label_names_json_path, 'r') as f:
+        idx_to_label = json.load(f)
+    label_names = [idx_to_label[str(i)] for i in range(len(idx_to_label))]
+    if 'label' in df.columns:
+        for i, label in enumerate(label_names):
+            idx_to_label[str(i + 1_000_000)] = f'label: {label}'
+
+    if queried_images_json_path is not None:
+        assert detector_output_cache_base_dir is not None
+        assert detector_version is not None
+        detection_js = process_queried_images(
+            df=df, queried_images_json_path=queried_images_json_path,
+            detector_output_cache_base_dir=detector_output_cache_base_dir,
+            detector_version=detector_version, datasets=datasets,
+            samples_per_label=samples_per_label, seed=seed)
+    elif detection_json_path is not None:
+        with open(detection_json_path, 'r') as f:
+            detection_js = json.load(f)
+        images = {}
+        for img in detection_js['images']:
+            path = img['file']
+            if datasets is None or path[:path.find('/')] in datasets:
+                images[path] = img
+        detection_js['images'] = images
+
+    classification_time = datetime.date.fromtimestamp(
+        os.path.getmtime(classification_csv_path))
+    classifier_timestamp = classification_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    classification_js = combine_classification_with_detection(
+        detection_js=detection_js, df=df, idx_to_label=idx_to_label,
+        label_names=label_names, classifier_name=classifier_name,
+        classifier_timestamp=classifier_timestamp, threshold=threshold,
+        label_pos=label_pos, relative_conf=relative_conf)
+
+    os.makedirs(os.path.dirname(output_json_path), exist_ok=True)
+    with open(output_json_path, 'w') as f:
+        json.dump(classification_js, f, indent=1)
+
+
+def process_queried_images(
+         df: pd.DataFrame,
+         queried_images_json_path: str,
+         detector_output_cache_base_dir: str,
+         detector_version: str,
+         datasets: Optional[Sequence[str]] = None,
+         samples_per_label: Optional[int] = None,
+         seed: int = 123
+         ) -> Dict[str, Any]:
+    """Main function."""
+    # input validation
+    assert os.path.exists(queried_images_json_path)
+    detection_cache_dir = os.path.join(
+        detector_output_cache_base_dir, f'v{detector_version}')
+    assert os.path.isdir(detection_cache_dir)
+
+    # extract dataset name from crop path so we can process 1 dataset at a time
+    df['dataset'] = df.index.map(lambda x: x[:x.find('/')])
     unique_datasets = df['dataset'].unique()
+
     if datasets is not None:
         for ds in datasets:
             assert ds in unique_datasets
@@ -156,102 +220,129 @@ def main(classification_csv_path: str,
         print(f'Sampling {samples_per_label} bounding boxes per label')
         df = df.groupby('label').sample(samples_per_label, random_state=seed)
 
-    classification_time = datetime.date.fromtimestamp(
-        os.path.getmtime(classification_csv_path))
-    classifier_timestamp = classification_time.strftime('%Y-%m-%d %H:%M:%S')
-
-    with open(label_names_json_path, 'r') as f:
-        idx_to_label = json.load(f)
-    label_names = [idx_to_label[str(i)] for i in range(len(idx_to_label))]
-    if 'label' in df.columns:
-        for i, label in enumerate(label_names):
-            idx_to_label[str(i + 1_000_000)] = f'label: {label}'
-
-    contains_preds = all(label_name in df.columns for label_name in label_names)
-    if not contains_preds:
-        print('CSV does not contain predictions. Outputting labels only.')
-
     # load queried images JSON, needed for ground-truth bbox info
     with open(queried_images_json_path, 'r') as f:
         queried_images_js = json.load(f)
 
-    classification_js = {
-        'info': {
-            'classifier': classifier_name,
-            'classification_completion_time': classifier_timestamp,
-            'format_version': "1.0"
-        },
-        'classification_categories': idx_to_label,
+    merged_js: Dict[str, Any] = {
         'images': {}  # start as dict, will convert to list later
     }
-    images = classification_js['images']
+    images = merged_js['images']
 
     for ds in datasets:
         print('processing dataset:', ds)
         ds_df = df[df['dataset'] == ds]
 
-        detection_json_path = os.path.join(
-            detector_output_cache_dir, f'{ds}.json')
-        with open(detection_json_path, 'r') as f:
+        with open(os.path.join(detection_cache_dir, f'{ds}.json'), 'r') as f:
             detection_js = json.load(f)
-
         img_file_to_index = {
             im['file']: idx
             for idx, im in enumerate(detection_js['images'])
         }
 
         # compare info dicts
-        class_info = classification_js['info']
+        class_info = merged_js['info']
         detection_info = detection_js['info']
-        if 'detector' not in class_info:
-            class_info['detector'] = detection_info['detector']
-        assert class_info['detector'] == detection_info['detector']
+        key = 'detector'
+        if key not in class_info:
+            class_info[key] = detection_info[key]
+        assert class_info[key] == detection_info[key]
 
         # compare detection categories
         key = 'detection_categories'
-        if key not in classification_js:
-            classification_js[key] = detection_js[key]
-        assert classification_js[key] == detection_js[key]
+        if key not in merged_js:
+            merged_js[key] = detection_js[key]
+        assert merged_js[key] == detection_js[key]
         cat_to_catid = {v: k for k, v in detection_js[key].items()}
 
         for crop_path in tqdm(ds_df.index):
             # crop_path: <dataset>/<img_file>___cropXX_mdvY.Y.jpg
             #            [----<img_path>----]       [-<suffix>--]
-            img_path, suffix = crop_path.split('___crop')
+            img_path, suffix = crop_path.split('___crop')[0]
             img_file = img_path[img_path.find('/') + 1:]
-            crop_index = int(suffix[:2])
 
-            if '_mdv' in suffix:  # file has detection entry
-                if img_path not in images:
-                    img_idx = img_file_to_index[img_file]
-                    images[img_path] = detection_js['images'][img_idx]
-                    images[img_path]['file'] = img_path
+            # file has detection entry
+            if '_mdv' in suffix and img_path not in images:
+                img_idx = img_file_to_index[img_file]
+                images[img_path] = detection_js['images'][img_idx]
+                images[img_path]['file'] = img_path
 
-            else:  # bounding box is from ground truth
-                if img_path not in images:
-                    images[img_path] = {
-                        'file': img_path,
-                        'max_detection_conf': 1.0,
-                        'detections': []
-                    }
-                    for bbox_dict in queried_images_js[img_path]['bbox']:
-                        catid = cat_to_catid[bbox_dict['category']]
-                        images[img_path]['detections'].append({
-                            'category': catid,
+            # bounding box is from ground truth
+            elif img_path not in images:
+                images[img_path] = {
+                    'file': img_path,
+                    'max_detection_conf': 1.0,
+                    'detections': [
+                        {
+                            'category': cat_to_catid[bbox_dict['category']],
                             'conf': 1.0,
                             'bbox': bbox_dict['bbox']
-                        })
+                        }
+                        for bbox_dict in queried_images_js[img_path]['bbox']
+                    ]
+                }
+    return merged_js
 
-            detection_dict = images[img_path]['detections'][crop_index]
-            detection_dict['classifications'] = row_to_classification_list(
-                row=ds_df.loc[crop_path], label_names=label_names,
-                contains_preds=contains_preds, label_pos=label_pos,
-                threshold=threshold, relative_conf=relative_conf)
 
-    classification_js['images'] = list(images.values())
+def combine_classification_with_detection(
+        detection_js: Dict[str, Any],
+        df: pd.DataFrame,
+        idx_to_label: Mapping[str, str],
+        label_names: Sequence[str],
+        classifier_name: str,
+        classifier_timestamp: str,
+        threshold: float,
+        label_pos: Optional[str] = None,
+        relative_conf: bool = False
+        ) -> Dict[str, Any]:
+    """
+    Args:
+        detection_js: dict, detections JSON file, except that 'images'
+            is dict (img_path => dict) instead of a list
+        df: pd.DataFrame, classification results, indexed by crop path
+        idx_to_label: dict, str(label_id) => label name, may also include
+            str(label_id + 1e6) => 'label: {label_name}'
+        label_names: list of str, label names
+        classifier_name: str, name of classifier to include in output JSON
+        classifier_timestamp: str, timestamp to include in output JSON
+        threshold: float, for each crop, omit classification results for
+            categories whose confidence is below this threshold
+        label_pos: one of [None, 'first', 'last']
+            None: do not include labels in the output JSON
+            'first' / 'last': position in classification list to put the label
+        relative_conf: bool, if True then for each class, outputs its relative
+            confidence over the confidence of the true label, requires "label"
+            to be in CSV
 
-    with open(output_json_path, 'w') as f:
-        json.dump(classification_js, f, indent=1)
+    Returns: dict, detections JSON file updated with classification results
+    """
+    detection_js['info'].update({
+        'classifier': classifier_name,
+        'classification_completion_time': classifier_timestamp,
+        'format_version': '1.0'
+    })
+    detection_js['classification_categories'] = idx_to_label
+
+    contains_preds = (set(label_names) <= set(df.columns))
+    if not contains_preds:
+        print('CSV does not contain predictions. Outputting labels only.')
+
+    images = detection_js['images']
+
+    for crop_path in tqdm(df.index):
+        # crop_path: <dataset>/<img_file>___cropXX_mdvY.Y.jpg
+        #            [----<img_path>----]       [-<suffix>--]
+        img_path, suffix = crop_path.split('___crop')
+        crop_index = int(suffix[:2])
+
+        detection_dict = images[img_path]['detections'][crop_index]
+        detection_dict['classifications'] = row_to_classification_list(
+            row=df.loc[crop_path], label_names=label_names,
+            contains_preds=contains_preds, label_pos=label_pos,
+            threshold=threshold, relative_conf=relative_conf)
+
+    detection_js['images'] = list(images.values())
+    return detection_js
 
 
 def _parse_args() -> argparse.Namespace:
@@ -267,41 +358,52 @@ def _parse_args() -> argparse.Namespace:
         'label_names_json',
         help='path to JSON file mapping label index to label name')
     parser.add_argument(
-        'queried_images_json',
-        help='path to queried images JSON file')
-    parser.add_argument(
-        '-n', '--classifier-name', required=True,
-        help='(required) name of classifier')
-    parser.add_argument(
-        '-c', '--detector-output-cache-dir', required=True,
-        help='(required) path to directory where detector outputs are cached')
-    parser.add_argument(
-        '-v', '--detector-version', required=True,
-        help='(required) detector version string, e.g., "4.1"')
-    parser.add_argument(
         '-o', '--output-json', required=True,
         help='(required) path to save output JSON with both detection and '
              'classification results')
     parser.add_argument(
+        '-n', '--classifier-name', required=True,
+        help='(required) name of classifier')
+    parser.add_argument(
         '-t', '--threshold', type=float, default=0.1,
-        help='Confidence threshold between 0 and 1.0. Exclude classes below '
-             'this confidence in the output file.')
+        help='Confidence threshold between 0 and 1. In the output file, omit '
+             'classifier results on classes whose confidence is below this '
+             'threshold.')
     parser.add_argument(
         '-d', '--datasets', nargs='*',
-        help='optionally limit output to images from certain datasets')
-    parser.add_argument(
+        help='optionally limit output to images from certain datasets. Assumes '
+             'that image paths are given as <dataset>/<img_file>.')
+
+    detection_json_group = parser.add_argument_group(
+        'arguments for passing in a detections JSON file')
+    detection_json_group.add_argument(
+        '-j', '--detection-json',
+        help='path to detections JSON file')
+
+    queried_images_group = parser.add_argument_group(
+        'arguments for passing in a queried images JSON file')
+    queried_images_group.add_argument(
+        '-q', '--queried-images-json',
+        help='path to queried images JSON file')
+    queried_images_group.add_argument(
+        '-c', '--detector-output-cache-dir',
+        help='(required) path to directory where detector outputs are cached')
+    queried_images_group.add_argument(
+        '-v', '--detector-version',
+        help='(required) detector version string, e.g., "4.1"')
+    queried_images_group.add_argument(
         '-s', '--samples-per-label', type=int,
         help='randomly sample this many bounding boxes per label (each label '
              'must have at least this many examples)')
-    parser.add_argument(
+    queried_images_group.add_argument(
         '--seed', type=int, default=123,
         help='random seed, only used if --samples-per-label is given')
-    parser.add_argument(
+    queried_images_group.add_argument(
         '--label', choices=['first', 'last'], default=None,
         help='Whether to put the label first or last in the list of '
              'classifications. If this argument is omitted, then no labels are '
              'included in the output.')
-    parser.add_argument(
+    queried_images_group.add_argument(
         '--relative-conf', action='store_true',
         help='for each class, outputs its relative confidence over the '
              'confidence of the true label, requires "label" to be in CSV')
@@ -312,13 +414,14 @@ if __name__ == '__main__':
     args = _parse_args()
     main(classification_csv_path=args.classification_csv,
          label_names_json_path=args.label_names_json,
-         queried_images_json_path=args.queried_images_json,
-         classifier_name=args.classifier_name,
-         detector_output_cache_base_dir=args.detector_output_cache_dir,
-         detector_version=args.detector_version,
          output_json_path=args.output_json,
+         classifier_name=args.classifier_name,
          threshold=args.threshold,
          datasets=args.datasets,
+         detection_json_path=args.detection_json,
+         queried_images_json_path=args.queried_images_json,
+         detector_output_cache_base_dir=args.detector_output_cache_dir,
+         detector_version=args.detector_version,
          samples_per_label=args.samples_per_label,
          seed=args.seed,
          label_pos=args.label,
