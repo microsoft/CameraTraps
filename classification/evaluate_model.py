244f8a5b0ff8933c9d03841dcd73e29ff906c194
Christopher Yeh <chrisyeh96@users.noreply.github.com>
2020-09-20 05:16:52 +0000

    classifier: minor updates

--- classification/evaluate_model.py
+++ classification/evaluate_model.py
@@ -185,6 +185,9 @@ def main(params_json_path: str, ckpt_path: str, output_dir: str,
             assert target_names == set(label_names) | {'other'}
             label_names.append('other')
 
+            with open(os.path.join(output_dir, 'label_index.json'), 'w') as f:
+                json.dump(dict(enumerate(label_names)), f)
+
         with open(label_index_json_path, 'r') as f:
             idx_to_label = json.load(f)
         classifier_name_to_idx = {
