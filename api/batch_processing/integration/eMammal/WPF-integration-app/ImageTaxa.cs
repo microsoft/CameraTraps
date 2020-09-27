890280c8908d9bf5377d3f837459d280546e7614
Dan Morris <dan@microsoft.com>
2020-09-27 10:36:21 -0700

    Initial version of eMammal app

---
+++ api/batch_processing/integration/eMammal/WPF-integration-app/ImageTaxa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eMammal_integration_application
+{
+    /// <summary>
+    /// Class for generating image sequence annotation list
+    /// </summary>
+    public class ImageTaxa
+    {     
+        public int sequenceId { get; set; }
+        public int projectTaxaId { get; set; }
+    }
+}
