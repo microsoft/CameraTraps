cc0f2e3b98eac462f475e76583b9fc09efb223e9
Siyu Yang <yasiyu@microsoft.com>
2021-01-12 17:40:51 -0800

    Use Batch container workloads; TF able to access GPU.

---
+++ api/batch_processing/api_core/batch_service/score.py
@@ -0,0 +1,9 @@
+import PIL.Image as Image
+import numpy as np
+import tensorflow as tf
+
+print('tensorflow tf version:', tf.__version__)
+print('tf_detector.py, tf.test.is_gpu_available:', tf.test.is_gpu_available())
+
+
+
