f821b5ba167b9a06dd047f273fa02183c235ae92
Dan Morris <dan@microsoft.com>
2021-01-13 19:50:19 -0800

    Download LILA images by species

--- data_management/download_lila_subset.py
+++ data_management/download_lila_subset.py
@@ -9,23 +9,26 @@
 
 import json
 import urllib.request
+import tempfile
+import zipfile
 import os
+
 from tqdm import tqdm
 from multiprocessing.pool import ThreadPool
+from urllib.parse import urlparse
 
-# SAS URLs come from:
-#
-# http://lila.science/?attachment_id=792
-#
-# In this example, we're using the Missouri Camera Traps data set
-sas_url = 'https://lilablobssc.blob.core.windows.net/missouricameratraps/images?st=2020-01-01T00%3A00%3A00Z&se=2034-01-01T00%3A00%3A00Z&sp=rl&sv=2019-07-07&sr=c&sig=zf5Vb3BmlGgBKBM1ZtAZsEd1vZvD6EbN%2BNDzWddJsUI%3D'
+# LILA camera trap master metadata file
+metadata_url = 'http://lila.science/wp-content/uploads/2020/03/lila_sas_urls.txt'
 
-# This assumes you've downloaded the metadata file from LILA
-json_filename = r'd:\temp\missouri_camera_traps_set1.json'
+# In this example, we're using the Missouri Camera Traps data set and the Caltech Camera Traps dataset
+datasets_of_interest = ['Missouri Camera Traps','ENA24','Caltech Camera Traps']
 
-output_dir = r'd:\temp\missouri_camera_traps_subset'
+# All lower-case; we'll convert category names to lower-case when comparing
+species_of_interest = ['red_fox','fox','grey fox','red fox']
 
-species_of_interest = 'red_fox'
+# We'll write images, metadata downloads, and temporary files here
+output_dir = r'c:\temp\lila_downloads_by_species'
+os.makedirs(output_dir,exist_ok=True)
 
 # We will demonstrate two approaches to downloading, one that loops over files
 # and downloads directly in Python, another that uses AzCopy.
@@ -36,76 +39,250 @@ use_azcopy_for_download = False
 
 overwrite_files = False
 
-# Number of concurrent download threads (when not using AzCopy)
+# Number of concurrent download threads (when not using AzCopy) (AzCopy does its
+# own magical parallelism)
 n_download_threads = 50
-
-
-#%% Environment prep and derived constants
-
-base_url = sas_url.split('?')[0]
-sas_token = sas_url.split('?')[1]
-os.makedirs(output_dir,exist_ok=True)
-
-
-#%% Open the metadata file
-
-with open(json_filename, 'r') as f:
-    data = json.load(f)
-
-categories = data['categories']
-annotations = data['annotations']
-images = data['images']
-
-
-#%% Build a list of image files (relative path names) that match the target species
-
-# Retrieve the category ID we're interested in
-category_of_interest = list(filter(lambda x: x['name'] == species_of_interest, categories))
-assert len(category_of_interest) == 1
-category_of_interest = category_of_interest[0]
-category_id_of_interest = category_of_interest['id']
-
-# Retrieve all the images that match that category
-image_ids_of_interest = set([ann['image_id'] for ann in annotations if ann['category_id'] == category_id_of_interest])
-
-print('Selected {} of {} images'.format(len(image_ids_of_interest),len(images)))
-
-# Retrieve image file names
-filenames = [im['file_name'] for im in images if im['id'] in image_ids_of_interest]
-assert len(filenames) == len(image_ids_of_interest)
 
 
 #%% Support functions
 
-def download_image(fn):
+def download_url(url, destination_filename=None, force_download=False, verbose=True):
+    """
+    Download a URL (defaulting to a temporary file)
+    """
     
-    url = base_url + '/' + fn
-    target_file = os.path.join(output_dir,fn)
-    if ((not overwrite_files) and (os.path.isfile(target_file))):
-        # print('Skipping file {}'.format(fn))
-        return
+    if destination_filename is None:
+        temp_dir = os.path.join(tempfile.gettempdir(),'lila')
+        os.makedirs(temp_dir,exist_ok=True)
+        url_as_filename = url.replace('://', '_').replace('.', '_').replace('/', '_')
+        destination_filename = \
+            os.path.join(temp_dir,url_as_filename)
+            
+    if (not force_download) and (os.path.isfile(destination_filename)):
+        print('Bypassing download of already-downloaded file {}'.format(os.path.basename(url)))
+        return destination_filename
     
-    # print('Downloading {} to {}'.format(url,target_file))        
-    os.makedirs(os.path.dirname(target_file),exist_ok=True)
-    urllib.request.urlretrieve(
-        url,target_file)
+    if verbose:
+        print('Downloading file {} to {}'.format(os.path.basename(url),destination_filename),end='')
+    
+    os.makedirs(os.path.dirname(destination_filename),exist_ok=True)
+    urllib.request.urlretrieve(url, destination_filename)  
+    assert(os.path.isfile(destination_filename))
+    
+    if verbose:
+        nBytes = os.path.getsize(destination_filename)    
+        print('...done, {} bytes.'.format(nBytes))
+        
+    return destination_filename
 
+
+def download_relative_filename(url, output_base, verbose=False):
+    """
+    Download a URL to output_base, preserving relative path
+    """
     
+    p = urlparse(url)
+    # remove the leading '/'
+    assert p.path.startswith('/'); relative_filename = p.path[1:]
+    destination_filename = os.path.join(output_base,relative_filename)
+    download_url(url, destination_filename, verbose=verbose)
+    
+
+def unzip_file(input_file, output_folder=None):
+    """
+    Unzip a zipfile to the specified output folder, defaulting to the same location as
+    the input file    
+    """
+    
+    if output_folder is None:
+        output_folder = os.path.dirname(input_file)
+        
+    with zipfile.ZipFile(input_file, 'r') as zf:
+        zf.extractall(output_folder)
+
+
+#%% Download and parse the metadata file
+
+# Put the master metadata file in the same folder where we're putting images
+p = urlparse(metadata_url)
+metadata_filename = os.path.join(output_dir,os.path.basename(p.path))
+download_url(metadata_url, metadata_filename)
+
+# Read lines from the master metadata file
+with open(metadata_filename,'r') as f:
+    metadata_lines = f.readlines()
+metadata_lines = [s.strip() for s in metadata_lines]
+
+# Parse those lines into a table
+metadata_table = {}
+
+for s in metadata_lines:
+    
+    if len(s) == 0 or s[0] == '#':
+        continue
+    
+    # Each line in this file is name/sas_url/json_url
+    tokens = s.split(',')
+    assert len(tokens)==3
+    url_mapping = {'sas_url':tokens[1],'json_url':tokens[2]}
+    metadata_table[tokens[0]] = url_mapping
+    
+    assert 'https' not in tokens[0]
+    assert 'https' in url_mapping['sas_url']
+    assert 'https' in url_mapping['json_url']
+
+
+#%% Download and extract metadata for the datasets we're interested in
+
+for ds_name in datasets_of_interest:
+    
+    assert ds_name in metadata_table
+    json_url = metadata_table[ds_name]['json_url']
+    
+    p = urlparse(json_url)
+    json_filename = os.path.join(output_dir,os.path.basename(p.path))
+    download_url(json_url, json_filename)
+    
+    # Unzip if necessary
+    if json_filename.endswith('.zip'):
+        
+        with zipfile.ZipFile(json_filename,'r') as z:
+            files = z.namelist()
+        assert len(files) == 1
+        unzipped_json_filename = os.path.join(output_dir,files[0])
+        if not os.path.isfile(unzipped_json_filename):
+            unzip_file(json_filename,output_dir)        
+        else:
+            print('{} already unzipped'.format(unzipped_json_filename))
+        json_filename = unzipped_json_filename
+    
+    metadata_table[ds_name]['json_filename'] = json_filename
+    
+# ...for each dataset of interest
+
+
+#%% List of files we're going to download (for all data sets)
+
+# Flat list or URLS, for use with direct Python downloads
+urls_to_download = []
+
+# For use with azcopy
+downloads_by_dataset = {}
+
+for ds_name in datasets_of_interest:
+    
+    json_filename = metadata_table[ds_name]['json_filename']
+    sas_url = metadata_table[ds_name]['sas_url']
+    
+    base_url = sas_url.split('?')[0]    
+    assert not base_url.endswith('/')
+    
+    sas_token = sas_url.split('?')[1]
+    assert not sas_token.startswith('?')
+    
+    ## Open the metadata file
+    
+    with open(json_filename, 'r') as f:
+        data = json.load(f)
+    
+    categories = data['categories']
+    for c in categories:
+        c['name'] = c['name'].lower()
+    category_id_to_name = {c['id']:c['name'] for c in categories}
+    annotations = data['annotations']
+    images = data['images']
+
+
+    ## Build a list of image files (relative path names) that match the target species
+
+    category_ids = []
+    
+    for species_name in species_of_interest:
+        matching_categories = list(filter(lambda x: x['name'] == species_name, categories))
+        if len(matching_categories) == 0:
+            continue
+        assert len(matching_categories) == 1
+        category = matching_categories[0]
+        category_id = category['id']
+        category_ids.append(category_id)
+    
+    print('Found {} matching categories for data set {}:'.format(len(category_ids),ds_name))
+    
+    if len(category_ids) == 0:
+        continue
+    
+    for i_category,category_id in enumerate(category_ids):
+        print(category_id_to_name[category_id],end='')
+        if i_category != len(category_ids) -1:
+            print(',',end='')
+    print('')
+    
+    # Retrieve all the images that match that category
+    image_ids_of_interest = set([ann['image_id'] for ann in annotations if ann['category_id'] in category_ids])
+    
+    print('Selected {} of {} images for dataset {}'.format(len(image_ids_of_interest),len(images),ds_name))
+    
+    # Retrieve image file names
+    filenames = [im['file_name'] for im in images if im['id'] in image_ids_of_interest]
+    assert len(filenames) == len(image_ids_of_interest)
+    
+    # Convert to URLs
+    for fn in filenames:        
+        url = base_url + '/' + fn
+        urls_to_download.append(url)
+
+    downloads_by_dataset[ds_name] = {'sas_url':sas_url,'filenames':filenames}
+    
+# ...for each dataset
+
+print('Found {} images to download'.format(len(urls_to_download)))
+
+
 #%% Download those image files
 
 if use_azcopy_for_download:
     
-    print('Downloading images for {0} with azcopy'.format(species_of_interest))
+    for ds_name in downloads_by_dataset:
     
-    # Write out a list of files, and use the azcopy "list-of-files" option to download those files
-    # this azcopy feature is unofficially documented at https://github.com/Azure/azure-storage-azcopy/wiki/Listing-specific-files-to-transfer
-    az_filename = os.path.join(output_dir, 'filenames.txt')
-    with open(az_filename, 'w') as f:
-        for fn in filenames:
-            f.write(fn.replace('\\','/') + '\n')
-    cmd = 'azcopy cp "{0}" "{1}" --list-of-files "{2}"'.format(
-            sas_url, output_dir, az_filename)            
-    os.system(cmd)
+        print('Downloading images for {} with azcopy'.format(ds_name))
+        sas_url = downloads_by_dataset[ds_name]['sas_url']
+        filenames = downloads_by_dataset[ds_name]['filenames']
+    
+        # We want to use the whole relative path for this script (relative to the base of the container)
+        # to build the output filename, to make sure that different data sets end up in different folders.
+        base_url = sas_url.split('?')[0]
+        sas_token = sas_url.split('?')[1]
+        
+        p = urlparse(base_url)
+        account_path = p.scheme + '://' + p.netloc
+        assert account_path == 'https://lilablobssc.blob.core.windows.net'
+        
+        container_and_folder = p.path[1:]
+        
+        container_name = container_and_folder.split('/')[0]
+        folder = container_and_folder.split('/',1)[1]
+        
+        container_sas_url = account_path + '/' + container_name + '?' + sas_token
+        
+        # The container name will be included because it's part of the file name
+        container_output_dir = output_dir # os.path.join(output_dir,container_name)
+        os.makedirs(container_output_dir,exist_ok=True)
+        
+        filenames = [folder + '/' + s for s in filenames]
+    
+        # Write out a list of files, and use the azcopy "list-of-files" option to download those files
+        # this azcopy feature is unofficially documented at https://github.com/Azure/azure-storage-azcopy/wiki/Listing-specific-files-to-transfer
+        az_filename = os.path.join(output_dir, 'filenames_{}.txt'.format(ds_name.lower().replace(' ','_')))
+        with open(az_filename, 'w') as f:
+            for fn in filenames:
+                f.write(fn.replace('\\','/') + '\n')
+                
+        cmd = 'azcopy cp "{0}" "{1}" --list-of-files "{2}"'.format(
+                container_sas_url, container_output_dir, az_filename)            
+        
+        # import clipboard; clipboard.copy(cmd)
+        
+        os.system(cmd)
     
 else:
     
@@ -114,12 +291,12 @@ else:
     
     if n_download_threads <= 1:
     
-        for fn in tqdm(filenames):        
-            download_image(fn)
+        for url in tqdm(urls_to_download):        
+            download_relative_filename(url,output_dir,verbose=True)
         
     else:
     
         pool = ThreadPool(n_download_threads)        
-        tqdm(pool.imap(download_image, filenames), total=len(filenames))
+        tqdm(pool.imap(lambda s: download_relative_filename(s,output_dir,verbose=False), urls_to_download), total=len(urls_to_download))
     
 print('Done!')
