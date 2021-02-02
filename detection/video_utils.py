8d698a5a132598d717ad673c5e321b8f36d0dedf
Dan Morris <dmorris@cs.stanford.edu>
2021-02-02 10:22:32 -0800

    Scratch work in video_utils.py

--- detection/video_utils.py
+++ detection/video_utils.py
@@ -61,7 +61,7 @@ def find_videos(dirname: str, recursive: bool = False) -> List[str]:
 
 # http://tsaith.github.io/combine-images-into-a-video-with-python-3-and-opencv-3.html
 
-def frames_to_video(images, Fs, output_file_name):
+def frames_to_video(images, Fs, output_file_name, codec_spec='h264'):
     """
     Given a list of image files and a sample rate, concatenate those images into
     a video and write to [output_file_name].
@@ -76,7 +76,7 @@ def frames_to_video(images, Fs, output_file_name):
     height, width, channels = frame.shape
 
     # Define the codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+    fourcc = cv2.VideoWriter_fourcc(*codec_spec)
     out = cv2.VideoWriter(output_file_name, fourcc, Fs, (width, height))
 
     for image in images:
