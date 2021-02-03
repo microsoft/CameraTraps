9e520fb04b1120d5c2be27283c3a72982b382d5a
Siyu Yang <yasiyu@microsoft.com>
2021-02-02 22:41:22 -0800

    API working for the blob storage container URL case.

--- api/batch_processing/api_core/server_job_status_table.py
+++ api/batch_processing/api_core/server_job_status_table.py
@@ -2,49 +2,53 @@
 # Licensed under the MIT License.
 
 """
-A class to manage updating the status of an API request / Azure Batch Job.
+A class to manage updating the status of an API request / Azure Batch Job using
+the Cosmos DB table "batch_api_jobs".
 """
-import os
+
 import unittest
 import uuid
 from datetime import datetime
+from typing import Union, Optional
 
 from azure.cosmos.cosmos_client import CosmosClient
+from azure.cosmos.exceptions import CosmosResourceNotFoundError
+
+from server_api_config import API_INSTANCE_NAME, COSMOS_ENDPOINT, COSMOS_WRITE_KEY
+from server_utils import get_utc_time
 
 
 class JobStatusTable:
 
-    def __init__(self, api_instance):
-        self.api_instance = api_instance
-        COSMOS_ENDPOINT = os.environ['COSMOS_ENDPOINT']
-        COSMOS_WRITE_KEY = os.environ['COSMOS_WRITE_KEY']
+    allowed_statuses = ['running', 'failed', 'problem', 'completed', 'canceled']
 
+    def __init__(self, api_instance=None):
+        self.api_instance = api_instance if api_instance is not None else API_INSTANCE_NAME
         cosmos_client = CosmosClient(COSMOS_ENDPOINT, credential=COSMOS_WRITE_KEY)
         db_client = cosmos_client.get_database_client('camera-trap')
         self.db_jobs_client = db_client.get_container_client('batch_api_jobs')
 
-
-    def create_job_status(self, job_id: str, status: dict, call_params: dict):
-        assert 'request_status' in status
-        assert status['request_status'] in ['running', 'failed', 'problem', 'completed']
-        assert 'message' in status
+    def create_job_status(self, job_id: str, status: Union[dict, str], call_params: dict) -> dict:
+        """Usually called with the status set to `created` when the request is first received"""
 
         # job_id should be unique across all instances, and is also the partition key
         item = {
             'id': job_id,
             'api_instance': self.api_instance,
             'status': status,
-            'last_updated': str(datetime.now()),
+            'last_updated': get_utc_time(),
             'call_params': call_params
         }
         created_item = self.db_jobs_client.create_item(item)
         return created_item
 
-
-    def update_job_status(self, job_id: str, status: dict):
-        assert 'request_status' in status
-        assert status['request_status'] in ['running', 'failed', 'problem', 'completed']
-        assert 'message' in status
+    def update_job_status(self, job_id: str, status: Union[dict, str]) -> dict:
+        """ On updating, the status is usually a dict with fields `request_status`
+        and `message`
+        """
+        if isinstance(status, dict) and 'request_status' in status:
+            assert status['request_status'] in JobStatusTable.allowed_statuses
+            assert 'message' in status
 
         item = {
             'id': job_id,
@@ -55,19 +59,25 @@ class JobStatusTable:
         replaced_item = self.db_jobs_client.replace_item(job_id, item)
         return replaced_item
 
-
-    def read_job_status(self, job_id):
+    def read_job_status(self, job_id) -> Optional[dict]:
         # job_id is also the partition key
-        read_item = self.db_jobs_client.read_item(job_id, partition_key=job_id)
-        item = {k: v for k, v in read_item.items() if not k.startswith('_')}
-        return item
+        try:
+            read_item = self.db_jobs_client.read_item(job_id, partition_key=job_id)
+        except CosmosResourceNotFoundError:
+            return None  # job_id not a key
+        except Exception as e:
+            print(f'server_job_status_table, read_job_status, exception: {e}')
+            raise
+        else:
+            item = {k: v for k, v in read_item.items() if not k.startswith('_')}
+            return item
 
 
 class TestJobStatusTable(unittest.TestCase):
+    api_instance = 'api_test'
 
     def test_insert(self):
-        api_instance = 'api_test'
-        table = JobStatusTable(api_instance)
+        table = JobStatusTable(TestJobStatusTable.api_instance)
         status = {
             'request_status': 'running',
             'message': 'this is a test'
@@ -77,10 +87,8 @@ class TestJobStatusTable(unittest.TestCase):
         self.assertTrue(job_id == item['id'], 'Expect job_id to be the id of the item')
         self.assertTrue(item['status']['request_status'] == 'running', 'Expect fields to be inserted correctly')
 
-
     def test_update_and_read(self):
-        api_instance = 'api_test'
-        table = JobStatusTable(api_instance)
+        table = JobStatusTable(TestJobStatusTable.api_instance)
         status = {
             'request_status': 'running',
             'message': 'this is a test'
@@ -93,9 +101,14 @@ class TestJobStatusTable(unittest.TestCase):
             'message': 'this is a test again'
         }
         res = table.update_job_status(job_id, status)
-        print(f'JOB_ID IS {job_id}')
         item_read = table.read_job_status(job_id)
         self.assertTrue(item_read['status']['request_status'] == 'completed', 'Expect field to have updated')
+
+    def test_read_invalid_id(self):
+        table = JobStatusTable(TestJobStatusTable.api_instance)
+        job_id = uuid.uuid4().hex  # should not be in the database
+        item_read = table.read_job_status(job_id)
+        self.assertIsNone(item_read)
 
 
 if __name__ == '__main__':
--- api/batch_processing/api_core/start_batch_api.sh
+++ api/batch_processing/api_core/start_batch_api.sh
@@ -5,6 +5,11 @@
 
 # Get the credentials from KeyVault
 
+# run `source start_batch_api.sh` to persist the credentials as env variables in the
+# current shell for easy debugging by launching the Flask app separately.
+
+# need "-o tsv" for the Azure CLI queries to get rid of quote marks
+
 SUBSCRIPTION=74d91980-e5b4-4fd9-adb6-263b8f90ec5b
 KEY_VAULT_NAME=cameratraps
 
@@ -12,42 +17,42 @@ KEY_VAULT_NAME=cameratraps
 echo Log in to your Azure account via the CLI. A browser window should open shortly...
 az login
 
-APP_TENANT_ID=$(az keyvault secret show --name batch-api-tenant-id --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value)
+APP_TENANT_ID=$(az keyvault secret show --name batch-api-tenant-id --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value -o tsv)
 echo APP_TENANT_ID read from KeyVault
 export APP_TENANT_ID
 
-APP_CLIENT_ID=$(az keyvault secret show --name batch-api-client-id --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value)
+APP_CLIENT_ID=$(az keyvault secret show --name batch-api-client-id --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value -o tsv)
 echo APP_CLIENT_ID read from KeyVault
 export APP_CLIENT_ID
 
-APP_CLIENT_SECRET=$(az keyvault secret show --name batch-api-client-secret --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value)
+APP_CLIENT_SECRET=$(az keyvault secret show --name batch-api-client-secret --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value -o tsv)
 echo APP_CLIENT_SECRET read from KeyVault
 export APP_CLIENT_SECRET
 
 # blob storage account with containers for scripts and job outputs
 export STORAGE_ACCOUNT_NAME=cameratrap
 
-STORAGE_ACCOUNT_KEY=$(az keyvault secret show --name cameratrap-storage-account-key --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value)
+STORAGE_ACCOUNT_KEY=$(az keyvault secret show --name cameratrap-storage-account-key --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value -o tsv)
 echo STORAGE_ACCOUNT_KEY read from KeyVault
 export STORAGE_ACCOUNT_KEY
 
 # Azure Batch
-export BATCH_ACCOUNT_NAME=cameratraps
-export BATCH_ACCOUNT_URL=https://cameratraps.eastus.batch.azure.com
+export BATCH_ACCOUNT_NAME=cameratrapssc
+export BATCH_ACCOUNT_URL=https://cameratrapssc.southcentralus.batch.azure.com
 
 # Azure Container Registry - Azure Batch gets the Docker image from here
 export REGISTRY_SERVER=cameratracrsppftkje.azurecr.io
 
-REGISTRY_PASSWORD=$(az keyvault secret show --name registry-password --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value)
-echo STORAGE_ACCOUNT_KEY read from KeyVault
+REGISTRY_PASSWORD=$(az keyvault secret show --name registry-password --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value -o tsv)
+echo REGISTRY_PASSWORD read from KeyVault
 export REGISTRY_PASSWORD
 
 # for App Configuration
-APP_CONFIG_CONNECTION_STR=$(az keyvault secret show --name camera-trap-app-config-connection-str --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value)
+APP_CONFIG_CONNECTION_STR=$(az keyvault secret show --name camera-trap-app-config-connection-str --subscription $SUBSCRIPTION --vault-name $KEY_VAULT_NAME --query value -o tsv)
 echo APP_CONFIG_CONNECTION_STR read from KeyVault
 export APP_CONFIG_CONNECTION_STR
 
 export FLASK_APP=server
 export FLASK_ENV=development
 
-flask run -p 5000
+#flask run -p 5000 --eager-loading --no-reload
