dbee65259414801d73f1dc16328d8e988dab2a91
Siyu Yang <yasiyu@microsoft.com>
2021-02-08 19:02:03 -0800

    Change timestamp format to allow Cosmos DB query of submissions in the past day. Add a script to send a daily summary. (#237)

--- api/batch_processing/api_core/server_utils.py
+++ api/batch_processing/api_core/server_utils.py
@@ -24,11 +24,6 @@ def make_error(error_code: int, error_message: str) -> Tuple[dict, int]:
     return {'error': error_message}, error_code
 
 
-def print_job(job_id, msg):
-    # TODO use this func for logging
-    log.info(f'job_id={job_id}. {msg}')
-
-
 def check_data_container_sas(input_container_sas: str) -> Optional[Tuple[int, str]]:
     """
     Returns a tuple (error_code, msg) if not a usable SAS URL, else returns None
@@ -52,8 +47,10 @@ def check_data_container_sas(input_container_sas: str) -> Optional[Tuple[int, st
 
 
 def get_utc_time() -> str:
-    # return current UTC time in string format, e.g., '2019-05-19 08:57:43'
-    return datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    # return current UTC time as a string in the ISO 8601 format (so we can query by
+    # timestamp in the Cosmos DB job status table.
+    # example: '2021-02-08T20:02:05.699689Z'
+    return datetime.utcnow().isoformat(timespec='microseconds') + 'Z'
 
 
 def get_job_status(request_status: str, message: Any) -> dict:
