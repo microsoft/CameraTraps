94c3dd77a8b4d1c951b2786db55bf41071a1db0e
annie.enchakattu <annie.enchakattu@gramener.com>
2021-12-01 04:53:06 +0000

    added config file

---
+++ api_flask_redis/api_core/animal_detection_api/config.py
@@ -0,0 +1,45 @@
+# sync API configurations
+REDIS_HOST = "localhost"
+
+REDIS_PORT = 6379
+
+TEMP_FOLDER = "temp"
+
+REDIS_QUEUE = "camera-trap-queue"
+
+# upper limit on total content length (all images and parameters)
+MAX_CONTENT_LENGTH_IN_MB = 5 * 8  # 5MB per image * number of images allowed
+
+MAX_IMAGES_ACCEPTED = 8
+
+IMAGE_CONTENT_TYPES = ['image/png', 'application/octet-stream', 'image/jpeg']
+
+
+DETECTION_CATEGORY_WHITELIST = ['1']
+
+# padding factor used for padding the detected animal 
+PADDING_FACTOR = 1.6 
+
+# Minimum detection confidence for showing a bounding box on the output image
+DEFAULT_CONFIDENCE_THRESHOLD = 0.85 
+
+# Number of top-scoring classes to show at each bounding box
+NUM_ANNOTATED_CLASSES = 3
+
+# Number of significant float digits in JSON output
+NUM_SIGNIFICANT_DIGITS = 3
+
+
+
+# detection configurations
+
+DETECTOR_MODEL_PATH = '/app/animal_detection_api/model/md_v4.1.0.pb'
+
+# use this when testing without docker
+#DETECTOR_MODEL_PATH = 'model/md_v4.1.0.pb'
+
+DETECTOR_MODEL_VERSION = 'v4.1.0'
+
+DEFAULT_DETECTION_CONFIDENCE = 0.9
+
+API_PREFIX='/v1/camera-trap/sync'
