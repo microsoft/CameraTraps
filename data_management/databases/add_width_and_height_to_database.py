35fcc064274f1f0926520b913f3436c8c77d0a4f
Dan Morris <agentmorris@gmail.com>
2021-12-25 17:54:13 -0800

    Added URL support to visualize_db

--- data_management/databases/add_width_and_height_to_database.py
+++ data_management/databases/add_width_and_height_to_database.py
@@ -15,13 +15,10 @@ image_base = '/datadrive/snapshotserengeti/images/'
 with open(datafile,'r') as f:
     data = json.load(f)
 
-
 for im in data['images']:
     if 'height' not in im:
         im_w, im_h = Image.open(image_base+im['file_name']).size
         im['height'] = im_h
         im['width'] = im_w
 
-
 json.dump(data, open(datafile,'w'))
-
