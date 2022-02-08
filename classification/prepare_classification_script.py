0a47f3793fd697a3227fa44b2d3939ba19e7c719
Dan Morris <dmorris@cs.stanford.edu>
2022-02-08 10:46:53 -0800

    Added new script to combine CCT files, some updates to postprocessing

--- classification/prepare_classification_script.py
+++ classification/prepare_classification_script.py
@@ -12,10 +12,10 @@
 import os
 
 organization_name = 'idfg'
-job_name = 'idfg-2022-01-25-R1_K'
-input_filename = 'idfg-2022-01-25-R1_K_detections.filtered_rde_0.60_0.85_30_0.20.json'
+job_name = 'idfg-2022-01-27-EOE2021S_Group6'
+input_filename = 'idfg-2022-01-27-EOE2021S_Group6_detections.filtered_rde_0.60_0.85_30_0.20.json'
 split_token = '.filtered_rde'
-image_base = '/home/user/data/idfg/R1_K'
+image_base = '/datadrive/idfg/EOE2021S_Group6'
 crop_path = os.path.join(os.path.expanduser('~/crops'),job_name + '_crops')
 device_id = 1
 
