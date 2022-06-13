83bee3d35ae4e0e3599e3c1c48cc3c829d4a2a75
Dan Morris <dmorris@cs.stanford.edu>
2022-06-12 19:20:40 -0700

    Moved classification script generation into manage_local_batch

--- api/batch_processing/data_preparation/manage_local_batch.py
+++ api/batch_processing/data_preparation/manage_local_batch.py
@@ -331,9 +331,9 @@ from api.batch_processing.postprocessing.compare_batch_results import (
 
 options = BatchComparisonOptions()
 
-options.job_name = 'organization-short'
-options.output_folder = os.path.expanduser('~/tmp/organization-short')
-options.image_folder = '/datadrive/home/sftp/organization-short_/data'
+options.job_name = organization_name_short
+options.output_folder = os.path.join(postprocessing_output_folder,'model_comparison')
+options.image_folder = input_path
 
 options.pairwise_options = []
 
@@ -560,10 +560,198 @@ html_output_file = ppresults.output_html_file
 path_utils.open_file(html_output_file)
 
 
-#%% Scrap
+#%% Run MegaClassifier (actually, write out a script that runs MegaClassifier)
 
-# ...and so ends the process for 90% of jobs; the remaining cells are things
-# we do for special cases, but often enough to keep the code handy.
+classifier_name_short = 'megaclassifier'
+threshold_str = '0.15' # 0.6
+classifier_name = 'megaclassifier_v0.1_efficientnet-b3'
+
+organization_name = organization_name_short
+job_name = base_task_name
+input_filename = filtered_output_filename # combined_api_output_file
+input_files = [input_filename]
+image_base = input_path
+crop_path = os.path.join(os.path.expanduser('~/crops'),job_name + '_crops')
+output_base = combined_api_output_folder
+device_id = 0
+
+output_file = os.path.join(filename_base,'run_{}_'.format(classifier_name_short) + job_name +  '.sh')
+
+classifier_base = os.path.expanduser('~/models/camera_traps/megaclassifier/v0.1/')
+assert os.path.isdir(classifier_base)
+
+checkpoint_path = os.path.join(classifier_base,'v0.1_efficientnet-b3_compiled.pt')
+assert os.path.isfile(checkpoint_path)
+
+classifier_categories_path = os.path.join(classifier_base,'v0.1_index_to_name.json')
+assert os.path.isfile(classifier_categories_path)
+
+target_mapping_path = os.path.join(classifier_base,'idfg_to_megaclassifier_labels.json')
+assert os.path.isfile(target_mapping_path)
+
+classifier_output_suffix = '_megaclassifier_output.csv.gz'
+final_output_suffix = '_megaclassifier.json'
+
+n_threads_str = '50'
+image_size_str = '300'
+batch_size_str = '64'
+num_workers_str = '8'
+logdir = filename_base
+
+classification_threshold_str = '0.05'
+
+# This is just passed along to the metadata in the output file, it has no impact
+# on how the classification scripts run.
+typical_classification_threshold_str = '0.75'
+
+##%% Set up environment
+
+commands = []
+# commands.append('cd CameraTraps/classification\n')
+# commands.append('conda activate cameratraps-classifier\n')
+
+
+##%% Crop images
+
+commands.append('\n### Cropping ###\n')
+
+# fn = input_files[0]
+for fn in input_files:
+
+    input_file_path = fn
+    crop_cmd = ''
+    
+    crop_comment = '\n# Cropping {}\n'.format(fn)
+    crop_cmd += crop_comment
+    
+    crop_cmd += "python crop_detections.py \\\n" + \
+    	 input_file_path + ' \\\n' + \
+         crop_path + ' \\\n' + \
+         '--images-dir "' + image_base + '"' + ' \\\n' + \
+         '--threshold "' + threshold_str + '"' + ' \\\n' + \
+         '--square-crops ' + ' \\\n' + \
+         '--threads "' + n_threads_str + '"' + ' \\\n' + \
+         '--logdir "' + logdir + '"' + ' \\\n' + \
+         '\n'
+    crop_cmd = '{}'.format(crop_cmd)
+    commands.append(crop_cmd)
+
+
+##%% Run classifier
+
+commands.append('\n### Classifying ###\n')
+
+# fn = input_files[0]
+for fn in input_files:
+
+    input_file_path = fn
+    classifier_output_path = crop_path + classifier_output_suffix
+    
+    classify_cmd = ''
+    
+    classify_comment = '\n# Classifying {}\n'.format(fn)
+    classify_cmd += classify_comment
+    
+    classify_cmd += "python run_classifier.py \\\n" + \
+    	 checkpoint_path + ' \\\n' + \
+         crop_path + ' \\\n' + \
+         classifier_output_path + ' \\\n' + \
+         '--detections-json "' + input_file_path + '"' + ' \\\n' + \
+         '--classifier-categories "' + classifier_categories_path + '"' + ' \\\n' + \
+         '--image-size "' + image_size_str + '"' + ' \\\n' + \
+         '--batch-size "' + batch_size_str + '"' + ' \\\n' + \
+         '--num-workers "' + num_workers_str + '"' + ' \\\n'
+    
+    if device_id is not None:
+        classify_cmd += '--device {}'.format(device_id)
+        
+    classify_cmd += '\n\n'        
+    classify_cmd = '{}'.format(classify_cmd)
+    commands.append(classify_cmd)
+		
+
+##%% Remap classifier outputs
+
+commands.append('\n### Remapping ###\n')
+
+# fn = input_files[0]
+for fn in input_files:
+
+    input_file_path = fn
+    classifier_output_path = crop_path + classifier_output_suffix
+    classifier_output_path_remapped = \
+        classifier_output_path.replace(".csv.gz","_remapped.csv.gz")
+    assert not (classifier_output_path == classifier_output_path_remapped)
+    
+    output_label_index = classifier_output_path_remapped.replace(
+        "_remapped.csv.gz","_label_index_remapped.json")
+                                       
+    remap_cmd = ''
+    
+    remap_comment = '\n# Remapping {}\n'.format(fn)
+    remap_cmd += remap_comment
+    
+    remap_cmd += "python aggregate_classifier_probs.py \\\n" + \
+        classifier_output_path + ' \\\n' + \
+        '--target-mapping "' + target_mapping_path + '"' + ' \\\n' + \
+        '--output-csv "' + classifier_output_path_remapped + '"' + ' \\\n' + \
+        '--output-label-index "' + output_label_index + '"' + ' \\\n' + \
+        '\n'
+     
+    remap_cmd = '{}'.format(remap_cmd)
+    commands.append(remap_cmd)
+    
+
+##%% Merge classification and detection outputs
+
+commands.append('\n### Merging ###\n')
+
+# fn = input_files[0]
+for fn in input_files:
+
+    input_file_path = fn
+    classifier_output_path = crop_path + classifier_output_suffix
+    
+    classifier_output_path_remapped = \
+        classifier_output_path.replace(".csv.gz","_remapped.csv.gz")
+    
+    output_label_index = classifier_output_path_remapped.replace(
+        "_remapped.csv.gz","_label_index_remapped.json")
+    
+    final_output_path = os.path.join(output_base,
+                                     os.path.basename(classifier_output_path)).\
+        replace(classifier_output_suffix,
+        final_output_suffix)
+    final_output_path = final_output_path.replace('_detections','')
+    final_output_path = final_output_path.replace('_crops','')
+    
+    merge_cmd = ''
+    
+    merge_comment = '\n# Merging {}\n'.format(fn)
+    merge_cmd += merge_comment
+    
+    merge_cmd += "python merge_classification_detection_output.py \\\n" + \
+    	 classifier_output_path_remapped + ' \\\n' + \
+         output_label_index + ' \\\n' + \
+         '--output-json "' + final_output_path + '"' + ' \\\n' + \
+         '--detection-json "' + input_file_path + '"' + ' \\\n' + \
+         '--classifier-name "' + classifier_name + '"' + ' \\\n' + \
+         '--threshold "' + classification_threshold_str + '"' + ' \\\n' + \
+         '--typical-confidence-threshold "' + typical_classification_threshold_str + '"' + ' \\\n' + \
+         '\n'
+    merge_cmd = '{}'.format(merge_cmd)
+    commands.append(merge_cmd)
+
+
+##%% Write  out classification script
+
+with open(output_file,'w') as f:
+    for s in commands:
+        f.write('{}'.format(s))
+
+import stat
+st = os.stat(output_file)
+os.chmod(output_file, st.st_mode | stat.S_IEXEC)
 
 
 #%% Create a new category for large boxes
@@ -681,3 +869,153 @@ for classification_detection_file in classification_detection_files:
     options.output_dir = output_base
     ppresults = process_batch_results(options)
     path_utils.open_file(ppresults.output_html_file)
+
+
+#%% Within-image classification smoothing
+
+# Only count detections with a classification confidence threshold above
+# *classification_confidence_threshold*, which in practice means we're only
+# looking at one category per detection.
+#
+# If an image has at least *min_detections_above_threshold* such detections
+# in the most common category, and no more than *max_detections_secondary_class*
+# in the second-most-common category, flip all detections to the most common
+# category.
+#
+# Optionally treat some classes as particularly unreliable, typically used to overwrite an 
+# "other" class.
+
+classifier_output_path = final_output_path
+classifier_output_path_within_image_smoothing = classifier_output_path.replace(
+    '.json','_within_image_smoothing.json')
+
+with open(classifier_output_path,'r') as f:
+    d = json.load(f)
+
+# d['classification_categories']
+
+# im['detections']
+
+# path_utils.open_file(os.path.join(input_path,im['file']))
+
+from collections import defaultdict
+
+min_detections_above_threshold = 4
+max_detections_secondary_class = 3
+
+min_detections_to_overwrite_other = 2
+other_category_names = ['other']
+
+classification_confidence_threshold = 0.6
+
+category_name_to_id = {d['classification_categories'][k]:k for k in d['classification_categories']}
+other_category_ids = []
+for s in other_category_names:
+    other_category_ids.append(category_name_to_id[s])
+
+n_other_classifications_changed = 0
+n_other_images_changed = 0
+
+n_detections_flipped = 0
+n_images_changed = 0
+
+# im = d['images'][0]    
+for im in d['images']:    
+    
+    if 'detections' not in im or len(im['detections']) == 0:
+        continue
+    
+    detections = im['detections']
+
+    category_to_count = defaultdict(int)
+    for det in detections:
+        if 'classifications' in det:
+            for c in det['classifications']:
+                if c[1] >= classification_confidence_threshold:
+                    category_to_count[c[0]] += 1
+            # ...for each classification
+        # ...if there are classifications for this detection
+    # ...for each detection
+                    
+    if len(category_to_count) <= 1:
+        continue
+    
+    category_to_count = {k: v for k, v in sorted(category_to_count.items(),
+                                                 key=lambda item: item[1], 
+                                                 reverse=True)}
+    
+    keys = list(category_to_count.keys())
+    max_count = category_to_count[keys[0]]
+    secondary_count = category_to_count[keys[1]]
+    most_common_category = keys[0]
+    
+    assert max_count >= secondary_count
+    
+    # If we have at least *min_detections_to_overwrite_other* in a category that isn't
+    # "other", change all "other" classifications to that category
+    if max_count >= min_detections_to_overwrite_other and \
+        most_common_category not in other_category_ids:
+        
+        other_change_made = False
+        
+        for det in detections:
+            
+            if 'classifications' in det:            
+                
+                for c in det['classifications']:                
+                    
+                    if c[1] >= classification_confidence_threshold and \
+                        c[0] in other_category_ids:
+                            
+                        n_other_classifications_changed += 1
+                        other_change_made = True
+                        c[0] = most_common_category
+                        
+                # ...for each classification
+                
+            # ...if there are classifications for this detection
+            
+        # ...for each detection
+        
+        if other_change_made:
+            n_other_images_changed += 1
+        
+    # ...if we should overwrite all "other" classifications
+
+    if max_count < min_detections_above_threshold:
+        continue
+    
+    if secondary_count >= max_detections_secondary_class:
+        continue
+    
+    # At this point, we know we have a dominant category; change all other above-threshold
+    # classifications to that category.  That category may have been "other", in which case we may have
+    # already made the relevant changes.
+    
+    n_detections_flipped_this_image = 0
+    
+    # det = detections[0]
+    for det in detections:
+        if 'classifications' in det:
+            for c in det['classifications']:
+                if c[1] >= classification_confidence_threshold and \
+                    c[0] != most_common_category:
+                        
+                    c[0] = most_common_category
+                    n_detections_flipped += 1
+                    n_detections_flipped_this_image += 1
+            
+    if n_detections_flipped_this_image > 0:
+        n_images_changed += 1
+
+# ...for each image    
+
+print('Classification smoothing: changed {} detections on {} images'.format(
+    n_detections_flipped,n_images_changed))
+
+print('"Other" smoothing: changed {} detections on {} images'.format(
+      n_other_classifications_changed,n_other_images_changed))
+
+with open(classifier_output_path_within_image_smoothing,'w') as f:
+    json.dump(d,f,indent=2)
+    
