927543b04d2fc6f5b65cc43c8e726be35cf5a274
Dan Morris <dmorris@cs.stanford.edu>
2022-07-04 11:46:12 -0700

    taxonomy updates

--- taxonomy_mapping/preview_lila_taxonomy.py
+++ taxonomy_mapping/preview_lila_taxonomy.py
@@ -9,7 +9,8 @@
 import os
 import pandas as pd
 
-lila_taxonomy_file = r"G:\git\agentmorrisprivate\lila-taxonomy\lila-taxonomy-mapping.csv"
+# lila_taxonomy_file = r"G:\git\agentmorrisprivate\lila-taxonomy\lila-taxonomy-mapping.csv"
+lila_taxonomy_file = r"G:\temp\lila\lila-taxonomy-mapping_release.22.07.03.1608.csv"
 # lila_taxonomy_file = r"G:\temp\lila\lila_additions_2022.06.29.csv"
 
 preview_base = r'g:\temp\lila\lila_taxonomy_preview'
@@ -301,6 +302,8 @@ print('Found {} queries with multiple mappings ({} occurrences suppressed)'.form
 
 #%% Verify that nothing "unidentified" maps to a species or subspecies
 
+# E.g., "unidentified skunk" should never map to a specific species of skunk
+
 allowable_unknown_species = [
     'unknown_tayra' # AFAIK this is a unique species, I'm not sure what's implied here
 ]
@@ -336,11 +339,15 @@ for i_row,row in df.iterrows():
 
 for i_row,row in df.iterrows():
     if isinstance(row['scientific_name'],str):
-        assert isinstance(row['source'],str)
+        if 'source' in row:
+            assert isinstance(row['source'],str)
         assert isinstance(row['taxonomy_level'],str)
         
 
 #%% Find WCS mappings that aren't species or aren't the same as the input
+
+# WCS used scientific names, so these remappings are slightly more controversial
+# then the standard remappings.
 
 # row = df.iloc[-500]
 for i_row,row in df.iterrows():
@@ -474,6 +481,30 @@ for s in list(df.scientific_name):
 # ...for each scientific name    
 
 
+#%% Delete unused images
+
+used_images = []
+for images in scientific_name_to_preferred_images.values():
+    used_images.extend(images)
+    
+print('Using a total of {} images'.format(len(used_images)))
+used_images_set = set(used_images)
+
+import path_utils
+all_images = path_utils.recursive_file_list(image_base,False)
+
+unused_images = []
+for fn in all_images:
+    if fn not in used_images_set:
+        unused_images.append(fn)
+
+print('{} of {} files unused (diff {})'.format(len(unused_images),len(all_images),
+                                               len(all_images) - len(unused_images)))
+
+from tqdm import tqdm
+for fn in tqdm(unused_images):
+    os.remove(fn)    
+
 #%% Produce HTML preview
 
 from tqdm import tqdm
@@ -544,118 +575,3 @@ with open(html_output_file, 'w') as f:
 from path_utils import open_file # from ai4eutils
 open_file(html_output_file)
 
-
-#%% Find out which categories are actually used
-
-import json
-
-# Created by get_lila_category_list.py... contains counts for each category
-lila_dataset_to_categories_file = r"G:\temp\lila\lila_categories_list\lila_dataset_to_categories.json"
-lila_taxonomy_file = r"G:\temp\lila\lila-taxonomy-mapping_release.22.07.03.1608.csv"
-
-assert os.path.isfile(lila_dataset_to_categories_file)
-assert os.path.isfile(lila_taxonomy_file)
-
-with open(lila_dataset_to_categories_file,'r') as f:
-    lila_dataset_to_categories = json.load(f)
-
-used_category_mappings = []
-
-# dataset_name = datasets_to_map[0]
-for dataset_name in lila_dataset_to_categories.keys():
-    
-    ds_categories = lila_dataset_to_categories[dataset_name]
-    for category in ds_categories:
-        category_name = category['name'].lower()
-        assert ':' not in category_name
-        mapping_name = dataset_name + ':' + category_name
-        used_category_mappings.append(mapping_name)
-
-b_used_rows = [False] * len(df)
-
-# i_row = 0; row = df.iloc[i_row]; row
-for i_row,row in df.iterrows():
-    ds_name = row['dataset_name']
-    query = row['query']
-    mapping_name = ds_name + ':' + query
-    if mapping_name in used_category_mappings:
-        b_used_rows[i_row] = True
-    else:
-        print('Dropping unused mapping {}'.format(mapping_name))
-        
-
-#%% Generate the final output file
-
-df = pd.read_csv(lila_taxonomy_file)
-
-# release_taxonomy_file = lila_taxonomy_file.replace('.csv','_release.csv')
-release_taxonomy_file = r"G:\temp\lila\lila-taxonomy-mapping_release.22.07.03.1608.csv"
-assert not os.path.isfile(release_taxonomy_file)
-
-known_levels = ['stateofmatter',
-                     'kingdom',
-                     'phylum','subphylum',
-                     'superclass','class','subclass','infraclass',
-                     'superorder','order','parvorder','suborder','infraorder',
-                     'zoosection',
-                     'superfamily','family','subfamily','tribe',
-                     'genus',
-                     'species','subspecies','variety']
-
-levels_to_include = ['kingdom',
-                     'phylum','subphylum',
-                     'superclass','class','subclass','infraclass',
-                     'superorder','order','suborder','infraorder',
-                     'superfamily','family','subfamily','tribe',
-                     'genus',
-                     'species','subspecies','variety']
-
-levels_to_exclude = ['stateofmatter','zoosection','parvorder']
-
-for s in levels_to_exclude:
-    assert s not in levels_to_include
-    
-levels_used = set()
-
-# i_row = 0; row = df.iloc[i_row]; row
-for i_row,row in df.iterrows():
-    
-    if not isinstance(row['scientific_name'],str):
-        assert not isinstance(row['taxonomy_string'],str)
-        continue
-    
-    taxonomic_match = eval(row['taxonomy_string'])
-    
-    # match_at_level = taxonomic_match[0]
-    for match_at_level in taxonomic_match:
-        assert len(match_at_level) == 4
-        levels_used.add(match_at_level[1])
-        
-levels_used = [s for s in levels_used if isinstance(s,str)]
-
-for s in levels_used:
-    assert s in levels_to_exclude or s in levels_to_include, 'Unrecognized level {}'.format(s)
-
-for s in levels_to_include:
-    assert s in levels_used
-    
-for s in levels_to_include:
-    df[s] = ''
-    
-# i_row = 0; row = df.iloc[i_row]; row
-for i_row,row in df.iterrows():
-    
-    if not isinstance(row['scientific_name'],str):
-        assert not isinstance(row['taxonomy_string'],str)
-        continue
-    
-    # E.g.: (43117, 'genus', 'lepus', ['hares and jackrabbits']
-    taxonomic_match = eval(row['taxonomy_string'])
-    
-    for match_at_level in taxonomic_match:
-        level = match_at_level[1]
-        if level in levels_to_include:
-            df.loc[i_row,level] = match_at_level[2]
-
-df = df.drop('source',1)
-df.to_csv(release_taxonomy_file,header=True,index=False)    
