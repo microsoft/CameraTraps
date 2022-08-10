75f00dfe7f54c3a9bcb9494bfd078e82414a4628
Dan Morris <dmorris@cs.stanford.edu>
2022-08-10 14:38:00 -0700

    Removed obsolete synchronous API docs, replaced with new synchronous API.

--- api/synchronous/api_core/build_docker.sh
+++ api/synchronous/api_core/build_docker.sh
@@ -3,22 +3,20 @@
 # these dependency files are outside of the Docker context, so cannot use the COPY action in the Dockerfile to copy them into the Docker image.
 
 # this is the main dependency
-cp ../../../detection/run_tf_detector.py animal_detection_classification_api/
+cp ../../detection/run_tf_detector.py animal_detection_api/
 
 # which depends on the following
-cp ../../../ct_utils.py animal_detection_classification_api/
+cp ../../ct_utils.py animal_detection_api/
 
-mkdir animal_detection_classification_api/visualization/
-cp ../../../visualization/visualization_utils.py animal_detection_classification_api/visualization/
+mkdir -p animal_detection_api/visualization/
+cp ../../visualization/visualization_utils.py animal_detection_api/visualization/
 
 # visualization_utils in turn depends on the following
-mkdir animal_detection_classification_api/data_management
-mkdir animal_detection_classification_api/data_management/annotations/
-cp ../../../data_management/annotations/annotation_constants.py animal_detection_classification_api/data_management/annotations/
+mkdir -p animal_detection_api/data_management
+mkdir -p animal_detection_api/data_management/annotations/
+cp ../../data_management/annotations/annotation_constants.py animal_detection_api/data_management/annotations/
 
-
-# Modify the version and build numbers as needed in the API_DOCKER_IMAGE argument passed to this script
 echo $1
-sudo docker build . -t $1
-
+echo $2
+sudo docker build . --build-arg BASE_IMAGE=$1 -t $2 
 
