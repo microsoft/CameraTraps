b10228c8e9b1daa93b060cb7e8c5402c4ddad3a8
Ubuntu <megadetector@megadetector-float.fywutlkdhe2etlddvntzl12x1a.ex.internal.cloudapp.net>
2022-08-19 18:43:18 +0000

    changes for MD5 support reference MD5 model A update copy commands in build_docker to reflect updated folder structure copy run_tf_detector update imports in api_backend.py use generic load_model in api_backend.py update Flask requirement to resolve jinja2 error add step to clone yolo5 repo and checkout specific commit add cv2 dependencies to apt-install step add in detection_threshold param to generate_detections_one_image calls specify torch and torchvision versions and install requirements.txt after yolov5 requirements.txt

--- api/synchronous/api_core/build_docker.sh
+++ api/synchronous/api_core/build_docker.sh
@@ -3,18 +3,26 @@
 # these dependency files are outside of the Docker context, so cannot use the COPY action in the Dockerfile to copy them into the Docker image.
 
 # this is the main dependency
-cp ../../detection/run_tf_detector.py animal_detection_api/
+cp ../../../detection/tf_detector.py animal_detection_api/
+mkdir animal_detection_api/detection/
+cp -a ../../../detection/. animal_detection_api/detection/
+
+# copy yolo5 dependencies
+git clone https://github.com/ultralytics/yolov5/
+cd yolov5
+git checkout c23a441c9df7ca9b1f275e8c8719c949269160d1
+cd ../
 
 # which depends on the following
-cp ../../ct_utils.py animal_detection_api/
+cp ../../../ct_utils.py animal_detection_api/
 
 mkdir -p animal_detection_api/visualization/
-cp ../../visualization/visualization_utils.py animal_detection_api/visualization/
+cp ../../../visualization/visualization_utils.py animal_detection_api/visualization/
 
 # visualization_utils in turn depends on the following
 mkdir -p animal_detection_api/data_management
 mkdir -p animal_detection_api/data_management/annotations/
-cp ../../data_management/annotations/annotation_constants.py animal_detection_api/data_management/annotations/
+cp ../../../data_management/annotations/annotation_constants.py animal_detection_api/data_management/annotations/
 
 echo $1
 echo $2
