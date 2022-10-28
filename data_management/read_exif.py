0e9de966d2af943494bf57109bf1aed5d227d8c0
Dan Morris <dmorris@cs.stanford.edu>
2022-10-28 12:23:41 -0700

    Updated read_exif to support command-line operation

--- data_management/read_exif.py
+++ data_management/read_exif.py
@@ -18,7 +18,6 @@ import json
 
 from multiprocessing.pool import ThreadPool as ThreadPool
 from multiprocessing.pool import Pool as Pool
-# from functools import partial
 
 from tqdm import tqdm
 
@@ -28,21 +27,18 @@ from path_utils import find_images
 verbose = False
 n_print = 500
 debug_max_images = None
-use_joblib = False
-use_threads = False
-n_threads = 50
+
+# Number of concurrent workers
+n_workers = 1
+
+# Should we use threads (vs. processes) for parallelization?
+#
+# Not relevant if n_workers is 1.
+use_threads = True
 
 tag_types_to_ignore = set(['File','ExifTool'])
-# exiftool_command_name = r'c:\exiftool-12.13\exiftool(-k).exe'
-exiftool_command_name = r'c:\exiftool\exiftool.exe'
-assert os.path.isfile(exiftool_command_name)
 
-input_base = 'j:\\'
-output_base = 'j:\\exif_results'
-os.makedirs(output_base,exist_ok=True)
-
-file_list_cache_file = os.path.join(output_base,'file_list.txt')
-exif_cache_file = os.path.join(output_base,'exif_results.json')
+exiftool_command_name = 'exiftool'
 
 
 #%% Multiprocessing init
@@ -84,39 +80,25 @@ pinit(Counter(-1))
 
 #%% Functions
 
-def enumerate_files():
+def enumerate_files(input_folder):
     
     # image_files will contain the *relative* paths to all image files in the input folder
     
-    if (file_list_cache_file is None) or (not os.path.isfile(file_list_cache_file)):
-        
-        image_files = find_images(input_base,recursive=True)
-        image_files = [os.path.relpath(s,input_base) for s in image_files]
-        image_files = [s.replace('\\','/') for s in image_files]
-        if file_list_cache_file is not None:
-            with open(file_list_cache_file,'w') as f:
-                for fn in image_files:
-                    f.write(fn + '\n')
-                    
-        print('Enumerated {} files'.format(len(image_files)))
-        
-    else:               
-        
-        with open(file_list_cache_file,'r') as f:
-            image_files = f.readlines()
-            image_files = [s.strip() for s in image_files]
-            
-        print('Read a list of {} files'.format(len(image_files)))
-
+    image_files = find_images(input_folder,recursive=True)
+    image_files = [os.path.relpath(s,input_folder) for s in image_files]
+    image_files = [s.replace('\\','/') for s in image_files]
+    print('Enumerated {} files'.format(len(image_files)))
     return image_files
 
-# file_path = os.path.join(input_base,image_files[0])
-def read_exif_tags(file_path):
+
+def read_exif_tags_for_image(file_path):
     """
     Get relevant fields from EXIF data for an image
+    
+    Returns a list of lists, where each element is (type/tag/value)    
     """
     
-    result = {'status':'unknown','tags':{}}
+    result = {'status':'unknown','tags':[]}
     
     # -G means "Print group name for each tag", e.g. print:
     #
@@ -125,13 +107,15 @@ def read_exif_tags(file_path):
     # ...instead of:
     #
     # Bits Per Sample                 : 8
-    proc = subprocess.Popen([exiftool_command_name, '-G', file_path], stdout=subprocess.PIPE, encoding='utf8')
+    proc = subprocess.Popen([exiftool_command_name, '-G', file_path],
+                            stdout=subprocess.PIPE, encoding='utf8')
     
     exif_lines = proc.stdout.readlines()    
     exif_lines = [s.strip() for s in exif_lines]
-    if ( (exif_lines is None) or (len(exif_lines) == 0) or not any([s.lower().startswith('[exif]') for s in exif_lines])):
+    if ( (exif_lines is None) or (len(exif_lines) == 0) or not \
+        any([s.lower().startswith('[exif]') for s in exif_lines])):
         result['status'] = 'failure'
-        return results
+        return result
     
     # A list of three-element lists (type/tag/value)
     exif_tags = []
@@ -172,11 +156,19 @@ def read_exif_tags(file_path):
         
     # ...for each output line
         
-    return exif_tags
+    result['status'] = 'success'
+    result['tags'] = exif_tags
+    return result
 
 # ...process_exif()
+
+
+def populate_exif_data(im, input_folder, overwrite=True):
+    """
+    Populate EXIF data into the image object [im].
     
-def add_exif_data(im, overwrite=True):
+    Returns a modified version of [im].
+    """
     
     if cnt is not None:
         cnt.increment(n=1)
@@ -189,10 +181,14 @@ def add_exif_data(im, overwrite=True):
         return None
     
     try:
-        file_path = os.path.join(input_base,fn)
+        file_path = os.path.join(input_folder,fn)
         assert os.path.isfile(file_path)
-        exif_tags = read_exif_tags(file_path)
-        im['exif_tags'] = exif_tags
+        result = read_exif_tags_for_image(file_path)
+        if result['status'] == 'success':
+            exif_tags = result['tags']            
+            im['exif_tags'] = exif_tags
+        else:
+            print('Error reading EXIF data for {}'.format(file_path))
     except Exception as e:
         s = 'Error on {}: {}'.format(fn,str(e))
         print(s)
@@ -200,12 +196,15 @@ def add_exif_data(im, overwrite=True):
     return im
 
 
-def create_image_objects():
+def create_image_objects(input_folder):
+    """
+    Create empty image objects for every image in [input_folder].
+    """
     
-    image_files = enumerate_files()
+    image_files = enumerate_files(input_folder)
 
     images = []
-    for fn in tqdm(image_files):
+    for fn in image_files:
         im = {}
         im['file_name'] = fn
         images.append(im)
@@ -217,80 +216,170 @@ def create_image_objects():
     return images
 
 
-def read_exif_data(images):
+def populate_exif_for_images(input_folder,images):
+    """
+    Main worker loop: read EXIF data for each image object in [images] and 
+    populate the image objects.
+    """
     
-    if use_joblib:
-        
-        from joblib import Parallel, delayed
-        # results = Parallel(n_jobs=n_threads)(delayed(add_exif_data)(im) for im in images[0:10])
-        results = Parallel(n_jobs=n_threads)(delayed(add_exif_data)(im) for im in images)
-        
-    if n_threads == 1:
+    if n_workers == 1:
       
         results = []
-        for im in images:        
-            results.append(add_exif_data[im])    
+        for im in tqdm(images):
+            results.append(populate_exif_data(im,input_folder))
         
     else:
         
+        from functools import partial
         if use_threads:
-            pool = ThreadPool(n_threads)
+            print('Starting parallel thread pool with {} workers'.format(n_workers))
+            pool = ThreadPool(n_workers)
         else:
-            pool = Pool(n_threads)
+            print('Starting parallel process pool with {} workers'.format(n_workers))
+            pool = Pool(n_workers)
     
-        results = list(pool.map(add_exif_data,images))
+        results = list(pool.map(partial(populate_exif_data,input_folder=input_folder),images))
 
     return results
 
 
-def write_exif_results(results):
+def write_exif_results(results,output_file):
+    """
+    Write EXIF information to [output_file].
+    """
+    if output_file.endswith('.json'):
+        with open(output_file,'w') as f:
+            json.dump(results,f,indent=1)
+    else:
+        
+        assert output_file.endswith('.csv')
+        
+        # Find all EXIF tags that exist in any image
+        all_keys = set()
+        for im in results:
+            
+            keys_this_image = set()
+            exif_tags = im['exif_tags']
+            file_name = im['file_name']
+            for tag in exif_tags:
+                tag_name = tag[1]
+                assert tag_name not in keys_this_image, \
+                    'Error: tag {} appears twice in image {}'.format(
+                        tag_name,file_name)
+                all_keys.add(tag_name)
+                
+            # ...for each tag in this image
+            
+        # ...for each image
+        
+        all_keys = sorted(list(all_keys))
+        
+        header = ['File Name']
+        header.extend(all_keys)
+        
+        import csv
+        with open(output_file,'w') as csvfile:
+            
+            writer = csv.writer(csvfile)
+            
+            # Write header
+            writer.writerow(header)
+            
+            for im in results:
+                
+                row = [im['file_name']]
+                kvp_this_image = {tag[1]:tag[2] for tag in im['exif_tags']}
+                
+                for i_key,key in enumerate(all_keys):
+                    value = ''
+                    if key in kvp_this_image:
+                        value = kvp_this_image[key]
+                    row.append(value)                                        
+                # ...for each key that *might* be present in this image
+                
+                assert len(row) == len(header)
+                
+                writer.writerow(row)
+                
+            # ...for each image
+            
+        # ...with open()
     
-    with open(exif_cache_file,'w') as f:
-        json.dump(results,f,indent=1)
+    print('Wrote results to {}'.format(output_file))
+
+def is_tool(name):
+    """Check whether `name` is on PATH and marked as executable."""
+    
+    # https://stackoverflow.com/questions/11210104/check-if-a-program-exists-from-a-python-script
+
+    from shutil import which
+    return which(name) is not None
 
 
+def read_exif_from_folder(input_folder,output_file):
+    
+    assert os.path.isdir(input_folder)
+    
+    assert output_file.lower().endswith('.json') or output_file.lower().endswith('.csv'), \
+        'I only know how to write results to .json or .csv'
+        
+    try:
+        with open(output_file, 'a') as f:
+            if not f.writable():
+                raise IOError('File not writable')
+    except Exception:
+        print('Could not write to file {}'.format(output_file))
+        raise
+    
+    assert is_tool(exiftool_command_name), 'exiftool not available'
+
+    images = create_image_objects(input_folder)
+    results = populate_exif_for_images(input_folder,images)
+    write_exif_results(results,output_file)
+    
+    
 #%% Interactive driver
 
 if False:
     
     #%%
     
-    images = create_image_objects()
-    results = read_exif_data(images)
-    write_exif_results(results)
+    input_folder = os.path.expanduser('~/data/KRU-test')
+    # input_folder = os.path.expanduser('~/data/KRU')
+    # output_file = os.path.expanduser('~/data/test-exif.json')
+    output_file = os.path.expanduser('~/data/test-exif.csv')
     
+    read_exif_from_folder(input_folder,output_file)
+
+    #%%
+    
+    with open(output_file,'r') as f:
+        d = json.load(f)
+        
+
 #%% Command-line driver
 
+import argparse
+
+def main():
+
+    global use_threads,n_workers
+    
+    parser = argparse.ArgumentParser(description=('Read EXIF information from all images in' + \
+                                                  ' a folder, and write the results to .csv or .json'))
+
+    parser.add_argument('input_folder', type=str)
+    parser.add_argument('output_file', type=str)
+    parser.add_argument('--n_workers', type=int, default=1,
+                        help='Number of concurrent workers to use (defaults to 1)')
+    parser.add_argument('--use_threads', action='store_true',
+                        help='Use threads (instead of processes) for multitasking')
+    
+    args = parser.parse_args()
+    use_threads = args.use_threads
+    n_workers = args.n_workers
+    
+    read_exif_from_folder(args.input_folder,args.output_file)
+    
 if __name__ == '__main__':
-    
-    images = create_image_objects()
-    results = read_exif_data(images)
-    write_exif_results(results)
-
-
-#%% Create image objects
-    
-
-
-#%% Write results
-
-
-
-#%% Scrap
-
-if False:
-    
-    pass
-
-    #%%
-    
-    im = images[0]
-    file_path = os.path.join(input_base,im['file_name'])
-    exif_tags = read_exif_tags(file_path)
-    
-    #%%
-    
-    fn = r"J:\exif_results\exif_results.json"
-    with open(fn,'r') as f:
-        exif_results = json.load(f)
-    
+    main()
