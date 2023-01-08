2775d75e797c60ce7d013247ad557d5e6658fcb6
Dan Morris <dmorris@cs.stanford.edu>
2023-01-07 17:46:26 -0800

    Make sure category names are unique when validating .json DBs

--- data_management/databases/integrity_check_json_db.py
+++ data_management/databases/integrity_check_json_db.py
@@ -133,6 +133,7 @@ def integrity_check_json_db(jsonFile, options=None):
     imageIdToImage = {}
     annIdToAnn = {}
     catIdToCat = {}
+    catNameToCat = {}
     imageLocationSet = set()
     
     print('Checking categories...')
@@ -147,11 +148,15 @@ def integrity_check_json_db(jsonFile, options=None):
         assert isinstance(cat['name'],str), 'Illegal category name type'
         
         catId = cat['id']
+        catName = cat['name']
         
         # Confirm ID uniqueness
-        assert catId not in catIdToCat
+        assert catId not in catIdToCat, 'Category ID {} is used more than once'.format(catId)
         catIdToCat[catId] = cat
         cat['_count'] = 0
+        
+        assert catName not in catNameToCat, 'Category name {} is used more than once'.format(catName)
+        catNameToCat[catName] = cat        
         
     # ...for each category
         
