cbb5a95549386d46e2b7380220e510d46b14c6f6
Joejyn Wan <joejyn@gmail.com>
2025-01-07 14:06:21 +0800

    Remove extra arguments

--- PytorchWildlife/models/detection/ultralytics_based/megadetectorv6.py
+++ PytorchWildlife/models/detection/ultralytics_based/megadetectorv6.py
@@ -20,7 +20,7 @@ class MegaDetectorV6(YOLOV8Base):
         2: "vehicle"
     }
 
-    def __init__(self, weights=None, device="cpu", pretrained=True, version='yolov9c', filename="MDV6b-yolov9c.pt"):
+    def __init__(self, weights=None, device="cpu", version='yolov9c'):
         """
         Initializes the MegaDetectorV5 model with the option to load pretrained weights.
         
