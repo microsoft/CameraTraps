60093e27aa36b9216bfc3548af3d6b2706e12764
zhmiao <zhongqimiao@microsoft.com>
2025-04-09 19:14:33 -0700

    updates

--- PytorchWildlife/models/detection/ultralytics_based/Deepfaune.py
+++ PytorchWildlife/models/detection/ultralytics_based/Deepfaune.py
@@ -1,8 +1,8 @@
 """
 This is a Pytorch-Wildlife loader for the Deepfaune detector.
 The original Deepfaune model is available at: https://plmlab.math.cnrs.fr/deepfaune/software
-Model Weights Licence: CC BY-SA 4.0
-Model Weights Copyright CNRS 2024
+Licence: CC BY-SA 4.0
+Copyright CNRS 2024
 simon.chamaille@cefe.cnrs.fr; vincent.miele@univ-lyon1.fr
 """
 
