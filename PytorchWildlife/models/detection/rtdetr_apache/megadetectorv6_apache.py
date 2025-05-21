5499c6c76aa909125b6041faf8e97355344f8385
Daniela Ruiz <da.ruizl1@uniandes.edu.co>
2025-05-21 14:57:58 +0000

    weights links updated

--- PytorchWildlife/models/detection/rtdetr_apache/megadetectorv6_apache.py
+++ PytorchWildlife/models/detection/rtdetr_apache/megadetectorv6_apache.py
@@ -32,13 +32,13 @@ class MegaDetectorV6Apache(RTDETRApacheBase):
         """
         self.IMAGE_SIZE = 640
 
-        if version == "MDV6-rtdetr-c-apache":
-            url = "https://zenodo.org/records/15178680/files/MDV6-rtdetr_s.pth?download=1"
-            self.MODEL_NAME = "MDV6-rtdetr_s.pth"
-        elif version == "MDV6-rtdetr-e-apache":
-            url = "https://zenodo.org/records/15178680/files/MDV6-rtdetr_x.pth?download=1"
-            self.MODEL_NAME = "MDV6-rtdetr_x.pth"
+        if version == "MDV6-apa-rtdetr-c":
+            url = "https://zenodo.org/records/15398270/files/MDV6-apa-rtdetr-c.pth?download=1"
+            self.MODEL_NAME = "MDV6-apa-rtdetr-c.pth"
+        elif version == "MDV6-apa-rtdetr-e":
+            url = "https://zenodo.org/records/15398270/files/MDV6-apa-rtdetr-e.pth?download=1"
+            self.MODEL_NAME = "MDV6-apa-rtdetr-e.pth"
         else:
-            raise ValueError('Select a valid model version: MDV6-rtdetr-c-apache or MDV6-rtdetr-e-apache')
+            raise ValueError('Select a valid model version: MDV6-apa-rtdetr-c or MDV6-apa-rtdetr-e')
 
         super(MegaDetectorV6Apache, self).__init__(weights=weights, device=device, url=url)
--- PytorchWildlife/models/detection/yolo_mit/megadetectorv6_mit.py
+++ PytorchWildlife/models/detection/yolo_mit/megadetectorv6_mit.py
@@ -32,13 +32,13 @@ class MegaDetectorV6MIT(YOLOMITBase):
         """
         self.IMAGE_SIZE = 640
 
-        if version == 'MDV6-yolov9-c-mit':
-            url = "https://zenodo.org/records/15150394/files/MDV6-yolov9s-mit.ckpt?download=1"
-            self.MODEL_NAME = "MDV6-yolov9s-mit.ckpt"
-        elif version == 'MDV6-yolov9-e-mit':
-            url = "https://zenodo.org/records/15150394/files/MDV6-yolov9c-mit.ckpt?download=1"
-            self.MODEL_NAME = "MDV6-yolov9c-mit.ckpt"
+        if version == 'MDV6-mit-yolov9-c':
+            url = "https://zenodo.org/records/15398270/files/MDV6-mit-yolov9-c.ckpt?download=1"
+            self.MODEL_NAME = "MDV6-mit-yolov9-c.ckpt"
+        elif version == 'MDV6-mit-yolov9-e':
+            url = "https://zenodo.org/records/15398270/files/MDV6-mit-yolov9-e.ckpt?download=1"
+            self.MODEL_NAME = "MDV6-mit-yolov9-e.ckpt"
         else:
-            raise ValueError('Select a valid model version: MDV6-yolov9-c-mit or MDV6-yolov9-e-mit')
+            raise ValueError('Select a valid model version: MDV6-mit-yolov9-c or MDV6-mit-yolov9-e')
 
         super(MegaDetectorV6MIT, self).__init__(weights=weights, device=device, url=url)
