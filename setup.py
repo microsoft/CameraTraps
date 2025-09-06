ba6da8cc8d94b401e3a675c89febd9fce660b9a5
daniela <da.ruizl1@uniandes.edu.co>
2025-09-06 04:50:24 +0000

    pypi bugfix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', encoding="utf8") as file:
         long_description = file.read()
 setup(
     name='PytorchWildlife',
-    version='1.2.4', 
+    version='1.2.4.1', 
     packages=find_packages(),
     url='https://github.com/microsoft/CameraTraps/',  
     license='MIT',
